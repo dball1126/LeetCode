dffb3e74e0761c981043f3976de419bb5f509fe2
Daniel Ball <g.thirteen333@gmail.com>
2021-12-08 22:21:06 -0500

    Time: 99 ms (27.71%), Space: 38.3 MB (87.90%) - LeetHub

--- fibonacci-number/fibonacci-number.js
+++ fibonacci-number/fibonacci-number.js
@@ -1,11 +1,9 @@
 /**
- * @param {number} N
+ * @param {number} n
  * @return {number}
  */
-const fib = (n) => {
-    let dp = [0,1]
-    for (let i = 2; i <= n; i++) {
-        dp[i] = dp[i - 1] + dp[i -2]
-    }
-    return dp[n]
-}
+var fib = function(n, memo = new Map([[0,0],[1,1]])) {
+  if (memo.has(n)) return memo.get(n);
+  memo.set(n, fib(n-1, memo) + fib(n-2, memo))
+  return memo.get(n)
+};
