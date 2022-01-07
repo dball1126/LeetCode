30052f302b025854439235128f29eca65f4c5c2d
Daniel Ball <g.thirteen333@gmail.com>
2022-01-06 22:06:15 -0500

    Time: 76 ms (87.82%), Space: 40.9 MB (39.38%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.js
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.js
@@ -7,21 +7,19 @@
  *                      d[i] = Math.max(val, dp(i+1, h) )
  */
 var maxProfit = function(prices) {
-    let memo = [...new Array(prices.length+1)].map(a => [...new Array(2).fill(-Infinity)])
+    let [key, val, h] = ['', 0,  1]
 
-    const dp = (i, h) => {
-        if (i >= prices.length) return 0;
-        if (memo[i][h] !== -Infinity) return memo[i][h]
-        let val;
-
-        if (h === 1) {
-            val = dp(i+1, 0) - prices[i]
-        } else {
-            val = dp(i+2, 1) + prices[i]
+    let dp = [...new Array(prices.length+2)].map(a => [...new Array(3).fill(0)])
+    for (let i = prices.length-1; i >= 0; i--) {
+        for (let v = h; v >= 0; v--) {
+            if (v === 1) {
+                val = dp[i+1][0] - prices[i]
+            } else {
+                val = dp[i+2][1] + prices[i]
+            }
+            dp[i][v] = Math.max(val, dp[i+1][v])
         }
-
-        memo[i][h] = Math.max(val, dp(i+1, h))
-        return memo[i][h]
+        
     }
-    return dp(0, 1);
+    return dp[0][1]
 };
