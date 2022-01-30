10c577da009f80b798049a00e68ef9f80a539410
Daniel Ball <g.thirteen333@gmail.com>
2022-01-30 04:02:18 -0500

    Time: 93 ms (48.96%), Space: 42.8 MB (5.69%) - LeetHub

---
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -0,0 +1,29 @@
+/**
+ * Sate var: i for idx of costs
+ * Base case: 0 for out of bounds
+ * Recurrence Relation:
+ *  dp(n) = nums[i] + Math.min(dp(i-2), dp(i-1))
+ */
+// top down
+//  var minCostClimbingStairs = function(cost) {
+//     const memo = [...new Array(cost.length+1)].fill(Infinity)
+//     const dp = (i) => {
+//         if (i < 0) return 0
+//         if (memo[i] !== Infinity) return memo[i]
+//         memo[i] = cost[i] + Math.min(dp(i-1), dp(i-2))
+//         return memo[i]
+//     }
+//     dp(cost.length-1)
+//     console.log(memo)
+//     return Math.min(memo[cost.length-1], memo[cost.length-2])
+// };
+// bottom up
+var minCostClimbingStairs = function(cost) {
+    if (cost.length === 2) return Math.min(cost[0], cost[1])
+
+    for (let i = 2; i < cost.length; i++) {
+        cost[i] = cost[i] + Math.min(cost[i-2], cost[i-1])
+    }
+
+    return Math.min( cost[cost.length-1], cost[cost.length-2] )
+}
