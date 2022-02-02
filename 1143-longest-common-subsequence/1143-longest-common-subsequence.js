3594efcb7543e285d37f987a934af7b40762186d
Daniel Ball <g.thirteen333@gmail.com>
2022-02-01 22:53:59 -0500

    Time: 223 ms (25.45%), Space: 58.8 MB (26.45%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -7,21 +7,30 @@
  *      dp(i, j) = 1 + dp(i+1, j+1)
  *  else 
  *      dp(i, j) = Math.max(dp(i+1, j), dp(i, j+1))
+ * Space: O(n)
+ * Time: O(1) + n * m = O(n * m)
  */
-// top down
+// bottom up
+
 var longestCommonSubsequence = function(s, t) {
     let len = Math.max(s.length, t.length)
-    const memo = [...new Array(len+1)].map(a => [...new Array(len+1)].fill(-Infinity))
-    const dp = (i, j) => {
-        if (i >= s .length || j >= t.length) return 0;
-        if (memo[i][j] !== -Infinity) return memo[i][j];
-        memo[i][j] = 0;
-        if (s[i] === t[j]) {
-            memo[i][j] = 1 + dp(i+1, j+1)
-        } else {
-            memo[i][j] = Math.max(dp(i+1, j), dp(i, j+1))
+    const dp = [...new Array(len+1)].map(a => [...new Array(len+1)].fill(0))
+
+    for (let i = 0; i < s.length; i++) {
+        for (let j = 0; j < t.length; j++) {
+            if (s[i] === t[j]) {
+                dp[i][j] += 1
+                if (dp[i-1] && dp[i-1][j-1]) {
+                    dp[i][j] += dp[i-1][j-1]
+                }
+            } else {
+                let [v1, v2] = [0, 0]
+                if (dp[i-1] && dp[i-1][j]) v1 = dp[i-1][j]
+                if (dp[j-1] && dp[i][j-1]) v2 = dp[i][j-1]
+                dp[i][j] = Math.max(v1, v2)
+            }
         }
-        return memo[i][j];
+        
     }
-    return dp(0,0)
+    return dp[s.length-1][t.length-1]
 }
