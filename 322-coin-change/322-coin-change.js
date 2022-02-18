6c80091f2a55f049c0cc305c739b2bb547efc3cd
Daniel Ball <g.thirteen333@gmail.com>
2022-02-17 23:28:12 -0500

    Time: 237 ms (34.23%), Space: 49.2 MB (19.53%) - LeetHub

--- 322-coin-change/322-coin-change.js
+++ 322-coin-change/322-coin-change.js
@@ -9,36 +9,21 @@
  * return amt
  */
 // Top-Down
-// var coinChange = function(coins, amount) {
-//     const memo = [...new Array(amount+1)].fill(-1)
-
-//     const dp = (a) => {
-//         if (a === 0) return 0;
-//         if (memo[a] !== -1) return memo[a];
-//         memo[a] = Infinity;
-//         for (let c of coins) {
-//             // console.log("coin: " + c)
-//             if (a - c >= 0 && a >= c) {
-//                 memo[a] = Math.min(memo[a], 1 + dp(a-c))
-//             }
-//         }
-//         return memo[a];
-//     }
-//     const result = dp(amount)
-//     return result === Infinity ? -1 : result;
-// }
-// Bottom-Up
 var coinChange = function(coins, amount) {
-    if (amount === 0) return 0;
-    const dp = [...new Array(amount+1)].fill(Infinity);
-    dp[0] = 0
-    for (let a = 1; a <= amount; a++) {
-        for (let c of coins) {
+    const memo = [...new Array(amount+1)].fill(-1)
 
+    const dp = (a) => {
+        if (a === 0) return 0;
+        if (memo[a] !== -1) return memo[a];
+        memo[a] = Infinity;
+        for (let c of coins) {
+            // console.log("coin: " + c)
             if (a - c >= 0 && a >= c) {
-                dp[a] = Math.min(dp[a], 1 + dp[a-c])
+                memo[a] = Math.min(memo[a], 1 + dp(a-c))
             }
         }
+        return memo[a];
     }
-    return dp[amount] === Infinity ? -1 : dp[amount]
+    const result = dp(amount)
+    return result === Infinity ? -1 : result;
 }
