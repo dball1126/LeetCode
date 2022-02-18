275c7b04f7b7f24c85b9f1b8fd6ac5100843b534
Daniel Ball <g.thirteen333@gmail.com>
2022-02-17 23:29:21 -0500

    Time: 156 ms (67.17%), Space: 47.7 MB (30.89%) - LeetHub

--- 322-coin-change/322-coin-change.js
+++ 322-coin-change/322-coin-change.js
@@ -7,23 +7,40 @@
  *      if a -c >= 0 && a <= c
  *          amt = Math.min(amt, 1 + dp(a-c))
  * return amt
+ * 
+ * Time Complexity: O(n * m)  coins * amount
+ * Space Complexity: O(m) the amount
  */
 // Top-Down
-var coinChange = function(coins, amount) {
-    const memo = [...new Array(amount+1)].fill(-1)
+// var coinChange = function(coins, amount) {
+//     const memo = [...new Array(amount+1)].fill(-1)
 
-    const dp = (a) => {
-        if (a === 0) return 0;
-        if (memo[a] !== -1) return memo[a];
-        memo[a] = Infinity;
+//     const dp = (a) => {
+//         if (a === 0) return 0;
+//         if (memo[a] !== -1) return memo[a];
+//         memo[a] = Infinity;
+//         for (let c of coins) {
+//             if (a - c >= 0 && a >= c) {
+//                 memo[a] = Math.min(memo[a], 1 + dp(a-c))
+//             }
+//         }
+//         return memo[a];
+//     }
+//     const result = dp(amount)
+//     return result === Infinity ? -1 : result;
+// }
+// Bottom-Up
+var coinChange = function(coins, amount) {
+    if (amount === 0) return 0;
+    const dp = [...new Array(amount+1)].fill(Infinity);
+    dp[0] = 0
+    for (let a = 1; a <= amount; a++) {
         for (let c of coins) {
-            // console.log("coin: " + c)
+
             if (a - c >= 0 && a >= c) {
-                memo[a] = Math.min(memo[a], 1 + dp(a-c))
+                dp[a] = Math.min(dp[a], 1 + dp[a-c])
             }
         }
-        return memo[a];
     }
-    const result = dp(amount)
-    return result === Infinity ? -1 : result;
+    return dp[amount] === Infinity ? -1 : dp[amount]
 }
