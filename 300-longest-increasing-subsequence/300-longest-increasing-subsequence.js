c90b1fa9338477988bf6186355154172ea2d3839
Daniel Ball <g.thirteen333@gmail.com>
2022-02-18 20:43:26 -0500

    Time: 458 ms (9.16%), Space: 45.2 MB (6.55%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.js
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.js
@@ -1,39 +1,35 @@
 /**
- * State Var: i for idx of nums, j for previous idx removed
- * Base case: 0 for out of bounds
- * Recurrence relation:
- *  val = 0;
- *  if (i === j) val = 1
- *      if (nums[i] < nums[j]) 
- *          val = Math.max(1 + dp(i-1, i), dp(i-1, j))
- *      else 
- *          val += dp(i-1, j)
+ * State Var: i for idx of array .... to end of array
+ * Base Case: 1 for first element. 0 for out of bounds.
+ * Recurrence Relation:
+ * for i of nums 
+ *   for j of nums 
+ *       if (nums[j] < nums[i])
+ *          dp(i) = Math.max(dp(i), 1 + dp(j))
  * 
- * const doNothing = dp(i-1, j)
- * return val;
- * Time: O(1) + O(n) + O(n) = O(n)
- * Space: O(n*2)
+ *  return dp(i)
+ *  ***return max in dp array
  */
-
-// top down
 var lengthOfLIS = function(nums) {
-    let memo = [...new Array(nums.length+1)].map(a => [...new Array(nums.length+1)].fill(-Infinity))
+    const memo = [...new Array(nums.length+1)].fill(-Infinity)
+    memo[0] = 1;
     let max = 1;
 
-    const dp = (i, j) => {
-        if (i < 0) return 0;
-        if (memo[i][j] !== -Infinity) return memo[i][j]
-        let val = 0;
-        if (i === j) val = 1;
-
-        if (nums[i] < nums[j]) {
-            val = Math.max(1 + dp(i-1, i), dp(i-1, j))
-        } else {
-            val += dp(i-1, j)
+    const dp = (i) => {
+        if (memo[i] !== -Infinity) return memo[i];
+        memo[i] = 1;
+        for (let j = 0; j < i; j++) {
+            if (nums[j] < nums[i]) {
+                memo[i] = Math.max(memo[i], 1 + dp(j))
+            }
         }
-        return memo[i][j] = val;
+        return memo[i];
     }
-    nums.forEach((v, i) => dp(i, i))
-    memo.forEach(a => a.forEach(v => max = Math.max(max, v)))
-    return max;
+
+    for (let i = 1; i < nums.length; i++) {
+        dp(i);
+    }
+
+    memo.forEach(v => max = Math.max(max, v))
+    return max
 }
