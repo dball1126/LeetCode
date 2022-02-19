2b294b24cfa836e7c8d06662e7130ced3f9c4eb1
Daniel Ball <g.thirteen333@gmail.com>
2022-02-18 21:50:18 -0500

    Time: 172 ms (66.58%), Space: 44 MB (20.80%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.js
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.js
@@ -10,26 +10,43 @@
  *  return dp(i)
  *  ***return max in dp array
  */
+// Top Down
+// var lengthOfLIS = function(nums) {
+//     const memo = [...new Array(nums.length+1)].fill(-Infinity)
+//     memo[0] = 1;
+//     let max = 1;
+
+//     const dp = (i) => {
+//         if (memo[i] !== -Infinity) return memo[i];
+//         memo[i] = 1;
+//         for (let j = 0; j < i; j++) {
+//             if (nums[j] < nums[i]) {
+//                 memo[i] = Math.max(memo[i], 1 + dp(j))
+//             }
+//         }
+//         return memo[i];
+//     }
+
+//     for (let i = 1; i < nums.length; i++) {
+//         dp(i);
+//     }
+
+//     memo.forEach(v => max = Math.max(max, v))
+//     return max
+// }
+// Bottom Up
 var lengthOfLIS = function(nums) {
-    const memo = [...new Array(nums.length+1)].fill(-Infinity)
-    memo[0] = 1;
+    const dp = [...new Array(nums.length+1)].fill(1);
     let max = 1;
 
-    const dp = (i) => {
-        if (memo[i] !== -Infinity) return memo[i];
-        memo[i] = 1;
+    for (let i = 1; i < nums.length; i++) {
         for (let j = 0; j < i; j++) {
             if (nums[j] < nums[i]) {
-                memo[i] = Math.max(memo[i], 1 + dp(j))
+                dp[i] = Math.max(dp[i], 1 + dp[j])
             }
         }
-        return memo[i];
     }
 
-    for (let i = 1; i < nums.length; i++) {
-        dp(i);
-    }
-
-    memo.forEach(v => max = Math.max(max, v))
-    return max
+    dp.forEach(v => max = Math.max(max, v))
+    return max;
 }
