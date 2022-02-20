84732e66d5c630a6ddd6175089abeaf049a32ccb
Daniel Ball <g.thirteen333@gmail.com>
2022-02-20 01:08:40 -0500

    Time: 480 ms (12.96%), Space: 41.6 MB (40.74%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.js
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.js
@@ -9,20 +9,20 @@
  *  for i of vowels
  *      dp[i] += dp(i, n-1)
  * 
- * return dp[i]           
+ * return dp[i]
+ * Time: O(n) = 5 * O(n) = O(n)
+ * Space: O(1) (no overlapping sub problems)   
  */
+// Top-Down
  var countVowelStrings = function(n) {
     const vowels = ['a','e','i','o','u']
-    const memo = [...new Array(vowels.length+1)].map(a => [...new Array(n+1)].fill(-Infinity))
     const dp = (i, a) => {
         if (a === 0) return 1;
-        if (memo[i][a] !== -Infinity) return memo[i][a]
-        memo[i][a] = 0;
-
+        val = 0;
         for(let j = i; j < vowels.length; j++) {
-            memo[i][a] += dp(j, a-1)
+            val += dp(j, a-1)
         }
-        return memo[i][a]
+        return val
     }
     return dp(0, n)
 };
