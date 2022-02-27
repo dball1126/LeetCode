b04602ef835e41c05574ab997226c61d37db95ca
Daniel Ball <g.thirteen333@gmail.com>
2022-02-26 23:58:22 -0500

    Time: 96 ms (57.46%), Space: 44 MB (29.99%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.js
@@ -5,18 +5,28 @@
  *  dp[i] = Math.min(dp[i] + dp[i-1], dp[i] + dp[i-2])
  */
 // Top Down
- var minCostClimbingStairs = function(costs) {
-    const memo = [...new Array(costs.length+1)].fill(Infinity);
-    memo[0] = costs[0];
-    memo[1] = costs[1];
+//  var minCostClimbingStairs = function(costs) {
+//     const memo = [...new Array(costs.length+1)].fill(Infinity);
+//     memo[0] = costs[0];
+//     memo[1] = costs[1];
 
-    const dp = (i) => {
-        if ( i >= costs.length) return 0;
-        if (memo[i] !== Infinity) return memo[i]
-        return memo[i] = Math.min(costs[i] + memo[i-1], costs[i] + memo[i-2]);
+//     const dp = (i) => {
+//         if ( i >= costs.length) return 0;
+//         if (memo[i] !== Infinity) return memo[i]
+//         return memo[i] = Math.min(costs[i] + memo[i-1], costs[i] + memo[i-2]);
+//     }
+//     for(let i = 2; i < costs.length; i++) {
+//         dp(i)
+//     }
+//     return Math.min(memo[costs.length-1], memo[costs.length-2])
+// }
+// Bottom up
+var minCostClimbingStairs = function(costs) {
+    const dp = [...new Array(costs.length+1)].fill(Infinity);
+    dp[0] = costs[0];
+    dp[1] = costs[1];
+    for (let i = 2; i < costs.length; i++) {
+        dp[i] = Math.min(costs[i] + dp[i-1], costs[i] + dp[i-2]);
     }
-    for(let i = 2; i < costs.length; i++) {
-        dp(i)
-    }
-    return Math.min(memo[costs.length-1], memo[costs.length-2])
+    return Math.min(dp[costs.length-1], dp[costs.length-2])
 }
