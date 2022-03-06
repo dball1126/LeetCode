75c723fde405ae622f59f0ac472ea83f435f8c8b
Daniel Ball <g.thirteen333@gmail.com>
2022-03-06 04:07:58 -0500

    Time: 456 ms (12.82%), Space: 109.8 MB (8.06%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.js
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.js
@@ -1,34 +1,19 @@
-/**
- * Base cases 0 for for end of array h === 1 or out of bounds
- * State Var: i for day h for buying(1) selling(0)
- * Recurrence relation:
- *  if (buying)
- *      memo[i][h] = dp(i, 0) - prices[i]
- * if (selling)
- *  memo[i][h] = dp(i, 1) + prices[i] - fee
- * 
- * return Math.max( memo[i][h], dp(i+1, h))
- * @param {number[]} prices
- * @param {number} fee
- * @return {number}
- */
- var maxProfit = function(prices, fee) {
+var maxProfit = function(prices, fee) {
     let memo = [...new Array(prices.length+1)].map(a => [...new Array(3)].fill(-Infinity))
-
-    const dp = (i, h) => {
-        if (i >= prices.length) {
+    const dp = (i, t) => {
+        if (i === prices.length-1 && t === 1 || i >= prices.length){
             return 0;
         }
-        if (memo[i][h] !== -Infinity) return memo[i][h]
-        if (i === prices.length-1 && h === 1) return 0
-
-        if (h === 1) {
-            memo[i][h] = Math.max(dp(i+1, 0) - prices[i], -prices[i])
+        if (memo[i][t] !== -Infinity) return memo[i][t]
+    
+        if (t === 1) {
+           return memo[i][t] = Math.max(dp(i+1,t),
+                - prices[i] + dp(i+1, 0))
         } else {
-            memo[i][h] = Math.max(dp(i+1, 1) + prices[i] - fee, prices[i] - fee)
+            return memo[i][t] = Math.max(dp(i+1, t),
+                prices[i] + dp(i+1, 1) - fee)
+            
         }
-
-        return memo[i][h] = Math.max(memo[i][h], dp(i+1, h))
     }
     return dp(0, 1)
-};
+}
