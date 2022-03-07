f4c5ed4521286eb0b38c26ddc96b8e67caabbc28
Daniel Ball <g.thirteen333@gmail.com>
2022-03-06 19:24:36 -0500

    Time: 84 ms, Memory: 42.9 MB - LeetHub

--- unique-paths/unique-paths.js
+++ unique-paths/unique-paths.js
@@ -1,9 +1,4 @@
 /**
- * @param {number} m
- * @param {number} n
- * @return {number}
- */
-/**
  * Create 2d array.
  * State variables: r for row   c for column
  * Base case: 1 if you reach the end........ 0 if out of bounds.
@@ -12,15 +7,27 @@
  * Time: O(m * n)
  * Space: O(m * n)
  */
+// var uniquePaths = function(m, n) {
+//     const memo = [...new Array(m+1)].map(a => [...new Array(n+1)].fill(-Infinity))
+
+//     const dp = (r, c) => {
+//         if (r >= m || c >= n) return 0;
+//         if (r === m-1 && c === n-1) return 1;
+//         if (memo[r][c] !== -Infinity) return memo[r][c];
+
+//         return memo[r][c] = dp(r+1, c) + dp(r, c+1)
+//     }
+//     return dp(0, 0)
+// };
+// Bottom up
 var uniquePaths = function(m, n) {
-    const memo = [...new Array(m+1)].map(a => [...new Array(n+1)].fill(-Infinity))
-
-    const dp = (r, c) => {
-        if (r >= m || c >= n) return 0;
-        if (r === m-1 && c === n-1) return 1;
-        if (memo[r][c] !== -Infinity) return memo[r][c];
-
-        return memo[r][c] = dp(r+1, c) + dp(r, c+1)
+    const dp = [...new Array(m+1)].map(a => [...new Array(n+1)].fill(0))
+    dp[m-1][n-1] = 1;
+    for (let r = m-1; r >= 0; r--) {
+        for (let c = n-1; c >= 0; c--) {
+            if (!(r === m-1 && c === n-1))
+                dp[r][c] = dp[r+1][c] + dp[r][c+1] 
+        }
     }
-    return dp(0, 0)
-};
+    return dp[0][0]
+}
