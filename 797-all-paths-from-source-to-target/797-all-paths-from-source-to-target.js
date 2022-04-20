c7146886294d943c06d3a817f08326dbf36ddd68
Daniel Ball <g.thirteen333@gmail.com>
2022-04-19 21:38:05 -0400

    Time: 192 ms (15.17%), Space: 52 MB (37.47%) - LeetHub

---
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.js
@@ -0,0 +1,25 @@
+/**
+ * BFS, Queue
+ * Build adjList
+ * Time and Space: O(V * E)
+ */
+var allPathsSourceTarget = function(graph) {
+    const paths = [];
+    const q = [[0]]
+
+    while (q.length) {
+        const curr = q.shift();
+        const c = curr[curr.length-1]
+        if (c === graph.length-1) {
+            paths.push(curr)
+        } else {
+            const arr = graph[c]
+            if (arr) {
+                arr.forEach(v => {
+                    q.push([...curr, v])
+                })
+            }
+        }
+    }
+    return paths;
+};
