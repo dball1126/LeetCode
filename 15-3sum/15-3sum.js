2d73fb79899406c7d6bcadbe391f8f7b0af289ea
Daniel Ball <g.thirteen333@gmail.com>
2022-05-07 04:11:06 -0400

    Time: 231 ms (40.47%), Space: 52.3 MB (78.77%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -1,24 +1,24 @@
 function threeSum(n) {
-    n.sort((a,b)=> a-b); const map = new Map();
-
-    for (let i = 0; i < n.length-2; i++) {
-        let [l, r] = [i+1, n.length-1]
-        
-        while (l < r) {
-            let [n1, n2, n3] = [n[i], n[l], n[r]]
-            let k = n1 + "" + n2 + "" + n3
-
-            if (n1+n2+n3 === 0 && !map.has(k)) {
-                map.set(k, [n1,n2,n3])
-                l++; r--;
-            } else if (n1+n2+n3 < 0) {
-                l++
-                while (n[l] === n[l-1]) l++
-            } else {
-                r--
-                while (n[r+1] === n[r]) r--
+    if (n.length <= 2) return []
+    n.sort((a,b)=> a-b);
+    let result = [];
+    
+    for (let i = 0; i < n.length; i++) {
+        let [s, e] = [i+1, n.length-1]
+        while (s < e) {
+            let v = n[i] + n[s] + n[e]
+            if ( v === 0) {
+                result.push([n[i], n[s], n[e]])
             }
-        }   
+            if (v > 0) {
+                e--
+                while (s < e && n[e+1] === n[e]) e--
+            } else {
+                s++
+                while (s < e && n[s-1] === n[s]) s++
+            }
+        }
+        while (n[i+1] === n[i]) i++
     }
-    return Array.from(map.values())
-};
+    return result
+}
