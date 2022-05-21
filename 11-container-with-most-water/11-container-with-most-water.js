10973a2e9bb23ff2c71d68fc8cb71550fc083afc
Daniel Ball <g.thirteen333@gmail.com>
2022-05-21 00:45:36 -0400

    Time: 111 ms (40.28%), Space: 49.4 MB (74.24%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.js
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -1,32 +1,16 @@
-/**
- * 2 pointer, start from edges, collect two max values from the input
- * Time: O(n) + O(n) + O(1) = O(n)
- * Space: O(1)
- */
- var maxArea = function(n) {
-    let [m1, m2, mArea, l, r] = [n[0], n[1], -Infinity, 0, n.length-1]
-    for (let i = 2; i < n.length; i++) {
-        let v = n[i]
-        if (v > m1) {
-            if (m2 < m1) {
-                m2 = m1;
-                m1 = v;
-            } else {
-                m1 = v
-            }
-        } else if (v > m2) {
-            m2 = v
-        }
-    }
-    while (l < r) {
-        mArea = Math.max(mArea, Math.min(n[l], n[r]) * (r-l))
-        if ((n[l] === m1 && n[r] === m2) || (n[l] === m2 && n[r] === m1)) {
-            return mArea;
-        }
-        if (n[l] > n[r]) {
-            r--
+// two pointer
+// time O(n)
+// space O(1)
+
+const maxArea = (a) => {
+    let [s, e, max] = [0, a.length-1, 0]
+    while (s < e) {
+        max = Math.max(max, (e-s) * Math.min(a[s], a[e]))
+        if (a[s] <= a[e]) {
+            s++
         } else {
-            l++
+            e--
         }
     }
-};
+    return max;
+}
