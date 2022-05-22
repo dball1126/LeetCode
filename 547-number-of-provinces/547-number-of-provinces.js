002ae896893f92cb1e1132e4bd31f81f0d54c98e
Daniel Ball <g.thirteen333@gmail.com>
2022-05-22 01:48:13 -0400

    Time: 136 ms (14.97%), Space: 45.6 MB (25.23%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.js
+++ 547-number-of-provinces/547-number-of-provinces.js
@@ -2,47 +2,49 @@ class DisJoinSet {
     root = []
     rank = []
     provinces = 0
-    constructor(a) { this.init(a) }
+    constructor(a) {
+        this.provinces = a.length;
+        this.init(a);
+    }
     init(a) {
-        this.provinces = a.length
         for (let i = 0; i < a.length; i++) {
-            this.rank[i] = 1
-            this.root[i] = i
+            this.rank[i] = 1;
+            this.root[i] = i;
         }
+    }
+    isConnected(n1, n2) {
+        return this.find(n1) === this.find(n2)
     }
     find(n) {
         if (n === this.root[n]) return n;
-        return this.root[n] = this.find(this.root[n])
+        return this.root[n] = this.find(this.root[n]) 
     }
     union(n1, n2) {
         let [p1, p2] = [this.find(n1), this.find(n2)]
         if (p1 === p2) return;
-        this.provinces--;
-        
+        this.provinces -= 1;
         if (this.rank[p1] > this.rank[p2]) {
             this.root[p2] = p1;
         } else if (this.rank[p1] < this.rank[p2]) {
             this.root[p1] = p2;
         } else {
             this.root[p2] = p1;
-            this.rank[p1]++
+            this.rank[p1] += 1
         }
     }
 }
 /**
  * Union Find
- * Time and Space: O(V + E)
+ * Time and Space O(V + E)
  */
-var findCircleNum = function(isConnected) {
-    const unionSet = new DisJoinSet(isConnected);
-    
-    for (let i = 0; i < isConnected.length; i++) {
-        for (let j = 0; j < isConnected[i].length; j++) {
-            if (i !== j && isConnected[i][j] === 1) {
-                unionSet.union(i, j)
+var findCircleNum = function(a) {
+    const unionFind = new DisJoinSet(a);
+    for (let i = 0; i < a.length; i++) {
+        for (let j = i; j < a[i].length; j++) {
+            if (a[i][j] === 1) {
+                unionFind.union(i, j)
             }
         }
     }
-    
-    return unionSet.provinces
+    return unionFind.provinces;
 };
