52296a8d9b05062682f2528792ffb53b89230e5e
Daniel Ball <g.thirteen333@gmail.com>
2022-05-28 00:32:13 -0400

    Time: 76 ms (82.42%), Space: 45.3 MB (87.33%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.js
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.js
@@ -1,21 +1,17 @@
 /**
- * DFS
- * Use Recursion
- * Time and Space O(n)
+ * recursion, dfs, max variable outside function
+ * time and space O(n)
  */
-const diameterOfBinaryTree = (root) => {
+var diameterOfBinaryTree = function(root) {
     let max = 0;
-    const diameter = (node) => {
+    let dfs = (node) => {
         if (!node || (!node.left && !node.right)) return 0;
-        let [left, right] = [-1, -1]
-        if (node.left)
-            left = diameter(node.left)
-        if (node.right)
-            right = diameter(node.right)
-
-        max = Math.max(max,left + right + 2)
-        return Math.max(left, right) + 1
+        let [left, right] = [0,0]
+        if (node.left) left = 1 + dfs(node.left)
+        if (node.right) right = 1 + dfs(node.right)
+        max = Math.max(max, left + right)
+        return Math.max(left, right) 
     }
-    diameter(root)
-    return max
-}
+    dfs(root)
+    return max;
+};
