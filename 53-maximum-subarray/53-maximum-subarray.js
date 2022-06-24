3901f6f474ae4b1d7f9afd5e896e141006393681
Daniel Ball <g.thirteen333@gmail.com>
2022-06-24 00:28:08 -0400

    Time: 79 ms (97.78%), Space: 50.4 MB (41.22%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -1,12 +1,8 @@
-// Kadanes algo
-// Time; O(n)
-// Space: O(1)
-
-const maxSubArray = (a) => {
-    let max = a[0];
-    for (let i = 1; i < a.length; i++) {
-        a[i] = Math.max(a[i], a[i] + a[i-1])
-        max = Math.max(max, a[i])
+const maxSubArray = (n) => {
+    let max = n[0]
+    for (let i = 1; i < n.length; i++) {
+        n[i] = Math.max(n[i], n[i] + n[i-1])
+        max = Math.max(max, n[i])
     }
     return max
 }
