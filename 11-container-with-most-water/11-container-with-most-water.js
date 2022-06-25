41a62eaeebbb1cb895a65b92f41f760bb918270c
Daniel Ball <g.thirteen333@gmail.com>
2022-06-25 03:58:27 -0400

    Time: 117 ms (39.92%), Space: 50.3 MB (6.57%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.js
+++ 11-container-with-most-water/11-container-with-most-water.js
@@ -1,16 +1,21 @@
-// two pointer
-// time O(n)
-// space O(1)
-
-const maxArea = (a) => {
-    let [s, e, max] = [0, a.length-1, 0]
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+/**
+ * Two Pointer:
+ * Time: O(n), Space: O(1)
+ */
+var maxArea = function(a) {
+    let max = 0, s = 0, e = a.length-1
     while (s < e) {
-        max = Math.max(max, (e-s) * Math.min(a[s], a[e]))
+        let mx = Math.min(a[s], a[e])
+        max = Math.max(max, mx * (e-s))
         if (a[s] <= a[e]) {
             s++
         } else {
             e--
         }
     }
-    return max;
-}
+    return max
+};
