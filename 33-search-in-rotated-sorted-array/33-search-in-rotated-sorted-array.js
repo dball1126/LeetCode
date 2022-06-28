9442020b2c8172aaa53f8e875b4acb590807b369
Daniel Ball <g.thirteen333@gmail.com>
2022-06-27 21:29:36 -0400

    Time: 132 ms (5.40%), Space: 42.3 MB (30.50%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -5,13 +5,13 @@ const search = (a, t) => {
         if (a[m] === t) return m;
 
         if (a[m] > a[e]) {
-            if (t < a[m] && t >= a[s] && m !== e) {
+            if (t < a[m] && t >= a[s]) {
                 e = m - 1
             } else {
                 s = m + 1
             }
         } else {
-            if (t > a[m] && t <= a[e] && m !== e) {
+            if (t > a[m] && t <= a[e]) {
                 s = m + 1
             } else {
                 e = m - 1
