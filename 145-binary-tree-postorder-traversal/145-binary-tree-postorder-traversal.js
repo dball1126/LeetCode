e8a564d03055d87c7e7f7faae6af0061f3decce1
Daniel Ball <g.thirteen333@gmail.com>
2022-07-02 02:53:12 -0400

    Time: 72 ms (74.07%), Space: 42 MB (69.12%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.js
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.js
@@ -11,6 +11,25 @@
  * @return {number[]}
  */
 const postorderTraversal = (n) => {
-    if (!n) return []
-    return [...postorderTraversal(n.left), ...postorderTraversal(n.right), n.val]
+    if (!n) return [];
+    let curr = n, stack = [], result = [], prev = null
+    while (curr || stack.length) {
+        while (curr) {
+            if (curr.left && prev && curr.left.val === prev.val) {
+                break;
+            }
+            stack.push(curr)
+            curr = curr.left
+        }
+        if (!curr) curr = stack.pop();
+        if (curr.right && !prev || curr.right && prev && prev.val !== curr.right.val ) {
+            stack.push(curr)
+            curr = curr.right
+        } else {
+            result.push(curr.val)
+            prev = curr
+            curr = null
+        }
+    }
+    return result;
 }
