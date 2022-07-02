cba9e5f718efd683a992efa170ac902542de6f52
Daniel Ball <g.thirteen333@gmail.com>
2022-07-02 04:26:36 -0400

    Time: 97 ms (28.13%), Space: 41.7 MB (96.47%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.js
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.js
@@ -10,7 +10,15 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
-var preorderTraversal = function(root) {
-    if (!root) return []
-    return [root.val, ...preorderTraversal(root.left), ...preorderTraversal(root.right)]
-};
+var preorderTraversal = function(n) {
+    if (!n) return []
+    let curr = n, stack = [], result = [], prev = null
+    while (curr || stack.length) {
+        if (!curr) curr = stack.pop();
+        result.push(curr.val)
+        if (curr.right) stack.push(curr.right)
+        if (curr.left) stack.push(curr.left)
+        curr = null;
+    }
+    return result
+}
