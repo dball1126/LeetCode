b2bef0ada947f53d0f231f4370127bc2899a1a23
Daniel Ball <g.thirteen333@gmail.com>
2022-07-02 01:21:51 -0400

    Time: 89 ms (41.28%), Space: 42.6 MB (5.86%) - LeetHub

--- 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.js
+++ 94-binary-tree-inorder-traversal/94-binary-tree-inorder-traversal.js
@@ -11,6 +11,16 @@
  * @return {number[]}
  */
 const inorderTraversal = (n) => {
-    if (!n) return []
-    return [...inorderTraversal(n.left), n.val, ...inorderTraversal(n.right)]
+    if (!n) return [];
+    let curr = n, stack = [], result = []
+    while (curr || stack.length) {
+        while (curr) {
+            stack.push(curr)
+            curr = curr.left
+        }
+        if (!curr) curr = stack.pop();
+        result.push(curr.val)
+        curr = curr.right
+    }
+    return result;
 }
