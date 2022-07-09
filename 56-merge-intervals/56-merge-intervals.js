6324b74fd14a0643848bc62cf4ede3bda2300328
Daniel Ball <g.thirteen333@gmail.com>
2022-07-09 07:13:57 -0400

    Time: 229 ms (11.40%), Space: 50 MB (33.26%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -1,30 +1,25 @@
-/**
- * sort input...by x than y
- * Iterate over input and collect intervals that should be merged
- * Time: N * log(n) (sorting)
- * Space: O(n) new array
- */
-var merge = function(intervals) {
-    intervals.sort((a,b)=> a[0] - b[0])
-    let [min, max, result] = [Infinity, -Infinity, []];
+var merge = function(a) {
+  let result = []
+  if (a.length <= 1) return a;
+  a.sort((a,b)=> a[0] - b[0])
+  let s = a[0][0], e = a[0][1]
 
-    for (let i = 0; i < intervals.length; i++) {
-        const [s, e] = [intervals[i][0], intervals[i][1]]
-        if (min === Infinity && max === -Infinity) {
-            min = s;
-            max = e;
-        } else {
-            if (s <= max) {
-                max = Math.max(max, e)
-            } else {
-                result.push([min, max])
-                min = s
-                max = e
-            }
-        }
+  for (let i = 0; i < a.length; i++) {
+      if (s === undefined) {
+          s = a[i][0]
+          e = a[i][1]
+      } else {
+          if (a[i][0] <= e) {
+              e = Math.max(a[i][1], e)
+          } else {
+              result.push([s,e])
+              s = a[i][0]
+              e = a[i][1]
+          }
+      }
     }
-    if (min !== Infinity && max !== -Infinity) {
-        result.push([min, max])
+    if (s !== undefined) {
+        result.push([s, e])
     }
-    return result;
+    return result
 };
