cd35c27034b3a1b7020b15f94ce0ca400484eb61
Daniel Ball <g.thirteen333@gmail.com>
2022-07-10 00:01:34 -0400

    Time: 136 ms (79.92%), Space: 45.9 MB (57.44%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.js
@@ -1,32 +1,20 @@
+var longestPalindrome = function(str) {
+    if (!str || str.length <= 1) return str;
+    let result = str[0]
 
-/**
- * loop and two pointer, max variable
- * Time: O(n2)
- * Space: O(1)
- */
- var longestPalindrome = function(s) {
-    let max = s[0];
-    const expandAroundCenter = (l, r) => {
-        while (l >= 0 && r <= s.length-1) {
-            if (s[l] === s[r]) {
-                if (max.length < r - l + 1) {
-                    max = s.substring(l, r + 1)
-                }
-                l--;
-                r++;
-            } else {
-                break;
+    const validate = (s, e) => {
+        while (s >= 0 && e < str.length && str[s] === str[e]) {
+            if (result.length < (e - s + 1)) {
+                result = str.substring(s, e + 1)
             }
+            e++
+            s--
         }
     }
-    for (let i = 1; i < s.length; i++) {
-
-        expandAroundCenter(i-1, i+1)
-        if (s[i] === s[i-1]) {
-            if (max.length < 2) max = s[i-1] + s[i] 
-            expandAroundCenter(i-2, i+1)
-        }
+    for (let i = 1; i < str.length; i++) {
+        validate(i-1, i+1)
+        validate(i-1, i)
     }
-    return max;
-}
 
+    return result;
+};
