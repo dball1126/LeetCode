4460527b1d3f09b9e1afb944fdd966cc6f075bf4
Daniel Ball <g.thirteen333@gmail.com>
2022-08-11 07:48:02 -0400

    Time: 184 ms (27.74%), Space: 48.8 MB (23.13%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.js
+++ 76-minimum-window-substring/76-minimum-window-substring.js
@@ -1,41 +1,37 @@
 /**
- * Sliding window, map for t variable, start both pointers at 0
- * Time: O(n + m) n for s, and m for t
- * Space: O(m) m for t
+ * Sliding window:
+ * Time O(n) for s string
+ * Space: O(m) for t string
  */
-var minWindow = function(str, t) {
-    let map = new Map(), count = 0, s = 0, e = 0, result = "";
-
+var minWindow = function(s, t) {
+    let map = new Map();
     for (let i = 0; i < t.length; i++) {
         if (!map.has(t[i])) map.set(t[i], 0)
         map.set(t[i], map.get(t[i]) + 1)
     }
-    count = map.size;
 
-    while (e < str.length) { // slide right pointer 
+    let l = 0, r = 0, end = s.length, count = map.size, result = ""
 
-        if (map.has(str[e])) {
-            map.set(str[e], map.get(str[e]) - 1)
-            if (map.get(str[e]) === 0) count--
+    while (r < end) { // slide right pointer right
+        if (map.has(s[r])) {
+            map.set(s[r], map.get(s[r]) - 1)
+            if (map.get(s[r]) === 0) count--;
         }
         
-        while (count <= 0) { // slide left pointer
-            let sub = str.substring(s, e + 1)
-
-            if (!result) {
-                result = sub
-            } else {
-                if (sub.length < result.length)
-                    result = sub
+        if (count === 0) {
+            while (count <= 0) { // slide left pointer right
+                let str = s.substring(l, r + 1)
+                if (!result || str.length < result.length) {
+                    result = str
+                }
+                if (map.has(s[l])) {
+                    map.set(s[l], map.get(s[l]) + 1)
+                    if (map.get(s[l]) === 1) count++
+                }
+                l += 1
             }
-
-            if (map.has(str[s])) {
-                map.set(str[s], map.get(str[s]) + 1)
-                if (map.get(str[s]) === 1) count++
-            }
-            s++
         }
-        e++
+        r += 1
     }
     return result;
 };
