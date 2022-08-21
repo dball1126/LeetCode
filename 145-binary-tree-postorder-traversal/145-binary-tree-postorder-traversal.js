a0de3bfcaf528cf3ef9000657a54de6ba06f7cfa
Daniel Ball <g.thirteen333@gmail.com>
2022-08-21 10:09:06 -0400

    Time: 76 ms (72.06%), Space: 41.6 MB (96.34%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.js
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.js
@@ -1,35 +1,33 @@
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
+ * 
  */
-/**
- * @param {TreeNode} root
- * @return {number[]}
- */
-const postorderTraversal = (n) => {
-    if (!n) return [];
-    let curr = n, stack = [], result = [], prev = null
+var postorderTraversal = function(root) {
+    if (!root) return []
+    let stack = [], curr = root, result = [], prev = null
     while (curr || stack.length) {
-        while (curr) {
-            if (curr.left && prev && curr.left.val === prev.val) {
-                break;
-            }
-            stack.push(curr)
-            curr = curr.left
-        }
-        if (!curr) curr = stack.pop();
-        if (curr.right && !prev || curr.right && prev && prev.val !== curr.right.val ) {
-            stack.push(curr)
-            curr = curr.right
-        } else {
-            result.push(curr.val)
-            prev = curr
-            curr = null
-        }
+       while (curr) {
+           if (prev && curr.left && prev.val === curr.left.val) break
+           stack.push(curr)
+           if(curr.left) prev = curr
+           curr = curr.left
+       }
+       if (!curr) curr = stack.pop()
+
+       if (curr.right) {
+           if (prev && prev.val === curr.right.val) {
+               result.push(curr.val)
+               prev = curr
+               curr = null
+           } else {
+              stack.push(curr)
+               prev = curr
+               curr = curr.right
+           }
+       } else {
+           result.push(curr.val)
+           prev = curr
+           curr = null
+       }
     }
-    return result;
-}
+    return result 
+};
