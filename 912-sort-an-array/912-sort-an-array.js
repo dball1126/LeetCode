1fde7f4eae0c44ea79fdbed85f74566eb7c33930
Daniel Ball <g.thirteen333@gmail.com>
2022-08-28 03:56:08 -0400

    Time: 196 ms (76.74%), Space: 58.9 MB (48.81%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.js
+++ 912-sort-an-array/912-sort-an-array.js
@@ -1,18 +1,6 @@
-const sortArray = (n) => {
-   if (n.length <= 1) return n
-   let left = [], mid = [], right = []
-   let p = Math.floor((n.length / 2))
- p = n[p]
-   for (let i = 0; i < n.length; i++) {
-       const e = n[i];
-       if (e < p) {
-           left.push(e)
-       } else if (e > p) {
-           right.push(e)
-       } else {
-           mid.push(e)
-       }
-   }
-   
-   return [...sortArray(left), ...mid, ...sortArray(right)]
-}
+/**
+ * Time O(n * log(n))
+ */
+var sortArray = function(nums) {
+   return nums.sort((a,b) => a -b)
+};
