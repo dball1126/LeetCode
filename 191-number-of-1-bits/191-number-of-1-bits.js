542bdeb74d43b545c26da8d87c66b6f790751fc2
Daniel Ball <g.thirteen333@gmail.com>
2022-08-28 01:40:22 -0400

    Time: 146 ms (5.92%), Space: 42.3 MB (80.33%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.js
+++ 191-number-of-1-bits/191-number-of-1-bits.js
@@ -4,9 +4,9 @@
  */
 const hammingWeight = (n) => {
     let count = 0;
-    while (n !== 0) {
-        count++
-        n = n & (n - 1)
+    while ( n !== 0) {
+        count++;
+        n &= (n-1)
     }
-    return count;
+    return count
 }
