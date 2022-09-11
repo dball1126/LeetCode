5f926dfa2a70e47455a4995473053707acfaad65
Daniel Ball <g.thirteen333@gmail.com>
2022-09-11 02:07:09 -0400

    Time: 86 ms (72.76%), Space: 42.3 MB (88.87%) - LeetHub

---
+++ 67-add-binary/67-add-binary.js
@@ -0,0 +1,19 @@
+/**
+ * Time: O(m) size of b
+ * Space: O(1)
+ */
+var addBinary = function(a, b) {
+    a = BigInt(`0b${a}`)
+    b = BigInt(`0b${b}`)
+    var getSum = function(a, b) {
+        if (a === 0n || b === 0n) return a ^ b;
+        while (b !== 0n) {
+            let temp = a;
+            a ^= b
+            b &= temp
+            if (b !== 0n) b <<=1n
+        }   
+        return a; 
+    };
+    return getSum(a, b).toString(2)
+};
