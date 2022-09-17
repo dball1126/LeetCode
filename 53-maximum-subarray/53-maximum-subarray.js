1de3ee11d2e5077e9be331ace629cbde44a775d0
Daniel Ball <g.thirteen333@gmail.com>
2022-09-16 22:48:47 -0400

    Time: 133 ms (41.55%), Space: 50.5 MB (44.30%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -1,7 +1,11 @@
+/**
+* DP, Time: O(n), Space: O(1)
+**/
 const maxSubArray = (n) => {
+    if (n.length === 1) return n[0]
     let max = n[0]
     for (let i = 1; i < n.length; i++) {
-        n[i] = Math.max(n[i], n[i] + n[i-1])
+        n[i] = Math.max(n[i], n[i], n[i] + n[i - 1])
         max = Math.max(max, n[i])
     }
     return max
