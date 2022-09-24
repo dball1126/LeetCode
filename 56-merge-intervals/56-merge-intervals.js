857b36f03bdef7e65f7dd06229dc5e19325eb2d0
Daniel Ball <g.thirteen333@gmail.com>
2022-09-23 23:12:54 -0400

    Time: 108 ms (92.19%), Space: 49.5 MB (46.91%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.js
+++ 56-merge-intervals/56-merge-intervals.js
@@ -1,25 +1,25 @@
-var merge = function(a) {
-  let result = []
-  if (a.length <= 1) return a;
-  a.sort((a,b)=> a[0] - b[0])
-  let s = a[0][0], e = a[0][1]
+/**
+ * Time: O(n * log(n))  (sorting)
+ * Space: O(n)   (output array)
+ */
+var merge = function(n) {
+    if (n.length === 1) return n
+    n.sort((a, b) => a[0] - b[0])
+    let result = [], low = n[0][0], high = n[0][1]
+    for (let i = 1; i < n.length; i++) {
+        let low2 = n[i][0]
+        
+        if (high < low2) {
+            result.push([low, high])
+            low = n[i][0]
+            high = n[i][1]
 
-  for (let i = 0; i < a.length; i++) {
-      if (s === undefined) {
-          s = a[i][0]
-          e = a[i][1]
-      } else {
-          if (a[i][0] <= e) {
-              e = Math.max(a[i][1], e)
-          } else {
-              result.push([s,e])
-              s = a[i][0]
-              e = a[i][1]
-          }
-      }
+        } else {
+            high = Math.max(n[i][1], high)
+        }
+        if (i === n.length-1) {
+            result.push([low, high])
+        }
     }
-    if (s !== undefined) {
-        result.push([s, e])
-    }
-    return result
+    return result;
 };
