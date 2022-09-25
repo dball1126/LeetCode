73d975ad427643d05c076f2813a286e33146bb6d
Daniel Ball <g.thirteen333@gmail.com>
2022-09-25 03:05:41 -0400

    Time: 92 ms (58.44%), Space: 44.7 MB (13.70%) - LeetHub

---
+++ 22-generate-parentheses/22-generate-parentheses.js
@@ -0,0 +1,27 @@
+/**************************************
+ * Time and Space: O(2^n) (count the call stacks...the loop is constant)
+ **************************************/
+var generateParenthesis = function(n) {
+    if (!n) return []
+    let max = 2 * n, str = "()", result = new Set();
+
+    const backtrack = (i, curr, open, close) => {
+        if (close > open) return;
+        if (curr.length >= max && open === close && !result.has(curr)) {
+            return result.add(curr)
+        }
+        if (curr.length >= max || i >= 2) return;
+
+        for (let j = i; j < 2; j++) {
+            let newOpen = open, newClose = close
+            if (str[j] === "(") {
+                newOpen++
+            } else {
+                newClose++
+            }
+            backtrack(0, curr + str[j], newOpen, newClose)
+        }
+    }
+    backtrack(0, "", 0, 0)
+    return Array.from(result)
+};
