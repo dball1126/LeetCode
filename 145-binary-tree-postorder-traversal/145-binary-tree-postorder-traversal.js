1a20023b9ba5490b8fa88c1d783467fc13234dc6
Daniel Ball <g.thirteen333@gmail.com>
2022-10-01 04:43:45 -0400

    Time: 78 ms (72.60%), Space: 42.6 MB (12.12%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.js
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.js
@@ -1,4 +1,21 @@
-var postorderTraversal = function(root) {
-    if (!root) return []
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+/**
+ * DFS
+ * Left, Right, Root
+ * Time and Space: O(n)
+ */
+ var postorderTraversal = function(root) {
+    if (!root) return [];
     return [...postorderTraversal(root.left), ...postorderTraversal(root.right), root.val]
-}
+};
