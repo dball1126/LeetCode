a610f7333dc8d4d58ff49d557a21aedff98e2121
Daniel Ball <g.thirteen333@gmail.com>
2022-10-03 21:55:21 -0400

    Time: 143 ms (39.08%), Space: 47.2 MB (64.60%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.js
+++ 210-course-schedule-ii/210-course-schedule-ii.js
@@ -1,43 +1,39 @@
 /**
- * Topological sort.
- * result = []
- * indegrees = []
- * use Map
- * Time and Space O(V  + E) (2^n - 2)
- * Space: O(m)
+ * Kahns algorithm
+ * Topological Sort
+ * Time & Space: O(V + E)
  */
-
-var findOrder = function(numCourses, prerequisites) {
-    const result = []
-    const queue = []
-    const indegrees = [...new Array(numCourses)].fill(0);
-    const map = new Map();
-
-    for (let i = 0; i < prerequisites.length; i++) {
-        let [v, k] = [prerequisites[i][0], prerequisites[i][1]]
-        if (!map.has(k)) map.set(k, new Set());
-        if (!map.has(v)) map.set(v, new Set());
-        map.get(k).add(v)
+const createPrerequistesMap = (m, a, indegrees) => {
+    for (let i = 0; i < a.length; i++) {
+        let v = a[i][0], k = a[i][1]
+        m.get(k).add(v)
         indegrees[v]++
     }
-    indegrees.forEach((v, i) => v === 0 ? queue.push(i) : undefined)
-    
-    while(queue.length) {
-        let c = queue.shift();
-        
-        if (indegrees[c] === 0) result.push(c);
-        if (result.length === numCourses) return result;
+}
+var findOrder = function(n, p) {
+    let pMap = new Map(), indegrees = new Array(n).fill(0)
+    for (let i = 0; i < n; i++) {
+        pMap.set(i, new Set())
+    }
+    createPrerequistesMap(pMap, p, indegrees)
+    let result = [], q = [], visited = new Set()
+    for (let i = 0; i < n; i++) {
+        if (indegrees[i] === 0) q.push(i)
+    }
+    while (q.length) {
+        let n = q.shift();
+        if (visited.has(n)) continue
+        if (indegrees[n] <= 0) {
+            result.push(n)
+            visited.add(n)
+        }
+        let arr = Array.from(pMap.get(n))
 
-        if (map.has(c)) {
-            for (let i = 0; i < Array.from(map.get(c)).length; i++) {
-                const v = Array.from(map.get(c))[i];
-                if (map.has(v) && map.get(v).has(c)) return []; // detect cycle
-
-                indegrees[v]--
-                if (indegrees[v] === 0) queue.push(v)
-            }
+        for (let i = 0; i < arr.length; i++) {
+            if (pMap.get(arr[i]).has(n)) return []
+            indegrees[arr[i]]-=1
+                if (indegrees[arr[i]] <= 0) q.push(arr[i])
         }
     }
-    if (result.length === numCourses) return result;
-    return []
-}
+    return result.length === n ? result : [];
+};
