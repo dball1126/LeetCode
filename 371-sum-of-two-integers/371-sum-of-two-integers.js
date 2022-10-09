867515061be7f99d382c045905bbbeb3d29514f9
Daniel Ball <g.thirteen333@gmail.com>
2022-10-09 11:25:00 -0400

    Time: 103 ms (20.92%), Space: 41.6 MB (82.67%) - LeetHub

--- 371-sum-of-two-integers/371-sum-of-two-integers.js
+++ 371-sum-of-two-integers/371-sum-of-two-integers.js
@@ -1,14 +1,13 @@
 /**
- * Time: O(1) -1000 and 1000 only have 32 bits
- * Space: O(1)
+ * O(1)
  */
-var getSum = function(a, b) {
-    if (a === 0 || b === 0) return a ^ b;
-    while (b !== 0) {
-        let temp = a;
-        a ^= b
-        b &= temp
-        if (b !== 0) b <<=1
-    }   
-    return a; 
+var getSum = function(n1, n2) {
+
+    while (n2 !== 0) {
+      let temp = n1
+        n1 ^= n2
+        n2 &= temp
+        if (n2 !== 0) n2 <<= 1
+    }
+    return n1 | n2
 };
