a382e3b097322049135a554898cb63aa4b7328c1
Daniel Ball <g.thirteen333@gmail.com>
2022-10-10 02:46:13 -0400

    Time: 169 ms (30.83%), Space: 44.2 MB (40.28%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.js
+++ 29-divide-two-integers/29-divide-two-integers.js
@@ -1,35 +1,29 @@
-var divide = function(dividend, divisor) {
+var divide = function(a, b) {
     let isNegative = false;
-    if (dividend < 0 && divisor > 0 || dividend > 0 && divisor < 0) {
-        isNegative = true;
-    }
-    divisor = Math.abs(divisor)
-    dividend = Math.abs(dividend)
-    let result = 0;
-
-    const isResultTooBig = () => {
-        if (result > 2**31) {
-            if (isNegative) result = -result
-            if (result > 2**31 -1) return 2**31 - 1
-            if (result < (-(2**31)) ) return -(2**31)
+    if (a === 0 || b === 0) return 0;
+    if (a < 0 && b > 0 || a > 0 && b < 0) isNegative = true;
+    a = Math.abs(a)
+    b = Math.abs(b)
+    let count = 0, max = (2 ** 31) -1
+    while (a >= b) {
+        let tCount = 1, tempB = b, tempA = a
+        while (((tempB << 1) <= tempA) && ((tempB << 1) > tempB)) {
+            tempB <<= 1
+            tCount <<= 1
+            tempA >>= 1
+            if (tempB === 0) {
+                if (isNegative) return -(2 ** 31)
+                return max
+            }
         }
+        count += tCount
+        a -= tempB
     }
-
-    while (dividend >= divisor) {
-        let tempD = dividend, tempDivide = divisor, count = 1
-
-        while ((tempD >> 1) >= tempDivide) {
-            count <<= 1
-            tempD = tempD >> 1
-            tempDivide = tempDivide << 1
-        }
-        result += count;
-        dividend -= tempDivide
-        isResultTooBig();
+    if (!isNegative) {
+        if (count > max) return max
+        return count;
+    } else {
+        if (count > 2 ** 31) return - (2 ** 31)
+        return -(count)
     }
-    if (isNegative) result = -result
-
-    if (result > (2**31 -1)) return 2**31 - 1
-    if (result < (-(2**31)) ) return -(2**31)
-    return result
 };
