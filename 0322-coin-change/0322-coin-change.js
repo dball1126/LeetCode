cda03ea4d4f4dd7e2ce5fc280c30c27761b84c63
Daniel Ball <g.thirteen333@gmail.com>
2022-10-14 23:57:17 -0400

    Time: 314 ms (25.85%), Space: 47.9 MB (49.48%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.js
@@ -0,0 +1,28 @@
+/**
+ * Dynamic Programming
+ * Time and space: O(n)
+ */
+var coinChange = function(coins, amount) {
+    if (amount === 0) return 0
+    let memo = new Array(amount + 1).fill(undefined)
+    memo[0] = 0
+    const dp = (idx, sum) => {
+        if (sum === 0) return 0
+        if (memo[sum]) return memo[sum]
+        let value = Infinity
+        for (let i = idx; i < coins.length; i++) {
+            let val = sum - coins[i]
+            if (val >= 0 && val < sum) {
+                let nextVal = dp(0, val)
+                if (nextVal !== undefined && nextVal !== -1) {
+                    value = Math.min(value, nextVal + 1)
+                }
+            }
+        }
+        if ( value === Infinity) value = -1
+        memo[sum] = value
+        return memo[sum]
+    }
+    dp(0, amount)
+    return memo[amount]
+};
