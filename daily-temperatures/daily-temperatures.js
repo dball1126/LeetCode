9928fbda37d549e5f0bb7e0e53a05aab865d22fe
Daniel Ball <g.thirteen333@gmail.com>
2023-07-01 00:51:59 -0400

    Time: 328 ms (42.04%) | Memory: 65.9 MB (77.33%) - LeetSync

---
+++ daily-temperatures/daily-temperatures.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} temperatures
+ * @return {number[]}
+ */
+// Stack
+// Time and Space: O(n)
+var dailyTemperatures = function(temps) {
+    let stack = []
+    let result = temps.map(a => 0)
+    for (let i = 0; i < temps.length; i++) {
+        while (stack.length && temps[stack[stack.length-1]] < temps[i]) {
+            let idx = stack.pop()
+            result[idx] = i - idx
+        }
+        stack.push(i)
+    }
+    return result 
+};
