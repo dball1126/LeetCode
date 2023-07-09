cec594f2658b312f935d21645e19f893c533f587
Daniel Ball <g.thirteen333@gmail.com>
2023-07-08 23:42:16 -0400

    Time: 54 ms (81.57%) | Memory: 42.5 MB (18.06%) - LeetSync

--- binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
+++ binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
@@ -11,6 +11,15 @@
  * @return {number[]}
  */
 var inorderTraversal = function(root) {
-    if (!root) return []
-    return [...inorderTraversal(root.left), root.val, ...inorderTraversal(root.right)]    
+    let stack = [], curr = root, inorder = []
+    while (curr || stack.length) {
+        while (curr && curr.left) {
+            stack.push(curr)
+            curr = curr.left
+        }
+        if (!curr) curr = stack.pop()
+        inorder.push(curr.val)
+        curr = curr.right
+    }
+    return inorder
 };
