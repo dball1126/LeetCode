2111c10aae3b1a15f2ce09dd08b9c50c3de1b42d
Daniel Ball <g.thirteen333@gmail.com>
2023-07-09 04:40:30 -0400

    Time: 563 ms (73.08%) | Memory: 44.9 MB (76.92%) - LeetSync

---
+++ the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var beautifulSubsets = function(nums, k) {
+    let result = 0, len = nums.length
+    
+    const backtrack = (curr, idx) => {
+        if (curr.length) result+=1
+        if (idx >= len) return
+        for (let i = idx; i < nums.length; i++) {
+            let find = curr.find(v => Math.abs(v - nums[i]) === k)
+            if (!find) {
+                curr.push(nums[i])
+                backtrack(curr, i+1)
+                curr.pop();
+            }
+        }
+    }
+    backtrack([], 0)
+    return result
+};
