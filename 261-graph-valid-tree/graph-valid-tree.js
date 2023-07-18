9fb8bd89354bdab140b94a8bd5c573f2d2041d36
Daniel Ball <g.thirteen333@gmail.com>
2023-07-17 21:54:14 -0400

    Time: 80 ms (34.19%) | Memory: 45.7 MB (55.13%) - LeetSync

---
+++ 261-graph-valid-tree/graph-valid-tree.js
@@ -0,0 +1,28 @@
+// Union Find with path compression
+// Time and Space: O(V + E)...v for vertices(n) and e for edges
+// Find operation is considered amortized time...which is constant
+var validTree = function(n, edges) {
+    let root = new Array(n), rank = new Array(n), connected = 0
+    for (let i = 0; i < n; i++) {root[i] = i; rank[i] = 1;}
+
+    const find = (nde) => root[nde] === nde ? nde : find(root[nde])
+
+    const union = (n1, n2) => {
+        n1 = find(n1); n2 = find(n2);
+        if (n1 === n2) return true;
+        connected++
+        if (rank[n1] > rank[n2]) {
+            root[n2] = n1
+        } else if (rank[n2] > rank[n1]) {
+            root[n1] = n2
+        } else {
+            rank[n1]++
+            root[n2] = n1
+        }
+        return false;
+    } 
+    for (let [x, y] of edges) {
+        if (union(x, y)) return false;
+    }
+    return connected === n-1
+};
