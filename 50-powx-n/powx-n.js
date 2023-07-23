df6ab4b24a1e62818962a020cc258bbe49438e18
Daniel Ball <g.thirteen333@gmail.com>
2023-07-23 04:27:44 -0400

    Time: 7710 ms (5.03%) | Memory: 42.4 MB (22.30%) - LeetSync

---
+++ 50-powx-n/powx-n.js
@@ -0,0 +1,18 @@
+var myPow = function(x, n) {
+    if (n === 0) return 1;
+    if (n < 0) {x = 1/x; n = -1 * n}
+    if (n === 1) return x;
+    let result = x, count = 1
+    n -= 1
+
+        while (n > 0) {
+            if ( n >= count) {
+                n -= count; count += count;
+                result *= result;
+            } else {
+                n -=1; count +=1;
+                result *= x
+            }
+        }
+        return result;
+};
