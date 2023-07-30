fc1bb3a3bc9d14b2956cf3570164b056f8cad1a9
Daniel Ball <g.thirteen333@gmail.com>
2023-07-29 20:19:04 -0400

    Time: 66 ms (72.21%) | Memory: 45.4 MB (9.96%) - LeetSync

---
+++ 90-subsets-ii/subsets-ii.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function(nums) {
+    nums.sort((a, b) => a - b)
+    const result = [], resultSet = new Set();
+    const backtrack =  (curr, idx) => {
+        let copy = [], copyStr = ""
+        curr.forEach(v => {
+            copyStr += v;
+            copy.push(v)
+        })
+        if (!resultSet.has(copyStr)) {
+            resultSet.add(copyStr);
+            result.push(copy);
+        }
+        if (idx >= nums.length) return;
+        for (let i = idx; i < nums.length; i++) {
+            curr.push(nums[i])
+            backtrack(curr, i + 1)
+            curr.pop();
+        }
+    }
+    backtrack([], 0)
+    return result
+};
