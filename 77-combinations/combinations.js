b5b80fb94e58a0d25147da6942f9f8aba3fccb9c
Daniel Ball <g.thirteen333@gmail.com>
2023-07-30 02:54:45 -0400

    Time: 95 ms (94.09%) | Memory: 48.3 MB (50.57%) - LeetSync

---
+++ 77-combinations/combinations.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number[][]}
+ */
+// Time: O(n * (2^k))
+// Space: O(k)
+var combine = function(n, k) {
+    const result = []
+    const backtrack = (idx, curr) => {
+        if (curr.length === k) return result.push([...curr]);
+        if (idx > n) return;
+
+        for (let i = idx; i <= n; i++) {
+            curr.push(i);
+            backtrack(i+1, curr);
+            curr.pop();
+        }
+
+    }
+    backtrack(1, [])
+    return result;
+};
