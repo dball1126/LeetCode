2482809e35dba92f8e323119782b79f9c4e682e5
Daniel Ball <g.thirteen333@gmail.com>
2023-07-30 05:24:44 -0400

    Time: 46 ms (97.61%) | Memory: 42 MB (51.59%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+// Time: O(4^n)...n is digits length and 4 is the numbers
+// Space: O(n)
+var letterCombinations = function(digits) {
+    if (!digits) return []
+    const result = []
+
+    const backtrack = (idx1, idx2, curr) => {
+        if (curr.length === digits.length) {
+            return result.push(curr)
+        }
+        if (idx1 >= digits.length) return;
+        let phoneMap = new Map([["2", "abc"],["3","def"],["4","ghi"],["5","jkl"],["6","mno"]
+        ,["7","pqrs"],["8","tuv"],["9","wxyz"]]);
+        let arr = phoneMap.get(digits[idx1])
+        if (idx2 >= arr.length) return;
+
+        for (let i = idx2; i < arr.length; i++) {
+            let val = arr[i]
+
+            backtrack(idx1+1, idx2, curr + val)
+        }
+    }
+    backtrack(0,0,"")
+    return result;
+};
