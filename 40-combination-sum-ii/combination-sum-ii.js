0a92f18cbf1c5b2218aeca01c6a5c74edad2a808
Daniel Ball <g.thirteen333@gmail.com>
2023-08-04 00:08:34 -0400

    Time: 61 ms (88.49%) | Memory: 44.6 MB (38.85%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+
+// Time: O((n ^t) * n)
+// Space: O(n)
+var combinationSum2 = function(nums, tgt) {
+    let result = [], set = new Set()
+    nums.sort((a, b) => a - b)
+    
+    const backtracking = (idx, curr, sum) => {
+
+        if (sum === tgt) {
+            let copy = [], copyStr = ''
+            curr.forEach(n => {copy.push(n); copyStr += n})
+            if (!set.has(copyStr)) {result.push(copy); set.add(copyStr)}
+            return;
+        }
+        if (idx >= nums.length) return
+
+        for (let i = idx; i < nums.length; i++) {
+            if ((sum + nums[i]) <= tgt) {
+                curr.push(nums[i])
+                backtracking(i+1, curr, sum + nums[i])
+                curr.pop();
+                while (i+1 < nums.length && nums[i+1] === nums[i]) i++
+            }
+        }
+    }
+    backtracking(0, [], 0)
+    return result;
+};
