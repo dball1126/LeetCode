fa1f538456ed1d56073cdaa72b4b99b3423bfd94
Daniel Ball <g.thirteen333@gmail.com>
2023-08-05 21:49:40 -0400

    Time: 63 ms (78.23%) | Memory: 45.6 MB (8.58%) - LeetSync

--- 90-subsets-ii/subsets-ii.js
+++ 90-subsets-ii/subsets-ii.js
@@ -1,25 +1,27 @@
-// Time: O((n^2) * n)...sorting is dominated
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+// Time: O((2^n) * n)
 // Space: O(n)
 var subsetsWithDup = function(nums) {
+    const result = [], numSet = new Set();
     nums.sort((a, b) => a - b)
-    const result = [], resultSet = new Set();
-    const backtrack =  (curr, idx) => {
+    const backtrack = (idx, curr) => {
         let copy = [], copyStr = ""
-        curr.forEach(v => {
-            copyStr += v;
-            copy.push(v)
-        })
-        if (!resultSet.has(copyStr)) {
-            resultSet.add(copyStr);
-            result.push(copy);
-        }
-        if (idx >= nums.length) return;
+        curr.forEach(v => {copy.push(v); copyStr += v})
+        if (!numSet.has(copyStr)) result.push(copy)
+        if (idx >= nums.length) return
+
         for (let i = idx; i < nums.length; i++) {
             curr.push(nums[i])
-            backtrack(curr, i + 1)
-            curr.pop();
+            backtrack(i+1, curr)
+            curr.pop()
+            while (i+1 < nums.length && nums[i] === nums[i+1]){
+                i++
+            }
         }
     }
-    backtrack([], 0)
+    backtrack(0, [])
     return result
 };
