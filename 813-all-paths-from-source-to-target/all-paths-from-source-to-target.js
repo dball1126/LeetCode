cc0c754e01e6bdfea04672f04b19da8463fb12f4
Daniel Ball <g.thirteen333@gmail.com>
2023-08-19 22:57:19 -0400

    Time: 82 ms (89.04%) | Memory: 52.2 MB (23.26%) - LeetSync

---
+++ 813-all-paths-from-source-to-target/all-paths-from-source-to-target.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[][]} graph
+ * @return {number[][]}
+ */
+// Time and Space: O(V * E)
+var allPathsSourceTarget = function(graph) {
+    if (!graph.length) return []
+    const paths = [], queue = [[[0]]]
+    let len = graph.length-1
+    while (queue.length) {
+        let levels = queue.shift();
+        const newLevel = []
+        for (let level of levels) {
+            const v = level[level.length-1]
+            if (v === len) {
+                paths.push(level);
+                continue;
+            }
+            const arr = graph[v]
+            if (arr) {
+                for (let edge of arr) {
+                    newLevel.push([...level, edge])
+                }
+            }
+        }
+        if (newLevel.length) queue.push(newLevel)
+    }
+    return paths;
+};
