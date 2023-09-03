9436f6534b2747aa3fb6ff6e7d09e4814b611041
Daniel Ball <g.thirteen333@gmail.com>
2023-09-03 02:47:00 -0400

    Time: 46 ms (93.02%) | Memory: 43.8 MB (26.94%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.js
+++ 22-generate-parentheses/generate-parentheses.js
@@ -2,18 +2,21 @@
  * @param {number} n
  * @return {string[]}
  */
+// Backtracking
+// Time: O(4^n)
+// Space: O(n*2)  = O(n)
 var generateParenthesis = function(n) {
     let result = []
 
     const backtrack = (o, c, curr) => {
         if (o > n || c > n || c > o) return;
         if (o === n && c === n) return result.push(curr)
-
         backtrack(o+1, c, curr + "(")
-
         backtrack(o, c +1, curr + ")")
     }
     backtrack(0, 0, "")
-
     return result
 };
+console.log(
+generateParenthesis(3))
+///[ '((()))', '(()())', '(())()', '()(())', '()()()' ]
