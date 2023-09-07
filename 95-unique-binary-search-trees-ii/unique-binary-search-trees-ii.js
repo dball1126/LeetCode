75b1961807fed743d74381ac5b6812a996752487
Daniel Ball <g.thirteen333@gmail.com>
2023-09-06 21:28:11 -0400

    Time: 74 ms (55.95%) | Memory: 48 MB (71.65%) - LeetSync

---
+++ 95-unique-binary-search-trees-ii/unique-binary-search-trees-ii.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {number} n
+ * @return {TreeNode[]}
+ */
+// Dynamic prorgramming / Backtracking
+// Time: O(n*2)
+// Space: O(n)
+var generateTrees = function(n) {
+    const memo = new Map()    
+    const generate = (s, e) => {
+        if (e < s) return [null]
+        if (s === e) return [new TreeNode(s)]
+        if (memo.has(s + "" + e)) return [...memo.get(s + "" + e)]
+        const allCombos = []
+        for (let i = s; i <= e; i++) {
+            let left = generate(s, i-1)
+            let right = generate(i+1, e)
+            for(let l of left) {
+                for (let r of right) {
+                    allCombos.push(new TreeNode(i, l, r))
+                }
+            }
+        }
+        memo.set(s + "" + e, allCombos)
+        return allCombos
+    }
+    return generate(1, n)
+};
