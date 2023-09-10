8581fe8a01ef105dff56c37e3cc3b4439249773c
Daniel Ball <g.thirteen333@gmail.com>
2023-09-10 01:19:35 -0400

    Time: 66 ms (82.81%) | Memory: 46.4 MB (84.23%) - LeetSync

--- 207-course-schedule/course-schedule.js
+++ 207-course-schedule/course-schedule.js
@@ -3,30 +3,26 @@
  * @param {number[][]} prerequisites
  * @return {boolean}
  */
+// Topological Sort
+// Time & Space: O(V + E)
 var canFinish = function(numCourses, prerequisites) {
     const indegrees = new Array(numCourses).fill(0)
     let adjList = new Map(), queue = [], count = 0, visited = new Set()
 
     for(let [x, y] of prerequisites) {
-        // if (!adjList.has(y)) adjList.set(y, new Set())
         if (!adjList.has(x)) adjList.set(x, new Set())
         indegrees[y] = indegrees[y] + 1
-        // adjList.get(y).add(x)
         adjList.get(x).add(y)
     }
     indegrees.forEach((n, i) => n === 0 ? queue.push(i) : '')
     while (queue.length) {
         let nde = queue.shift()
-        if (visited.has(nde)) {
-            return false
-        }
+        if (visited.has(nde)) return false
         count += 1
         if (numCourses === count) return true
         visited.add(nde)
         if (adjList.has(nde)) {
-            let set = adjList.get(nde)
-
-            let arr = Array.from(set)
+            let arr = Array.from(adjList.get(nde))
             arr.forEach(n => {
                 indegrees[n] = indegrees[n] - 1
                 if (indegrees[n] === 0) {
