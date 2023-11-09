cec3175b30fefb5270843f652aa4d3819fee5145
Daniel Ball <g.thirteen333@gmail.com>
2023-11-09 08:44:38 -0500

    Time: 74 ms (16.82%) | Memory: 46.3 MB (11.22%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.js
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.js
@@ -1,36 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} target
+ * @param {number} k
+ * @return {number[]}
+ */
+// Adjaceny List // Graph
+// Breadth-First-Search
+// Time and Space: O(n)...n for nodes
 var distanceK = function(root, target, k) {
     let adjList = new Map(), nodes = [], visited = new Set()
 
     const buildList = (nde) => {
         if (!nde) return
         if (!adjList.has(nde.val)) adjList.set(nde.val, new Set())
-
         if (nde.left) {
             if (!adjList.has(nde.left.val)) adjList.set(nde.left.val, new Set())
             adjList.get(nde.left.val).add(nde.val)
             adjList.get(nde.val).add(nde.left.val)
             buildList(nde.left)
         }
-
         if (nde.right) {
             if (!adjList.has(nde.right.val)) adjList.set(nde.right.val, new Set())
             adjList.get(nde.right.val).add(nde.val)
             adjList.get(nde.val).add(nde.right.val)
             buildList(nde.right)
         }
-
     }
     buildList(root) 
     if (adjList.has(target.val)) {
         const queue = [[target.val, 0]]
-
         while (queue.length) {
             let [val, dist] = queue.shift()
             visited.add(val)
-            
-            if (dist === k) {
-                nodes.push(val)
-            }
+            if (dist === k) nodes.push(val)
             if (dist+1 <= k) {
                 Array.from(adjList.get(val)).forEach(n => {
                     if (!visited.has(n)) {
@@ -41,6 +50,5 @@ var distanceK = function(root, target, k) {
             }
         }
     }
-
     return nodes;
 };
