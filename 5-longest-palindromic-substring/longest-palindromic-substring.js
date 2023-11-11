77c6f6683214a46e6af9e30b9a3d761a68631e02
Daniel Ball <g.thirteen333@gmail.com>
2023-11-11 03:29:14 -0500

    Time: 95 ms (51.68%) | Memory: 44.9 MB (63.60%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -2,10 +2,8 @@
 // Time and Space: O(n)
 var longestPalindrome = function(str) {
     let n = str.length, max = [0,0], longest = ""
-    const memo = [...new Array(n+1)].map(a => [...new Array(n+1)])
     const dp = (i, j) => {
         if (i < 0 || j >= n) return 0
-        if (memo[i][j] !== undefined) return memo[i][j]
 
         let v = i === j ? 1 : 0
         if (str[i] === str[j]) {
@@ -13,7 +11,7 @@ var longestPalindrome = function(str) {
             v = v + dp(i-1, j+1)
             if (j - i >= (max[1] - max[0])) max = [i, j]
         }
-        return memo[i][j] = v
+        return  v
     }
     for (let i = 0; i < n; i++) {
         dp(i, i)
