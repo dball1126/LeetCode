2787e4980e4272032a311489891d25db830780eb
Daniel Ball <g.thirteen333@gmail.com>
2023-11-19 01:41:44 -0500

    Time: 61 ms (45.55%) | Memory: 43.9 MB (65.78%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.js
@@ -0,0 +1,18 @@
+// Time: O(n)
+// Space: O(1)
+var jump = function(nums) {
+    let curr = nums[0], n = nums.length, jumps = 0;
+    if (n <= 1) return jumps
+    for (let i = 0; i < n; i++) {
+        curr = Math.max(curr, nums[i])
+        nums[i] = curr
+        curr -=1
+    }
+
+    for (let i = 0; i < n; i++) {
+        jumps +=1
+        if (i + nums[i] >= n-1) return jumps
+        i = i + nums[i]-1
+    }
+    return jumps
+};
