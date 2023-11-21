2df098e26477253968c5976f0915773b5fef74c6
Daniel Ball <g.thirteen333@gmail.com>
2023-11-21 00:54:26 -0500

    Time: 3607 ms (9.09%) | Memory: 51.2 MB (71.21%) - LeetSync

---
+++ 2001-jump-game-vii/jump-game-vii.js
@@ -0,0 +1,29 @@
+/**
+ * @param {string} s
+ * @param {number} minJump
+ * @param {number} maxJump
+ * @return {boolean}
+ */
+var canReach = function(s, minJump, maxJump) {
+    let queue = [0], n = s.length, min = 0
+
+    while (queue.length) {
+        let i = queue.shift();
+
+        if (i === n-1) return true;
+        let newMin = Math.min(i + maxJump, n-1)
+        if ((i + minJump) <= newMin) {
+ 
+            if (newMin <= min) continue;
+            let begin = Math.max(min+1, i + minJump)
+
+            for (let j = begin; j <= newMin; j++) {
+                // console.log("J" + j)
+                if (s[j] === '0') queue.push(j)
+            }
+            min = newMin
+        }
+    }
+
+    return false;
+};
