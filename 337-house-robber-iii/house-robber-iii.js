673cd0164685081dee1799217ea15a175277855b
Daniel Ball <g.thirteen333@gmail.com>
2023-11-24 23:07:43 -0500

    Time: 54 ms (96.40%) | Memory: 46.8 MB (71.22%) - LeetSync

---
+++ 337-house-robber-iii/house-robber-iii.js
@@ -0,0 +1,23 @@
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var rob = function(root) {
+    const dfs = (nde) => {
+        if (!nde) return [0, 0]
+        if (!nde.left && !nde.right) {
+            return [nde.val, 0]
+        }
+
+        const [p1, pp1] = dfs(nde.left)
+        const [p2, pp2] = dfs(nde.right)
+
+        const prev = pp1 + pp2 + nde.val
+        const prevprev = Math.max(p1+p2, p1+pp2, p2 + pp1, pp1+pp2)
+        return [prev, prevprev]
+    }
+
+    const [prev, prevprev] = dfs(root)
+
+    return Math.max(prev, prevprev)
+};
