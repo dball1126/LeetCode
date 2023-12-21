fcca8f1c82c191af56cc26bd546dce8910de5da8
Daniel Ball <g.thirteen333@gmail.com>
2023-12-20 22:05:05 -0500

    Time: 313 ms (44.69%) | Memory: 45.1 MB (29.20%) - LeetSync

---
+++ 3210-count-beautiful-substrings-i/count-beautiful-substrings-i.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+// Time: O(n^2)
+// Space: O(1)...two variables and vowels size is 5
+var beautifulSubstrings = function(s, k) {
+    let total = 0, v = 0, c = 0, vowels = new Set(['a','e','i','o','u']), n = s.length
+    
+    for (let i = n-1; i >= 0; i--) {
+        vowels.has(s[i]) ? v++ : c++
+        let copyV = v, copyC = c;
+
+        for (let j = n-1; j >= i+1; j--) {
+            if (copyV === copyC && (copyV * copyC) % k === 0) total++
+            vowels.has(s[j]) ? copyV-- : copyC--
+        }
+    }
+    return total;
+};
