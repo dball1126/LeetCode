ccc2b27fe95d4a76683d3de0c03d53d22674dcd0
Daniel Ball <g.thirteen333@gmail.com>
2024-01-04 18:27:48 -0500

    Time: 234 ms (95.63%) | Memory: 90.5 MB (78.38%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.js
+++ 239-sliding-window-maximum/sliding-window-maximum.js
@@ -7,20 +7,20 @@
 // Time: O(n)...since elements can be at most checked twice.
 // Space: O(k)...for the stack length
 var maxSlidingWindow = function(nums, k) {
-    let result = [], qeueue = [], s = 0, n = nums.length
+    let result = [], queue = [], s = 0, n = nums.length
     let e = 0
 
     while (e < n) {
 
-        while (qeueue.length && nums[qeueue[qeueue.length-1]] <= nums[e]) {
-            qeueue.pop();
+        while (queue.length && nums[queue[queue.length-1]] <= nums[e]) {
+            queue.pop();
         }
-        qeueue.push(e)
+        queue.push(e)
 
         if ((e - s) + 1 === k) {
-            result.push(nums[qeueue[0]])
-            if (qeueue[0] === s) {
-                qeueue.shift()
+            result.push(nums[queue[0]])
+            if (queue[0] === s) {
+                queue.shift()
             }
             s++
         }
