0a3d8a2a495c9e473c8646abf9352829ec5a4922
Daniel Ball <g.thirteen333@gmail.com>
2024-01-04 18:17:18 -0500

    Time: 268 ms (83.28%) | Memory: 91.5 MB (51.86%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.js
+++ 239-sliding-window-maximum/sliding-window-maximum.js
@@ -3,26 +3,28 @@
  * @param {number} k
  * @return {number[]}
  */
-// Sliding Window & STACK
-// Time: O(n)
-// Space: O(k)
+// Sliding Window + Stack
+// Time: O(n)...since elements can be at most checked twice.
+// Space: O(k)...for the stack length
 var maxSlidingWindow = function(nums, k) {
-    let result = [], s = 0, e = 0, n = nums.length, stack = []
-    while (e < n) { // slide right pointer right
+    let result = [], stack = [], s = 0, n = nums.length
+    let e = 0
 
-        while(e < n && (e-s) < k) {
-            while (stack.length && nums[stack[stack.length-1]] < nums[e]) {
-                stack.pop();
+    while (e < n) {
+
+        while (stack.length && nums[stack[stack.length-1]] <= nums[e]) {
+            stack.pop();
+        }
+        stack.push(e)
+
+        if ((e - s) + 1 === k) {
+            result.push(nums[stack[0]])
+            if (stack[0] === s) {
+                stack.shift()
             }
-            stack.push(e)
-            e++
+            s++
         }
-        result.push(nums[stack[0]])
-
-        while (stack.length && stack[0] <= s) { // slide left pointer right
-            stack.shift()
-        }
-        s++
+        e++
     }
     return result
 };
