d8f4bf28ef25693fb011057d7e2c76b9d1c79bbe
Daniel Ball <g.thirteen333@gmail.com>
2024-01-04 18:27:01 -0500

    Time: 238 ms (93.50%) | Memory: 90.7 MB (73.80%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.js
+++ 239-sliding-window-maximum/sliding-window-maximum.js
@@ -3,24 +3,24 @@
  * @param {number} k
  * @return {number[]}
  */
-// Sliding Window + Stack
+// Sliding Window + Monotonic Deque
 // Time: O(n)...since elements can be at most checked twice.
 // Space: O(k)...for the stack length
 var maxSlidingWindow = function(nums, k) {
-    let result = [], stack = [], s = 0, n = nums.length
+    let result = [], qeueue = [], s = 0, n = nums.length
     let e = 0
 
     while (e < n) {
 
-        while (stack.length && nums[stack[stack.length-1]] <= nums[e]) {
-            stack.pop();
+        while (qeueue.length && nums[qeueue[qeueue.length-1]] <= nums[e]) {
+            qeueue.pop();
         }
-        stack.push(e)
+        qeueue.push(e)
 
         if ((e - s) + 1 === k) {
-            result.push(nums[stack[0]])
-            if (stack[0] === s) {
-                stack.shift()
+            result.push(nums[qeueue[0]])
+            if (qeueue[0] === s) {
+                qeueue.shift()
             }
             s++
         }
