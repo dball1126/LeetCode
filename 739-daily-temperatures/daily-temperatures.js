55ff87381ea176ef0d7350aaf28d8f6922c0bb1c
Daniel Ball <g.thirteen333@gmail.com>
2024-01-04 19:44:41 -0500

    Time: 250 ms (35.73%) | Memory: 66.4 MB (55.51%) - LeetSync

--- 739-daily-temperatures/daily-temperatures.js
+++ 739-daily-temperatures/daily-temperatures.js
@@ -2,16 +2,23 @@
  * @param {number[]} temperatures
  * @return {number[]}
  */
-// Time and Space: O(n)
-var dailyTemperatures = function(temps) {
-    let stack = []
-    let result = temps.map(a => 0)
-    for (let i = 0; i < temps.length; i++) {
-        while (stack.length && temps[stack[stack.length-1]] < temps[i]) {
-            let idx = stack.pop()
-            result[idx] = i - idx
-        }
-        stack.push(i)
+// Monotonic stack
+// Time & Space: O(n)
+var dailyTemperatures = function(temperatures) {
+    const n  = temperatures.length;
+    let result = temperatures.map(a => a), stack = [n-1]
+    result[n-1] = 0
+
+    for (let i = n-2; i >= 0; i--) {
+       while (stack.length && temperatures[stack[stack.length-1]] <= temperatures[i]) {
+        stack.pop()
+       }
+       if (!stack.length) {
+        result[i] = 0
+       } else {
+       result[i] = stack[stack.length-1] - i
+       }
+       stack.push(i)
     }
-    return result 
+    return result
 };
