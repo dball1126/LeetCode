132fe4ac105fefd33405131fce34f2f98f91c1ab
Daniel Ball <g.thirteen333@gmail.com>
2024-01-10 01:39:04 -0500

    Time: 57 ms (30.54%) | Memory: 41.6 MB (70.14%) - LeetSync

---
+++ 1514-minimum-value-to-get-positive-step-by-step-sum/minimum-value-to-get-positive-step-by-step-sum.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+// Dynamic Programming
+// Time: O(n)
+// Space: O(1)
+var minStartValue = function (nums) {
+    let count = 1, prev = 1, n = nums.length
+    
+    for (let i = 0; i < n; i++) {
+        prev += nums[i]
+        if (prev < 1) {
+            let diff = Math.abs(prev) + 1
+            count += diff
+            prev += diff
+        }
+    }
+    return count;
+};
