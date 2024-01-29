1ef2d6a18934df1dd1c1e237fec34a388a2be218
Daniel Ball <g.thirteen333@gmail.com>
2024-01-28 20:57:37 -0500

    Time: 59 ms (43.92%) | Memory: 51.7 MB (11.45%) - LeetSync

---
+++ 792-binary-search/binary-search.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+// Binary Search
+// Time: O(log(n))
+var search = function(nums, target) {
+    let l = 0, r = nums.length-1
+    
+    while (l <= r) {
+        let m = Math.floor((l + r) / 2)
+
+        if (nums[m] === target) {
+            return m
+        } else if (nums[m] > target) {
+            r = m-1
+        } else {
+            l = m+1
+        }
+    }
+    return -1
+};
