688c34bf493b693bcf58805fbe88e0ab843045dd
Daniel Ball <g.thirteen333@gmail.com>
2024-01-31 21:46:47 -0500

    Time: 429 ms (69.87%) | Memory: 119.2 MB (90.62%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.js
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.js
@@ -5,30 +5,34 @@
  * @param {number} destination
  * @return {boolean}
  */
-// DFS, STACK
-// Time and Space: O(n)
-var validPath = function(n, edges, src, dest) {
-    const adjList = new Map(), stack = [src], visited = new Set()
+// Depth-First-Search
+// Time and Space: O(V + E)
+var validPath = function(n, edges, source, destination) {
+    if (source === destination) return true;
+    const adjList = new Map(), visited = new Set(), stack = []
 
-    for (let [x, y] of edges) {
-        if (!adjList.has(x)) adjList.set(x, [])
-        if (!adjList.has(y)) adjList.set(y, [])
-        adjList.get(x).push(y)
-        adjList.get(y).push(x)
+    for (let [x , y] of edges) {
+        if (!adjList.has(x)) adjList.set(x, new Set());
+        if (!adjList.has(y)) adjList.set(y, new Set());
+        adjList.get(x).add(y);
+        adjList.get(y).add(x);
     }
+
+    if (adjList.has(source)) stack.push(source)
+
     while (stack.length) {
         const nde = stack.pop();
-        visited.add(nde)
-        if (nde === dest) return true
+        visited.add(nde);
+        if (nde === destination) return true;
         if (adjList.has(nde)) {
-            const arr = adjList.get(nde)
-            for (let node of arr) {
-                if (node === dest) return true
-                if (!visited.has(node)) {
-                    stack.push(node);
-                    visited.add(node);
+            const nodes = adjList.get(nde)
+            if (nodes.has(destination)) return true;
+            Array.from(nodes).forEach(n => {
+                if (!visited.has(n)) {
+                    visited.add(n)
+                    stack.push(n);
                 }
-            }
+            })
         }
     }
     return false;
