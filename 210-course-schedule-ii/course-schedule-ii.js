61be0be23ced405667a2100bbcf3c03652fc03da
Daniel Ball <g.thirteen333@gmail.com>
2024-02-04 22:07:20 -0500

    Time: 75 ms (51.14%) | Memory: 56.1 MB (20.74%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.js
@@ -0,0 +1,31 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+const findOrder = (numCourses , pre) => {
+    let indegrees = new Array(numCourses ).fill(0), map = new Map(), result = []
+    const queue = new Queue();
+    for(let [k, v] of pre) {
+        if (!map.has(v)) map.set(v, []);
+        indegrees[k]++;
+        map.get(v).push(k);
+    }
+    for (let i = 0; i < indegrees.length; i++) {
+        if (indegrees[i] === 0) queue.enqueue(i)
+    }
+
+    while (!queue.isEmpty()) {
+        let node = queue.dequeue();
+        result.push(node);
+        if (map.has(node)) {
+            map.get(node).forEach(nde => {
+                indegrees[nde]--
+                if ( indegrees[nde] === 0) {
+                    queue.enqueue(nde)
+                }
+            })
+        }
+    }
+   return result.length === numCourses  ? result : []
+}
