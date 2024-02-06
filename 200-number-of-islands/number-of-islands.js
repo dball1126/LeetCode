09d12940f3f267205da7791ad73a2abaadf5b64d
Daniel Ball <g.thirteen333@gmail.com>
2024-02-05 22:26:25 -0500

    Time: 101 ms (27.94%) | Memory: 65.4 MB (5.96%) - LeetSync

--- 200-number-of-islands/number-of-islands.js
+++ 200-number-of-islands/number-of-islands.js
@@ -2,21 +2,19 @@
  * @param {character[][]} grid
  * @return {number}
  */
+// Union Find with path compression and union by rank
+// Time: O(n * m * 4 + O(1) * amortized time) = O(n * m)
+// Space: O(n * m * 2) = O(n * m)
 var numIslands = function(grid) {
-    const n = grid.length, m = grid[0].length
-    const root = [...new Array(n*m)]
-    const rank = [...new Array(n*m)].fill(1)
-    const dirs = [[1,0], [-1,0], [0,1],[0,-1]]
-    let islands = 0
+    let n = grid.length, m = grid[0].length, islands = 0
+    let rank = new Array(n * m).fill(1), root = [...new Array(n * m).keys()]
+    let dirs = [[1,0], [-1,0], [0, 1], [0, -1]]
 
-    const find = (nde) => root[nde] === nde ? root[nde] : find(root[nde])
-    const union = (n1, n2) => {
-        let p1 = find(n1), p2 = find(n2)
-
-        if (p1 === p2) return
-      
-        islands--
-        if (rank[p1] > rank[p2]) {
+    const find = (i) => root[i] = i === root[i] ? i : find(root[i]) // path compression
+    const union = (i, j) => {
+        let p1 = find(i), p2 = find(j)
+        if (p1 === p2) return;
+        if (rank[p1] > rank[p2]) { // union by rank
             root[p2] = p1
         } else if (rank[p2] > rank[p1]) {
             root[p1] = p2
@@ -24,25 +22,21 @@ var numIslands = function(grid) {
             rank[p1]++
             root[p2] = p1
         }
+        islands--
     }
-
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++) { // build array
         for (let j = 0; j < m; j++) {
-            let idx = (i * m) + j
-            root[idx] = idx;
-            if (grid[i][j] === '1') islands++
+            if (grid[i][j] === "1") islands++
         }
     }
-
     for (let i = 0; i < n; i++) {
         for (let j = 0; j < m; j++) {
-            if (grid[i][j] === '1') {
-                for(let [x, y] of dirs) {
-                    let r = i+x, c = j + y
-                    if (r >= 0 && c >= 0 && r < n && c < m) {
-                        if (grid[r][c] === '1') {
-                            union((i*m) + j, (r*m) + c)
-                        }
+            
+            for (let [c, r] of dirs) {
+                if ((i + c) >= 0 && (j + r) >= 0 && (i + c) < n && (j + r) < m) {
+                    if (grid[i][j] === "1" && grid[i+c][j+r] === "1") {
+
+                        union((i * m) + j, (i+c) * m + (j + r))
                     }
                 }
             }
