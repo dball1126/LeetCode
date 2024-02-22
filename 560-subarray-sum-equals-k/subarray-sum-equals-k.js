300d0e8f33bb2eace54b5cfda6d9da2ba66be2ad
Daniel Ball <g.thirteen333@gmail.com>
2024-02-21 20:02:48 -0500

    Time: 72 ms (78.73%) | Memory: 58.7 MB (34.10%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.js
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.js
@@ -3,19 +3,21 @@
  * @param {number} k
  * @return {number}
  */
-// Prefix Sum, Dynamic Programming
+// Prefix Sum
 // Time and Space: O(n)
 var subarraySum = function(nums, k) {
-   let dp = new Map([[0,1]]), n = nums.length, prefixSum = 0, total = 0;
+    if (!nums.length) return 0;
+    let result = 0, map = new Map([[0,1]]), sum = 0;
 
-    for (let i = 0; i < n; i++) {
-        prefixSum += nums[i]
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i]
+        
+        if (map.has(sum - k)) {
+            result += map.get(sum - k)
+        }
 
-        if (dp.has(prefixSum - k)) total += dp.get(prefixSum - k)
-
-        if (!dp.has(prefixSum)) dp.set(prefixSum, 0)
-
-        dp.set(prefixSum, dp.get(prefixSum) + 1)
+        if (!map.has(sum)) map.set(sum, 0)
+        map.set(sum, map.get(sum) + 1)
     }
-    return total
+    return result;
 };
