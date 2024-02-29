0d57bdcf937d3f19deb2a01af4b54fa2b6867012
Daniel Ball <g.thirteen333@gmail.com>
2024-02-29 08:11:27 -0500

    Time: 223 ms (12.67%) | Memory: 73.4 MB (39.44%) - LeetSync

--- 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.js
+++ 1305-number-of-visible-people-in-a-queue/number-of-visible-people-in-a-queue.js
@@ -5,29 +5,23 @@
 // Monotonic Stack
 // Time: O(n)
 // Space: O(n)
+// Monotonic Stack
+// Time: O(n)
+// Space: O(n)
 function canSeePersonsCount(heights) {
   let n = heights.length, result = heights.map(a => 0), stack = []
   const top = () => stack[stack.length-1]
 
-  for (let i = 0; i < heights.length; i++) {
+  for (let i = 0; i < n; i++) {
     
-      // next greater section
       while (stack.length && heights[top()] <= heights[i]) {
         let idx = stack.pop()
         result[idx]++
-        if (stack.length) {
-          result[top()]++
-        }
       }
-
+      if (stack.length) {
+        result[top()]++
+      }
       stack.push(i)
-  }
-
-  while (stack.length) {
-    let idx = stack.pop()
-    if (stack.length) {
-      result[top()]++
-    }
   }
 
   return result
