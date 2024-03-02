04a86799bbc032902820a92caf9bfc8696a90a1c
Daniel Ball <g.thirteen333@gmail.com>
2024-03-02 01:45:09 -0500

    Time: 84 ms (46.68%) | Memory: 55.4 MB (66.79%) - LeetSync

---
+++ 498-diagonal-traverse/diagonal-traverse.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[]}
+ */
+// Matrix Traversal
+// Time: O(n * m)...rows * cols
+// Space: O(1)...not counting output array
+var findDiagonalOrder = function(mat) {
+    if (!mat.length) return []
+    let result = [], r = 0, c = 0
+    let n = mat.length
+    let m = mat[n-1].length, dir = "U_R"
+
+    while ( r < n || c < m) {
+        let x = -1, y = 1
+        if (dir !== "U_R") {
+            x = 1, y = -1
+        }
+
+        result.push(mat[r][c])
+        if (r === n-1 && c === m-1) return result
+
+        while ((r+x) >= 0 && (r+x) < n && (c+y) >= 0 && (c+y) < m) {
+            r += x; c += y;
+            result.push(mat[r][c])
+        }
+
+        if (dir === "U_R") {
+            if ( (c+1) < m) {
+                c += 1
+            } else {
+                r += 1
+            }
+        } else {
+            if ((r+1) < n) {
+                r += 1
+            } else {
+                c += 1
+            }
+        }
+        dir = (dir === "U_R") ? "D_L" : "U_R"
+    }
+    return result;
+};
