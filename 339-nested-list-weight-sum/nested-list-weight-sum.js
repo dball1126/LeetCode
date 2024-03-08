92bd2e537473b0e6c7c0d1059caf64754db79969
Daniel Ball <g.thirteen333@gmail.com>
2024-03-07 23:20:07 -0500

    Time: 50 ms (70.52%) | Memory: 49.2 MB (63.87%) - LeetSync

---
+++ 339-nested-list-weight-sum/nested-list-weight-sum.js
@@ -0,0 +1,61 @@
+/**
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * function NestedInteger() {
+ *
+ *     Return true if this NestedInteger holds a single integer, rather than a nested list.
+ *     @return {boolean}
+ *     this.isInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Return the single integer that this NestedInteger holds, if it holds a single integer
+ *     Return null if this NestedInteger holds a nested list
+ *     @return {integer}
+ *     this.getInteger = function() {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a single integer equal to value.
+ *     @return {void}
+ *     this.setInteger = function(value) {
+ *         ...
+ *     };
+ *
+ *     Set this NestedInteger to hold a nested list and adds a nested integer elem to it.
+ *     @return {void}
+ *     this.add = function(elem) {
+ *         ...
+ *     };
+ *
+ *     Return the nested list that this NestedInteger holds, if it holds a nested list
+ *     Return null if this NestedInteger holds a single integer
+ *     @return {NestedInteger[]}
+ *     this.getList = function() {
+ *         ...
+ *     };
+ * };
+ */
+/**
+ * @param {NestedInteger[]} nestedList
+ * @return {number}
+ */
+// Depth First Search
+// Time and Space: O(n)
+var depthSum = function(nestedList) {
+    let result = 0
+
+    const dfs = (depth, arr) => {
+        for (let i = 0; i < arr.length; i++) {
+            const n = arr[i]
+            if (!n.isInteger()) {
+                dfs(depth+1, n.getList())
+            } else {
+                result += (n.getInteger() * depth)
+            }
+        }
+    }
+    dfs(1, nestedList)
+
+    return result
+};
