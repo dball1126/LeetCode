ceb7dee9aade65e668e8d82f62e447c94a158b6e
Daniel Ball <g.thirteen333@gmail.com>
2024-03-11 21:49:52 -0400

    Time: 273 ms (18.94%) | Memory: 55.1 MB (60.80%) - LeetSync

---
+++ 301-remove-invalid-parentheses/remove-invalid-parentheses.js
@@ -0,0 +1,70 @@
+/**
+ * @param {string} s
+ * @return {string[]}
+ */
+var removeInvalidParentheses = function(s) {
+    let open = 0, close = 0, str = "", result = new Set(), n = s.length
+
+    for (let i = 0; i < n; i++) { // O(n)
+        const v = s[i];
+        if (v === ")" && open <= close) {
+            continue;
+        } else if (v === "(") {
+            open++; str += v;
+        } else if (v === ")") {
+            close++; str += v;
+        } else {
+            str += v;
+        }
+    }
+    open = 0; close = 0;
+    let newStr = ""
+    for (let i = str.length -1; i >= 0; i--) { // O(n)
+        const v = str[i];
+        if (v === "(" && close <= open) {
+            continue;
+        } else if (v === "(") {
+            open++; newStr =  v + newStr;
+        } else if (v === ")") {
+            close++; newStr =  v + newStr;
+        } else {
+            newStr =  v + newStr;
+        }
+    }
+    let diff = (s.length - newStr.length)
+
+    const validParentheses = (str) => { // O(n)
+        let open = 0, close = 0;
+        for (let v of str) {
+            if (v === ")" && open <= close) {
+                return false;
+            } else if (v === ")") {
+                close++;
+            } else if (v === "(") {
+                open++;
+            }
+        }
+        return close === open
+    }
+
+    const backtrack = (idx, removes, curr) => { // O(n * 2^T)...T is the number of removals...n for checking the parentheses
+
+        if (removes === diff) {
+            let val = curr + s.slice(idx)
+            if (validParentheses(val)) result.add(val)
+            return
+        }
+        if (idx >= n) return
+
+        for (let i = idx; i < n; i++) {
+            const v = s[i]
+            if (v === "(" || v === ")") {
+                backtrack(i+1, removes + 1, curr)
+            }
+            curr += v;
+        }
+    }
+
+    backtrack(0, 0, "")
+    return result.size === 0 ? [""] : Array.from(result)
+};
