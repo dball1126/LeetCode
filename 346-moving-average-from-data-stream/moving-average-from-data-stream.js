052a52d47bc5c9c8ca57f9f2588cedc1092f3061
Daniel Ball <g.thirteen333@gmail.com>
2024-03-15 19:12:27 -0400

    Time: 83 ms (90.64%) | Memory: 57.3 MB (45.53%) - LeetSync

---
+++ 346-moving-average-from-data-stream/moving-average-from-data-stream.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number} size
+ */
+var MovingAverage = function(size) {
+    this.size = size;
+    this.nums = []
+};
+
+/** 
+ * @param {number} val
+ * @return {number}
+ */
+// Prefix sums
+ // Time: O(1)
+ // Space: O(n)
+MovingAverage.prototype.next = function(val) {
+    this.nums.push(val)
+    const n = this.nums.length
+    if (n > 1) {
+        this.nums[n-1] += this.nums[n-2]
+    }
+
+    if (n > this.size) { // use prefix sum
+        let sumE = this.nums[n-1]
+        let sumB = this.nums[n - this.size - 1]
+        sumE -= sumB
+
+        return sumE / this.size
+    } else {
+        return this.nums[n-1] / n
+    }
+};
+
+
+/** 
+ * Your MovingAverage object will be instantiated and called as such:
+ * var obj = new MovingAverage(size)
+ * var param_1 = obj.next(val)
+ */
