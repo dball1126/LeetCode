819157904aa079cab60789e2a6a4ec59d068eeb6
Daniel Ball <g.thirteen333@gmail.com>
2024-03-23 16:00:28 -0400

    Time: 67 ms (35.56%) | Memory: 52.3 MB (17.78%) - LeetSync

---
+++ 1192-divide-chocolate/divide-chocolate.js
@@ -0,0 +1,40 @@
+/**
+ * @param {number[]} sweetness
+ * @param {number} k
+ * @return {number}
+ */
+// Binary Search
+// Time:(n * log(m))...n for sweets in sweetness...m for max sum range of 1...sum
+var maximizeSweetness = function(sweetness, k) {
+    k += 1
+    let low = 1, high = 0
+    let minimumSweetness = -Infinity
+    for (let s of sweetness) {
+        high += s
+    }
+
+    const isPossible = (sweet) => {
+        let total = 0
+        let count = 0
+        for (let s of sweetness) {
+            count += s
+            if (count >= sweet) {
+                total++
+                count = 0
+            }
+        }
+        return total >= k
+    }
+
+    while (low <= high) {
+        let mid = Math.floor((high + low) / 2)
+
+        if (isPossible(mid)) {
+            minimumSweetness = Math.max(minimumSweetness, mid)
+            low = mid +1
+        } else {
+            high = mid -1
+        }
+    }
+    return minimumSweetness === -Infinity ? 0 : minimumSweetness
+};
