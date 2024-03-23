6c4369fc120e2d2e5e7b652fbb5c226a69369e15
Daniel Ball <g.thirteen333@gmail.com>
2024-03-23 13:50:27 -0400

    Time: 121 ms (32.17%) | Memory: 69.9 MB (16.08%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.js
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.js
@@ -2,16 +2,19 @@
  * @param {number[]} heights
  * @return {number[]}
  */
-// Monotonic Stack
+// Stack
 // Time and Space: O(n)
 var findBuildings = function(heights) {
-    let stack = []
+    const n = heights.length;
+
+    const stack = []
     const top = () => stack[stack.length-1]
-    heights.forEach((v, i) => {
-        while (stack.length && heights[top()] <= v) {
+
+    for (let i = 0; i < n; i++) {
+        while (stack.length && heights[top()] <= heights[i]) {
             stack.pop()
         }
         stack.push(i)
-    })
-    return stack
+    }
+    return stack;
 };
