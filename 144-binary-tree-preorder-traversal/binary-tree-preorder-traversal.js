fd273b0151fd591beec408e4b573154842c69576
Daniel Ball <g.thirteen333@gmail.com>
2024-03-24 12:45:10 -0400

    Time: 55 ms (37.73%) | Memory: 49.1 MB (29.62%) - LeetSync

--- 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.js
+++ 144-binary-tree-preorder-traversal/binary-tree-preorder-traversal.js
@@ -1,14 +1,24 @@
-// Depth First Search
-// Time and Space: O(n)
-// Iterative
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+// root, left, right
 var preorderTraversal = function(root) {
-    let stack = [], result = []
+    let curr = root, stack = [], preorder = []
 
-    while (root || stack.length) {
-        if (!root) root = stack.pop();
-        result.push(root.val);
-        if (root.right) stack.push(root.right)
-        root = root.left
+    while (stack.length || curr) {
+        if (!curr) curr = stack.pop();
+        preorder.push(curr.val)
+        if (curr.right) stack.push(curr.right)
+        curr = curr.left;
     }
-    return result;
-};
+    return preorder
+}
