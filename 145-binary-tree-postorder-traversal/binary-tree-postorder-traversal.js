54f3509adfb2c76131785055cdfd12bc3d3207e9
Daniel Ball <g.thirteen333@gmail.com>
2024-03-24 12:53:44 -0400

    Time: 54 ms (44.32%) | Memory: 48.7 MB (75.92%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.js
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.js
@@ -10,31 +10,7 @@
  * @param {TreeNode} root
  * @return {number[]}
  */
- 
-// Depth First Search
-// Time and Space: O(n)
-// Left, Right, Root
 var postorderTraversal = function(root) {
-    let stack = [], result = []
-
-    while (stack.length || root) {
-        let prev = result[result.length-1]
-
-        while (root && root.left && root.left.val !== prev) {
-            stack.push(root)
-            root = root.left
-        }
-        if (!root) root = stack.pop();
-        prev = result[result.length-1]
-
-        if (root.right && root.right.val !== prev) {
-            stack.push(root)
-            root = root.right
-        } else {
-            result.push(root.val)
-            root = null;
-        }
-    }
-    return result
+   if (!root) return []
+   return [...postorderTraversal(root.left), ...postorderTraversal(root.right), root.val] 
 };
-
