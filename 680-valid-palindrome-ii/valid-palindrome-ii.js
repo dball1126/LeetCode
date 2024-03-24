7e13770e58cbdf66050fe5fb6f6ffc9ba5598ff4
Daniel Ball <g.thirteen333@gmail.com>
2024-03-24 00:29:23 -0400

    Time: 72 ms (55.71%) | Memory: 56.2 MB (39.11%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.js
+++ 680-valid-palindrome-ii/valid-palindrome-ii.js
@@ -2,25 +2,26 @@
  * @param {string} s
  * @return {boolean}
  */
-// TWo-pointer 
-// Time: O(n), Space: O(1)
+// String
+// Time: O(n)
+// Space: O(1)
 var validPalindrome = function(s) {
     let l = 0, r = s.length-1
-
-    const valid = (left, right) => {
-        let l = left, r = right
-        while (l < r) {
-            if (s[l] !== s[r]) return false;
-            l++; r--
+    
+    const checkPalindrome = (i, j) => {
+        while (i < j) {
+            if (s[i] !== s[j]) return false;
+            i++; j--;
         }
-        return true
+        return true;
     }
 
     while (l < r) {
-        if (s[l] !== s[r]) {
-            return valid(l+1, r) || valid(l, r-1)
+        if (s[l] === s[r]) {
+            l++, r--;
+        } else {
+            return checkPalindrome(l+1, r) || checkPalindrome(l, r-1)
         }
-        l++; r--;
     }
-    return true
+    return true;
 };
