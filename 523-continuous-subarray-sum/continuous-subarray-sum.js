53a63b38a5e183f65a422c51088337f8f127bc77
Daniel Ball <g.thirteen333@gmail.com>
2024-03-24 11:49:30 -0400

    Time: 105 ms (67.54%) | Memory: 74.4 MB (27.66%) - LeetSync

--- 523-continuous-subarray-sum/continuous-subarray-sum.js
+++ 523-continuous-subarray-sum/continuous-subarray-sum.js
@@ -3,20 +3,26 @@
  * @param {number} k
  * @return {boolean}
  */
-// Prefix Sum / Dynamic Programming
+// Prefix Sums
 // Time and Space: O(n)
 var checkSubarraySum = function(nums, k) {
-    let prefixSumsDP = new Map(), sum = 0
-
+    const prefixSums = new Map()
+    let sum = 0
     for (let i = 0; i < nums.length; i++) {
         sum += nums[i]
-        if (i > 0) {
-            if (sum % k === 0) return true
-            if (prefixSumsDP.has(sum % k) && (i - prefixSumsDP.get(sum % k)) >= 2) return true;
-        }
-        if (!prefixSumsDP.has(sum % k)) {
-            prefixSumsDP.set(sum % k, i);            
+        if (!prefixSums.has(sum % k)) prefixSums.set(sum % k, i)
+    }
+    sum = 0
+    for (let i = 0; i < nums.length; i++) {
+        sum += nums[i]
+        if ((sum % k) === 0 && i >= 1) {
+            return true;
+        } else if (prefixSums.has(sum % k)) {
+            let idx = prefixSums.get(sum % k) 
+            if (i - idx >= 2) {
+                return true;
+            }
         }
     }
-    return false;
+    return false
 };
