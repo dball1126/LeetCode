69d148900b3316602672800d9dbf7285c11f0e29
Daniel Ball <g.thirteen333@gmail.com>
2024-03-25 00:18:28 -0400

    Time: 57 ms (23.35%) | Memory: 49.1 MB (28.58%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
@@ -2,30 +2,26 @@
  * @param {string} digits
  * @return {string[]}
  */
-// Backtracking
-// Time: O(4^n)
+// Time: O(4^n)...n being the number of digits
 // Space: O(n)
 var letterCombinations = function(digits) {
-    if (!digits.length) return []
-    let phoneMap = new Map([["2", "abc"],["3","def"],["4","ghi"],["5","jkl"],["6","mno"]
-    ,["7","pqrs"],["8","tuv"],["9","wxyz"]]);
-    let result = [], n = digits.length 
+    if (!digits) return []
+    let map = new Map([["2", ["a","b","c"]],["3", ["d","e","f"]],["4", ["g","h","i"]],["5", ["j","k","l"]],["6", ["m","n","o"]],["7", ["p","q","r","s"]],
+        ["8", ["t","u","v"]],["9", ["w","x","y","z"]]])
+    let combos = []
+    const backtrack = (idx, curr) => {
+        if (curr.length >= digits.length) {
+            return combos.push(curr)
+        }
 
-    const backtrack = (curr, idx) => {
-        if (curr.length === n) return result.push(curr)
-        if (idx >= n) return;
-
-        for (let i = idx; i < n; i++) {
-            
-            let arr = phoneMap.get(digits[i])
-
-            for (let j = 0; j < arr.length; j++) {
-                const v = arr[j];
-                backtrack( curr + v, i+1)
+        for (let i = idx; i < digits.length; i++) {
+            let d = digits[i]
+            let arr = map.get(d)
+            for (let c of arr) {
+                backtrack(i+1, curr + c)
             }
-            
         }
     }
-    backtrack("", 0)
-    return result;
+    backtrack(0, "")
+    return combos
 };
