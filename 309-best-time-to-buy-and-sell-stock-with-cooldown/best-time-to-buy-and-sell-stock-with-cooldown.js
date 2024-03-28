051a542e99fd25c856b5e4de461f73d99030c9a7
Daniel Ball <g.thirteen333@gmail.com>
2024-03-28 08:36:40 -0400

    Time: 53 ms (80.30%) | Memory: 51.8 MB (28.06%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.js
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.js
@@ -2,26 +2,20 @@
  * @param {number[]} prices
  * @return {number}
  */
-// Top-Down Dynamic Programming
+// Bottom-Up Dynamic Programming
 // Time and Space: O(n)
 var maxProfit = function(prices) {
     let n = prices.length
-    const memo = [...new Array(n+1)].map(a => [...new Array(3)].fill(-Infinity))
+    const memo = [...new Array(n+2)].map(a => [...new Array(4)].fill(0))
 
-    const dp = (i, c) => { // c is 0 for buy, 1 for sell
-        if (i >= n || c >= 2) return 0
-        if (memo[i][c] !== -Infinity) return memo[i][c]
-        memo[i][c] = 0
-        if (c === 0) {
-            const v1 =  dp(i+1, 1)  + (-prices[i])
-            const v2 = dp(i+1, c)
-            return memo[i][c] = Math.max(v1, v2)
-        } else {
-            const v1 = prices[i] + dp(i+2, 0)
-            const v2 = dp(i+1, c)
-            return memo[i][c] = Math.max(v1, v2)
-        }   
+    for (let i = n-1; i >= 0; i--) {
+        for(let c = 1; c >= 0;  c--) {
+            if (c === 1) {
+                memo[i][c] = Math.max(prices[i] + memo[i+2][0], memo[i+1][c])
+            } else {
+                memo[i][c] = Math.max(memo[i+1][1] + (-prices[i]), memo[i+1][c])
+            }
+        }
     }
-    dp(0,0)
-    return Math.max(memo[0][0])
-};
+    return memo[0][0]
+}
