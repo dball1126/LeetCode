4e407b0d5618e1315c7f39a026687bc9b9c1ede9
Daniel Ball <g.thirteen333@gmail.com>
2024-03-27 22:52:43 -0400

    Time: 76 ms (49.63%) | Memory: 59 MB (54.51%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,12 +3,14 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    if (prices.length <= 1) return 0
-    let min = prices[0], profit = 0
-    
+    if (!prices.length) return 0;
+    let min = prices[0], profit = 0;
+
     for (let i = 1; i < prices.length; i++) {
+        if (prices[i] > min) {
+            profit = Math.max(profit, prices[i] - min)
+        }
         min = Math.min(min, prices[i])
-        profit = Math.max(profit, prices[i] - min)
     }
     return profit;
 };
