4bfa2f3e53e86402b1f5cfd1c066f12d164de78b
Daniel Ball <g.thirteen333@gmail.com>
2024-03-30 03:25:49 -0400

    Time: 916 ms (65.69%) | Memory: 61.7 MB (5.06%) - LeetSync

---
+++ 212-word-search-ii/word-search-ii.js
@@ -0,0 +1,55 @@
+class Node {
+    constructor() {
+        this.isWord = false; this.keys = new Map();
+        
+    }
+}
+/**
+ * @param {character[][]} board
+ * @param {string[]} words
+ * @return {string[]}
+ */
+var findWords = function(board, words) {
+    const trie = new Node(), result = new Set()
+    const dirs = [[1,0],[-1,0],[0,1],[0,-1]]
+    let n = board.length, m = board[0].length
+    const visited = [...new Array(n)].map(a => [...new Array(m)].fill(false))
+    // build trie
+    for (let word of words) {
+        let root = trie;
+        for (let i = 0; i < word.length; i++) {
+            let c = word[i]
+            if (!root.keys.has(c)) root.keys.set(c, new Node())
+            root = root.keys.get(c)
+            if (i === word.length-1) root.isWord = true;
+        }
+    }
+
+    const backtrack = (nde, r, c, visited, curr) => {
+        if (nde.isWord) result.add(curr)
+        for (let [x, y] of dirs) {
+            let x1 = r+x, y1 = y+c
+
+            if (x1 >= 0 && x1 < n  && y1 >= 0 && y1 < m) {
+                let c = board[x1][y1]
+
+                if (nde.keys.has(c) && visited[x1][y1] === false) {
+                    visited[x1][y1] = true;
+                    backtrack(nde.keys.get(c), x1, y1, visited, curr + c)
+                    visited[x1][y1] = false;
+                }
+            }
+        }
+    }
+    for (let r = 0; r < n; r++) {
+        for (let c = 0; c < m; c++) {
+            let root = trie;
+            if (root.keys.has(board[r][c])) {
+                visited[r][c] = true
+                backtrack(root.keys.get(board[r][c]), r, c, visited, board[r][c])
+                visited[r][c] = false
+            }
+        }
+    }
+return Array.from(result)
+};
