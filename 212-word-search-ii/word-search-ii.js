c7f7a5be91f9847e23a02ed896c0b5b37e699f15
Daniel Ball <g.thirteen333@gmail.com>
2024-03-30 04:00:18 -0400

    Time: 909 ms (66.01%) | Memory: 61.7 MB (5.06%) - LeetSync

--- 212-word-search-ii/word-search-ii.js
+++ 212-word-search-ii/word-search-ii.js
@@ -9,37 +9,31 @@ class Node {
  * @param {string[]} words
  * @return {string[]}
  */
+// Backtracking
+// Time: O(n * (4^L))...n for # of words and L for longest word in the words
+// Space: O(n)
 var findWords = function(board, words) {
-    const trie = new Node(), result = new Set()
-    const dirs = [[1,0],[-1,0],[0,1],[0,-1]]
-    let n = board.length, m = board[0].length
+    const trie = new Node(), result = new Set(), n = board.length, m = board[0].length
     const visited = [...new Array(n)].map(a => [...new Array(m)].fill(false))
-    // build trie
-    for (let word of words) {
+    const dirs = [[1,0],[-1,0],[0,1],[0,-1]]
+    for (let word of words) { // build trie
         let root = trie;
         for (let i = 0; i < word.length; i++) {
             let c = word[i]
             if (!root.keys.has(c)) root.keys.set(c, new Node())
             root = root.keys.get(c)
-            if (i === word.length-1) root.isWord = true;
-        }
+            if (i === word.length-1) root.isWord = true;}
     }
-
     const backtrack = (nde, r, c, visited, curr) => {
         if (nde.isWord) result.add(curr)
         for (let [x, y] of dirs) {
             let x1 = r+x, y1 = y+c
-
             if (x1 >= 0 && x1 < n  && y1 >= 0 && y1 < m) {
                 let c = board[x1][y1]
-
                 if (nde.keys.has(c) && visited[x1][y1] === false) {
                     visited[x1][y1] = true;
                     backtrack(nde.keys.get(c), x1, y1, visited, curr + c)
-                    visited[x1][y1] = false;
-                }
-            }
-        }
+                    visited[x1][y1] = false;}}}
     }
     for (let r = 0; r < n; r++) {
         for (let c = 0; c < m; c++) {
@@ -47,9 +41,7 @@ var findWords = function(board, words) {
             if (root.keys.has(board[r][c])) {
                 visited[r][c] = true
                 backtrack(root.keys.get(board[r][c]), r, c, visited, board[r][c])
-                visited[r][c] = false
-            }
-        }
+                visited[r][c] = false}}
     }
-return Array.from(result)
+    return Array.from(result)
 };
