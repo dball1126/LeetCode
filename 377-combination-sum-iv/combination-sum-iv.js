09218cfd98173f206f5d0dc4f11fab46bff80007
Daniel Ball <g.thirteen333@gmail.com>
2024-04-05 23:36:37 -0400

    Time: 56 ms (71.10%) | Memory: 50.7 MB (73.94%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.js
+++ 377-combination-sum-iv/combination-sum-iv.js
@@ -3,22 +3,19 @@
  * @param {number} target
  * @return {number}
  */
-// Top-Down Dynamic Programming
+// Dynamic Programming
 // Time: O(n * m)
 // Space: O(m)
-var combinationSum4 = function(nums, target) {
-    let memo = [...new Array(target+1)]
-
-    const dp = (a) => {
-        if (a === 0) return 1;
-        if (memo[a] !== undefined) return memo[a]
-        memo[a] = 0
+var combinationSum4 = function(nums, target) { // Bottom-Up
+    let memo = [...new Array(target+1)].fill(0)
+    memo[0] = 1
+    for (let i = 1; i < target+1; i++) {
+        memo[i] = 0
         for (let v of nums) {
-            if ((a - v) >= 0 && (a - v) < a) {
-                memo[a] += dp(a - v)
+            if ((i - v) >= 0  && (i - v) < i) {
+                memo[i] += memo[i - v]
             }
         }
-        return memo[a]
     }
-    return dp(target)
+    return memo[target]
 };
