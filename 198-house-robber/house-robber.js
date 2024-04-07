0beccca7018b133e9978cf0f191850e652f840e6
Daniel Ball <g.thirteen333@gmail.com>
2024-04-07 03:46:23 -0400

    Time: 41 ms (95.85%) | Memory: 48.7 MB (52.14%) - LeetSync

--- 198-house-robber/house-robber.js
+++ 198-house-robber/house-robber.js
@@ -2,23 +2,16 @@
  * @param {number[]} nums
  * @return {number}
  */
-// Dynamic Programming
-// Time and Space: O(n)
-var rob = function(nums) { // Top Down
+var rob = function(nums) { // Bottom-Up
     if (nums.length === 1) return nums[0]
     if (nums.length === 2) return Math.max(nums[0], nums[1])
     const n = nums.length;
     let memo = [...new Array(n)]
     memo[0] = nums[0]; 
     memo[1] = nums[1]
-    const dp = (idx) => {
-        if (idx < 0 || idx >= n) return 0
-        if (memo[idx] !== undefined) return memo[idx]
-        memo[idx] = Math.max(dp(idx - 1 ), nums[idx] + dp(idx-2))
-        memo[idx-1] = Math.max(memo[idx-1], memo[idx-2])
-        dp(idx+1)
-        return memo[idx]
+    for (let i = 2; i < n; i++) {
+        memo[i] = Math.max(nums[i] + memo[i-2], memo[i-1])
+        memo[i-1] = Math.max(memo[i-1], memo[i-2])
     }
-    dp(2)
     return memo[n-1]
-};
+}
