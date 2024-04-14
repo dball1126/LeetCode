564d41521ba57d22c0fbbb1a650565ed6d7d4cfa
Daniel Ball <g.thirteen333@gmail.com>
2024-04-14 01:19:58 -0400

    Time: 55 ms (30.84%) | Memory: 49.2 MB (5.44%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.js
+++ 70-climbing-stairs/climbing-stairs.js
@@ -2,20 +2,20 @@
  * @param {number} n
  * @return {number}
  */
-// Top-down dynamic programming
-// Time & Space: O(n)
 var climbStairs = function(n) {
-    let memo = [...new Array(n)]
+    
+    let memo = [...new Array(n+1)]
 
-    const dp = (t) => {
-        if ( t === 0) return 1;
-        if (t < 0) return 0
+    const getStairs = (num) => {
+        if (num === 0) return 1;
+        if (memo[num] !== undefined) return memo[num]
 
-        if (memo[t] !== undefined) return memo[t]
-        memo[t] = 0
-        if ((t - 1) >= 0) memo[t] += dp(t-1)
-        if ((t - 2) >= 0) memo[t] += dp(t-2)
-        return memo[t]
+        if (num >= 2) {
+            return memo[num] = getStairs(num-1) +  getStairs(num-2)
+        } else {
+            return memo[num] = getStairs(num-1)
+        }
+
     }
-    return dp(n)
+    return getStairs(n)
 };
