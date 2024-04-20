a5effc9fd64c56b868a55aea9f54dea817894749
Daniel Ball <g.thirteen333@gmail.com>
2024-04-19 22:22:34 -0400

    Time: 45 ms (98.72%) | Memory: 50.7 MB (73.92%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.js
+++ 64-minimum-path-sum/minimum-path-sum.js
@@ -2,21 +2,19 @@
  * @param {number[][]} grid
  * @return {number}
  */
-// Dynamic Programming
-// Time and Space: O(n * m)
-// Bottom-Up
+// Min/Max Path sum Pattern
+// Top-Down Dynamic Programming
+// Time and Space: O(n^2)
 var minPathSum = function(grid) {
     let n = grid.length, m = grid[0].length
-    let memo = [...new Array(n+2)].map(a => [...new Array(m+2)].fill(Infinity))
+    const memo = [...new Array(n)].map(a => [...new Array(m)])
 
-    for (let r = n-1; r >= 0; r--) {
-        for (let c = m-1; c >= 0; c--) {
-            if (r === n-1 && c === m-1) {
-                memo[r][c] = grid[r][c]; 
-                continue;
-            }
-            memo[r][c] = grid[r][c] + Math.min(memo[r+1][c], memo[r][c+1])
-        }
+    const dp = (r, c) => {
+        if (r === n-1 && c === m-1) return grid[r][c]
+        if (r >= n || c >= m) return Infinity
+        if (memo[r][c] !== undefined) return memo[r][c]
+
+        return memo[r][c] = grid[r][c] + Math.min(dp(r+1, c), dp(r, c+1))
     }
-    return memo[0][0]
-}
+    return dp(0,0)
+};
