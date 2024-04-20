fc0fe4b4a7319e8c0330213d481e89039a5c0017
Daniel Ball <g.thirteen333@gmail.com>
2024-04-20 01:14:21 -0400

    Time: 366 ms (42.42%) | Memory: 75.3 MB (93.18%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.js
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.js
@@ -1,35 +1,25 @@
-/**
- * @param {number[][]} matrix
- */
+// Dynamic Programming
+// 2-D Prefix Sum 
+// Time and Space: O(n^2)
 var NumMatrix = function(matrix) {
-    const n = matrix.length, m = matrix[0].length;
-    this.dp = [[...new Array(m+1)].fill(0)]
+    let n = matrix.length, m = matrix[0].length
+    this.matrix = [...new Array(n+1)].map(a => [...new Array(m+1)].fill(0))
+    for (let r = 1; r <= n; r++) {
+        for (let c = 1; c <= m; c++) {
 
-    for (let i = 0; i < n; i++) {
-        this.dp.push([0, ...matrix[i]])
-    }
-
-
-    for (let r = 1; r < n+1; r++) {
-        for (let c = 1; c < m+1; c++) {
-            this.dp[r][c] = (this.dp[r][c] + (this.dp[r-1][c] + this.dp[r][c-1])) - this.dp[r-1][c-1]
+            this.matrix[r][c] = matrix[r-1][c-1] + 
+                                this.matrix[r-1][c] +
+                                this.matrix[r][c-1] -
+                                this.matrix[r-1][c-1]
         }
     }
 };
-
-/** 
- * @param {number} row1 
- * @param {number} col1 
- * @param {number} row2 
- * @param {number} col2
- * @return {number}
- */
 NumMatrix.prototype.sumRegion = function(row1, col1, row2, col2) {
-    row1++; col1++; row2++; col2++
-    return this.dp[row2][col2] - (this.dp[row2][col1-1] + this.dp[row1-1][col2]) + this.dp[row1-1][col1-1]
+    row1++, col1++, row2++, col2++
+
+    return this.matrix[row2][col2] - (
+        this.matrix[row2][col1 - 1] +
+        this.matrix[row1-1][col2] 
+    ) + this.matrix[row1-1][col1-1]
 };
-/** 
- * Your NumMatrix object will be instantiated and called as such:
- * var obj = new NumMatrix(matrix)
- * var param_1 = obj.sumRegion(row1,col1,row2,col2)
- */
+var obj = new NumMatrix([[1,2,3],[4,5,6],[7,8,9]])
