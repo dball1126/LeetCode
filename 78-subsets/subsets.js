66eae84a0064ad2606707823407c8a38c618c9c5
Daniel Ball <g.thirteen333@gmail.com>
2024-05-05 02:44:05 -0400

    Time: 72 ms (8.59%) | Memory: 52.9 MB (14.20%) - LeetSync

--- 78-subsets/subsets.js
+++ 78-subsets/subsets.js
@@ -2,13 +2,16 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+// Bitmasking
+// Time complexity: O(n * 2^n)
+// Space Complexity: O(1) if we don't count the output array
 function subsets(nums) {  
-    const n = nums.length, subsets = []
+    const len = BigInt(nums.length), subsets = []
 
-    for (let mask = 0; mask < (1 << n); mask++) {
-        const set = []
-        for (let i = 0; i < n; i++) {
-            if (mask & (1 << i)) {
+    for (let mask = 0; mask < (Number(1n << len)); mask++) { // 1 << n is equivalent to 2^n
+        let set = []
+        for (let i = 0; i < len; i++) {
+            if (mask & (1 << i)) { // check to see if the ith bit is set
                 set.push(nums[i])
             }
         }
