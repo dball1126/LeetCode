030fa931cef25a3124761d289c835e09f136473e
Daniel Ball <g.thirteen333@gmail.com>
2024-05-13 23:46:32 -0400

    Time: 2423 ms (7.14%) | Memory: 85.1 MB (7.14%) - LeetSync

---
+++ 2114-minimum-number-of-work-sessions-to-finish-the-tasks/minimum-number-of-work-sessions-to-finish-the-tasks.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} tasks
+ * @param {number} sessionTime
+ * @return {number}
+ */
+var minSessions = function(tasks, sessionTime) {
+    const n = tasks.length;
+    const done = (1 << n) - 1, memo = new Map();
+
+    const dp = (mask, t) => {
+        if (mask === done) return 0;
+        let ans = Infinity, k = mask +':' + t;
+        if (memo.has(k)) return memo.get(k);
+        if (t !== sessionTime) {
+            ans = dp(mask, sessionTime) + 1
+        }
+
+        for (let i = 0; i < n; i++) {
+            let v = tasks[i]
+            if (t -v >= 0 && (mask & (1 << i)) === 0) {
+                ans = Math.min(ans,
+                    dp(mask | (1 << i), t - v))
+            }
+        }
+        memo.set(k, ans)
+        return ans
+    }
+    return dp(0, 0)
+};
