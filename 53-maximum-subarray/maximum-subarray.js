50c0288f54d4af16519500b35531107d86d75ebd
Daniel Ball <g.thirteen333@gmail.com>
2024-05-14 21:11:32 -0400

    Time: 66 ms (82.37%) | Memory: 57 MB (94.73%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -2,13 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-// Bottom-Up Dynamic Programming
-// Time: O(n), Space: O(1)
+// Dynamic Programming
+// Time: O(n)
+// Space: O(1)
 var maxSubArray = function(nums) {
-    let max = nums[0] || 0, len = nums.length
-    for (let i = 1; i < len; i++) {
+    let max = nums[0], n = nums.length;
+    
+    for (let i = 1; i < n; i++) {
         nums[i] = Math.max(nums[i], nums[i] + nums[i-1])
-        max = Math.max(max, nums[i])
+        max = Math.max(nums[i], max)
     }
-    return max
+    return max;
 };
