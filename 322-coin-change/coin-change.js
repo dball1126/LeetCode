f125190ec7df12409c71a0ea8e55a0cdc43c2aaa
Daniel Ball <g.thirteen333@gmail.com>
2024-05-16 07:25:12 -0400

    Time: 108 ms (47.13%) | Memory: 54.9 MB (64.89%) - LeetSync

--- 322-coin-change/coin-change.js
+++ 322-coin-change/coin-change.js
@@ -3,27 +3,21 @@
  * @param {number} amount
  * @return {number}
  */
-// Top-Down Dynamic Programming
-// Time: O(n * m)...coins * amt
-// space: O(m)...amt
+// Bottom-Up Dynamic Programming
+// Time: O(n * m)...n for coins and m for amount
+// Space: O(m)
 var coinChange = function(coins, amount) {
-    const memo = [...new Array(amount+1)], n = coins.length
+    const memo = [...new Array(amount+1)].fill(Infinity)
+    memo[0] = 0
+    for (let i = 1; i <= amount; i++) {
 
-    const dp = (a) => {
-        if (a === 0) return 0;
-        let ans = Infinity
-
-        if (memo[a] !== undefined) return memo[a]
-
-        for (let i = 0; i < n; i++) {
-            let v = coins[i]
-
-            if ((a - v) >= 0 && (a - v) < a) {
-                ans = Math.min(ans, dp(a - v) + 1)
+        for (let c of coins) {
+            if (i - c === 0) {
+                memo[i] = 1
+            } else if (i - c >= 0 && i - c < i) {
+                memo[i] = Math.min(memo[i], (memo[i-c]  + 1))
             }
         }
-        return memo[a] = ans
     }
-    let result = dp(amount)
-    return result === Infinity ? -1 : result;
+    return memo[amount] === Infinity ? -1 : memo[amount]
 };
