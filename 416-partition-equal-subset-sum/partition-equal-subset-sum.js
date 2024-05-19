0a849f6321412b6f6be03a6e6e87333f6843a41d
Daniel Ball <g.thirteen333@gmail.com>
2024-05-19 04:38:03 -0400

    Time: 101 ms (94.81%) | Memory: 52.1 MB (84.18%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.js
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.js
@@ -1,16 +1,16 @@
 // Bottom-Up Dynamic Programming
-// Time: O(n * m)...n for nums and m for half the sum of all nums
+// Time: O(n * m)
 // Space: O(m)
 var canPartition = function(nums) {
-    let sum = nums.reduce((acc, v) => acc +v)
-    if (sum & 1) return false;
+    let sum = nums.reduce((acc, v) => acc + v)
+    if (sum & 1) return false
     sum = Math.floor(sum / 2)
-    const memo = [...new Array(sum+1)].fill(false)
-    memo[0] = true;
-    
-    for (let num of nums) {
-        for (let s = sum; s >= num; s--) {
-            memo[s] = memo[s] || memo[s - num]
+    let memo = [...new Array(sum+1)].fill(false)
+    memo[0] = true
+
+    for (let v of nums) {
+        for (let val = sum; val >= v; val--) {
+            memo[val] = (val - v === 0) || memo[val - v] || memo[val]
         }
     }
     return memo[sum]
