153fbc08b9793e8d0c18abfe25cf69c09b0efc98
Daniel Ball <g.thirteen333@gmail.com>
2024-05-25 21:16:32 -0400

    Time: 367 ms (46.28%) | Memory: 114.2 MB (35.54%) - LeetSync

---
+++ 474-ones-and-zeroes/ones-and-zeroes.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string[]} strs
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var findMaxForm = function(strs, m, n) {
+    let len = strs.length
+    let memo = [...new Array(len+1)].map(a => 
+        [...new Array(m+1)].map(a =>
+        [...new Array(n+1)]))
+    let ones = [...new Array(len+1)].fill(0)
+    let zeros = [...new Array(len+1)].fill(0)
+    for (let i = 0; i < len; i++) {
+        let str = strs[i]
+        for (let j = 0; j < str.length; j++ ) {
+            if (str[j] === "1") {
+                ones[i]++
+            } else {
+                zeros[i]++
+            }
+        }
+    }
+
+    let dp = (idx, o, z) => {
+        if (idx >= len || z === m && n === o) return 0
+        if (memo[idx][z][o] !== undefined) return memo[idx][z][o]
+        let next = dp(idx+1, o, z)
+        let v = 0
+        let one = ones[idx], zero = zeros[idx]
+        if (o + one <= n && z + zero <= m) {
+            v = 1 + dp(idx+1, o+one, z+zero)
+        }
+        return memo[idx][z][o] = Math.max(v, next)
+    }
+    return dp(0,0,0)
+};
