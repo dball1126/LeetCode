d4e97f6bb0cf7e04f79f0c7535f4c745d018831e
Daniel Ball <g.thirteen333@gmail.com>
2024-05-27 01:23:42 -0400

    Time: 127 ms (70.07%) | Memory: 51.7 MB (14.78%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.js
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.js
@@ -1,29 +1,21 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
-// Top-Down Dynamic Programming
+// Bottom-Up Dynamic Programming
 // Time: O(n^2)
 // Space: O(n)
 var lengthOfLIS = function(nums) {
     if (!nums.length) return 0
     let max = 1, n = nums.length
-    const memo = [...new Array(n+1)]
+    const dp = [...new Array(n+1)].fill(0)
     
-    const dp = (i) => {
-        if (i >= n) return memo[i] = 0;
-        if (i === n-1) return memo[i] = 1;
-        if (memo[i] !== undefined) return memo[i]
-        let v = 0
-        dp(i+1)
-        for (let j = i+1; j < n; j++) {
+    for (let i = n-1; i >= 0; i--) {
+        let v = 0;
+        for (let j = n-1; j >= i+1; j--) {
             if (nums[i] < nums[j]) {
-                v = Math.max(v, dp(j))
+                v = Math.max(v, dp[j])
             }
         }
-        max = Math.max(max, v+1)
-        return memo[i] = v + 1
+        dp[i] = v + 1
+        max = Math.max(max, dp[i])
     }
-    dp(0)
+    
     return max
 };
