013c885868e303b6a7962e0aab35c41f94c62787
Daniel Ball <g.thirteen333@gmail.com>
2024-05-30 19:57:46 -0400

    Time: 137 ms (71.87%) | Memory: 56.7 MB (33.28%) - LeetSync

--- 494-target-sum/target-sum.js
+++ 494-target-sum/target-sum.js
@@ -10,12 +10,11 @@ var findTargetSumWays = function(nums, target) {
     const dp = (i, tgt) => {
         if (i >= n) return tgt === target ? 1 : 0
         let k = i + ":" + tgt
-        let v1 = 0
         if (memo.has(k)) return memo.get(k)
 
         let way1 = dp(i+1, tgt - nums[i]), way2 = dp(i+1, tgt + nums[i])
 
-        memo.set(k, way1 + way2 + v1)
+        memo.set(k, way1 + way2)
         return way1 + way2
     }
     return dp(0, 0)
