c5b60d18ff9ab0c8b7d8eb55fcdee5b92e149c74
Daniel Ball <g.thirteen333@gmail.com>
2024-06-01 00:18:55 -0400

    Time: 55 ms (81.67%) | Memory: 49.3 MB (88.33%) - LeetSync

--- 1130-last-stone-weight-ii/last-stone-weight-ii.js
+++ 1130-last-stone-weight-ii/last-stone-weight-ii.js
@@ -2,21 +2,22 @@
  * @param {number[]} stones
  * @return {number}
  */
+ 
 // Time and Space: O(n * m)...n for stones and m for half the total sum
 var lastStoneWeightII = function(stones) {
     let sum = stones.reduce((acc, v) => acc + v)
     let half = Math.ceil(sum / 2), n = stones.length
-    let memo = [...new Array(half+1)].map(a => [...new Array(n+1)])
+    let memo = [...new Array(half+1)]
 
     const dp = (s , i) => {
         if (i >= n || s >= half) {
             let diff = sum - s
             return Math.abs(s - diff)
         }
-        if (memo[s][i] !== undefined) return memo[s][i]
+        if (memo[s] !== undefined) return memo[s]
 
         let v1 = dp(s + stones[i], i+1), v2 = dp(s, i+1)
-        return memo[s][i] = Math.min(v1, v2)
+        return memo[s] = Math.min(v1, v2)
     }
     return dp(0, 0)
 };
