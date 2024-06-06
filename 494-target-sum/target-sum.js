6f3f6cb23c9e8575e52cd15aa57e4d7116b148f4
Daniel Ball <g.thirteen333@gmail.com>
2024-06-06 18:39:03 -0400

    Time: 94 ms (80.28%) | Memory: 55.7 MB (44.12%) - LeetSync

--- 494-target-sum/target-sum.js
+++ 494-target-sum/target-sum.js
@@ -5,17 +5,21 @@
  */
 var findTargetSumWays = function(nums, target) {
     
-    let n = nums.length
-    const memo = new Map()
-    const dp = (i, tgt) => {
-        if (i >= n) return tgt === target ? 1 : 0
-        let k = i + ":" + tgt
-        if (memo.has(k)) return memo.get(k)
+    const n = nums.length
+    const sum = nums.reduce((acc, v ) => acc + v, 0)
+    if (sum < Math.abs(target)) return 0
+    target = sum + target
+    if (target & 1) return 0
+    target = Math.floor(target / 2)
 
-        let way1 = dp(i+1, tgt - nums[i]), way2 = dp(i+1, tgt + nums[i])
+    const memo = [...new Array(n+ 1)].map(a => [...new Array( target+ 1)])
 
-        memo.set(k, way1 + way2)
-        return way1 + way2
+    const dp = (idx, t) => {
+        if (idx < 0) return t === 0 ? 1 : 0
+        if (memo[idx][t] !== undefined) return memo[idx][t]
+
+        let v1 = dp(idx-1, t), v2 = dp(idx-1, t - nums[idx])
+        return memo[idx][t] = v1 + v2
     }
-    return dp(0, 0)
+    return dp(n-1, target)
 };
