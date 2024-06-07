355eb87a27a450a8a50ff7a8613915f7003b123b
Daniel Ball <g.thirteen333@gmail.com>
2024-06-06 22:22:21 -0400

    Time: 99 ms (78.55%) | Memory: 55.4 MB (47.06%) - LeetSync

--- 494-target-sum/target-sum.js
+++ 494-target-sum/target-sum.js
@@ -3,28 +3,24 @@
  * @param {number} target
  * @return {number}
  */
+// Top Down
 var findTargetSumWays = function(nums, target) {
-    
-    const n = nums.length;
-    const sum = nums.reduce((acc, v ) => acc + v, 0);
-    if (sum < Math.abs(target)) return 0;
-    target = sum + target;
-    if (target & 1) return 0;
-    target = Math.floor(target / 2);
+    let n = nums.length
+    let sum = nums.reduce((acc, v) => acc + v, 0)
+    if (sum < Math.abs(target)) return 0
+    target += sum
+    if (target & 1) return 0
+    target = Math.floor(target / 2)
 
-    const dp = [...new Array(n+ 1)].map(a => [...new Array( target+ 1)].fill(0))
+    let memo = [...new Array(n+1)].map(a => [...new Array( target + 1)])
 
-    for (let arr of dp) arr[0] = 1
+    const dp = (idx, t) => {
+        if (idx < 0) return t === 0 ? 1 : 0
+        if (memo[idx][t] !== undefined) return memo[idx][t]
 
-    for (let r = 1; r <= n; r++) {
-        for (let c = 0; c <= target; c++) {
+        let v1 = dp(idx -1, t), v2 = dp(idx - 1, t - nums[idx])
 
-            if (c - nums[r-1] >= 0) {
-                dp[r][c] = dp[r-1][c - nums[r-1]] +  dp[r-1][c]
-            } else {
-                dp[r][c] =  dp[r-1][c]
-            }
-        }
+        return memo[idx][t] = v1 + v2
     }
-    return dp[dp.length-1][dp[0].length-1]
+    return dp(n-1, target)
 };
