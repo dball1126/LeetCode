a0b44b3647b7848c7488fe48b0e256efa5fecd33
Daniel Ball <g.thirteen333@gmail.com>
2024-06-07 23:53:40 -0400

    Time: 62 ms (57.93%) | Memory: 51.5 MB (50.08%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.js
@@ -10,35 +10,29 @@
  * @param {ListNode} list2
  * @return {ListNode}
  */
-// Linked List traversal
-// Time: O(n + m)
-// Space: O(1)...output doesn't count
 var mergeTwoLists = function(list1, list2) {
-    let newHead = null, curr = null;
-    
+    let head = null, curr= null
+
     while (list1 || list2) {
-        let v1 = list1 ? list1 : {val: Infinity}
-        let v2 = list2 ? list2 : {val: Infinity}
-        let temp = null;
-        if (v1.val <= v2.val) {
-            temp = v1;
-            list1 = list1.next;
+        let v1 = Infinity, v2 = Infinity
+        if (list1) v1 = list1.val;
+        if (list2) v2 = list2.val;
+        let tgt = Infinity
+        if (v1 <= v2) {
+            tgt = list1;
+            list1 = list1.next
         } else {
-            temp = v2;
-            list2 = list2.next;
+            tgt = list2
+            list2 = list2.next
         }
-        if (curr) {
-            curr.next = temp;
-        }
-        curr = temp;
-        if (!newHead) newHead = curr;
-        if (!list1 && list2) {
-            curr.next = list2
-            break;
-        } else if (list1 && !list2) {
-            curr.next = list1;
-            break;
+
+        if (!head) {
+            head = tgt
+            curr = tgt
+        } else {
+            curr.next =tgt;
+            curr = tgt;
         }
     }
-    return newHead;
+    return head;
 };
