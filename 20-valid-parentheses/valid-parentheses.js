b42b95d18fc156099cc6a0837e9045f7778e02c1
Daniel Ball <g.thirteen333@gmail.com>
2024-06-07 22:36:43 -0400

    Time: 62 ms (33.09%) | Memory: 50.3 MB (48.07%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -1,20 +1,21 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
 // Stack
-// Time: O(n)
-// Space: O(1)
+// Time and Space: O(n)
 var isValid = function(s) {
-    const stack = [], n = s.length;
+    let stack = [], n = s.length
 
     for (let i = 0; i < n; i++) {
-        const v = s[i];
-        if ((v === ")" && "(" !== stack.pop())) {
-            return false;
-        } else if (v === "]" && "[" !== stack.pop()) {
-            return false;
-        } else if (v === "}" && "{" !== stack.pop()) {
-            return false;
-        }
+        let v = s[i]
         if (v === "(" || v === "[" || v === "{") {
             stack.push(v)
+        } else {
+            let v2 = stack.pop();
+            if (v === ")" && v2 !== "(") return false;
+            if (v === "]" && v2 !== "[") return false;
+            if (v === "}" && v2 !== "{") return false;
         }
     }
     return stack.length === 0
