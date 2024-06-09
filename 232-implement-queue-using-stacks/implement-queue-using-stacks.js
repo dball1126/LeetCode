571a1f10016de002d1a66a6952679c7d2bfd3605
Daniel Ball <g.thirteen333@gmail.com>
2024-06-09 01:23:09 -0400

    Time: 56 ms (25.23%) | Memory: 48.8 MB (52.27%) - LeetSync

---
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.js
@@ -0,0 +1,38 @@
+var MyQueue = function() {
+    this.stack1 = []; this.stack2 = []
+};
+
+/** 
+ * @param {number} x
+ * @return {void}
+ */
+MyQueue.prototype.push = function(x) {
+    while (this.stack1.length) {
+        this.stack2.push(this.stack1.pop())
+    }
+    this.stack2.push(x)
+    while (this.stack2.length) {
+        this.stack1.push(this.stack2.pop())
+    }
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.pop = function() {
+    return this.stack1.pop();
+};
+
+/**
+ * @return {number}
+ */
+MyQueue.prototype.peek = function() {
+    return this.stack1[this.stack1.length-1]
+};
+
+/**
+ * @return {boolean}
+ */
+MyQueue.prototype.empty = function() {
+    return this.stack1.length === 0
+};
