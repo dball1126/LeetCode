d7eafa5f2463eccbe7fa1041f2ed43a786712482
Daniel Ball <g.thirteen333@gmail.com>
2024-06-10 21:44:29 -0400

    Time: 64 ms (25.29%) | Memory: 49.2 MB (77.39%) - LeetSync

---
+++ 67-add-binary/add-binary.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} a
+ * @param {string} b
+ * @return {string}
+ */
+var addBinary = function(a, b) {
+    a = BigInt(`0b${a}`), b = BigInt(`0b${b}`)
+    return (a + b).toString(2)
+};
