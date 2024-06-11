12942c69ef463b27e26b381aca965b104331e8c2
Daniel Ball <g.thirteen333@gmail.com>
2024-06-10 22:04:39 -0400

    Time: 62 ms (70.29%) | Memory: 56.7 MB (76.81%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.js
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.js
@@ -10,24 +10,19 @@
  * @param {TreeNode} root
  * @return {number}
  */
-// Recursive Depth-First-Search
-// Time: O(n)
-// Space: O(h) if tree is balanced
 var diameterOfBinaryTree = function(root) {
-
-    let diameter = 0
-
+    let max = 0;
+    
     const dfs = (nde) => {
-        if (!nde) return 0
+        if (!nde) return 0;
 
+        let left = dfs(nde.left), right = dfs(nde.right);
+        
+        max = Math.max(left + right, left, right, max);
 
-
-        let left = dfs(nde.left), right = dfs(nde.right)
-        diameter = Math.max(diameter, left, right, left + right)
-
-        return Math.max(left, right) + 1
+        return Math.max(left, right) + 1;
     }
-    dfs(root)
 
-    return diameter
+    dfs(root);
+    return max;
 };
