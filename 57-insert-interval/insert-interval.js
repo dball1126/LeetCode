9d9ebb1e709f9c67fc28238adba64861deae0c83
Daniel Ball <g.thirteen333@gmail.com>
2024-06-12 21:36:04 -0400

    Time: 86 ms (15.51%) | Memory: 56.6 MB (7.65%) - LeetSync

--- 57-insert-interval/insert-interval.js
+++ 57-insert-interval/insert-interval.js
@@ -3,31 +3,23 @@
  * @param {number[]} newInterval
  * @return {number[][]}
  */
-// Merging arrays
-// Time: O(n)
-// Space: O(1)...not counting output array
 var insert = function(intervals, newInterval) {
-    let [x1, y1] = newInterval, merged = false;
-    const arr = []
+    let merged = false, [x1, y1] = newInterval, result = []
+
     for (let [x, y] of intervals) {
         if (merged) {
-            arr.push([x, y])
-            continue;
-        }
-        
-        if (y1 < x) {
-            arr.push([x1, y1]);
-            arr.push([x, y])
+            result.push([x, y]);
+        } else if (y1 < x) {
+            result.push([x1, y1], [x, y])
             merged = true;
-        } else if (((x1 >= x && x1 <= y ) || ( y1 >= x && y1 <= y)) ||
-                    ((x >= x1 && x <= y1 ) || ( y >= x1 && y <= y1)))  {
-            x1 = Math.min(x, x1)
-            y1 = Math.max(y, y1)
+        } else if (x1 > y) {
+            result.push([x, y])
         } else {
-            arr.push([x, y])
+            x1 = Math.min(x1, x);
+            y1 = Math.max(y1, y)
         }
-
     }
-    if (!merged) arr.push([x1, y1])
-    return arr
+
+    if (!merged) result.push([x1, y1])
+    return result;
 };
