a4c9f3db78d0cbd35b5a0354ec301b5ed771640b
Daniel Ball <g.thirteen333@gmail.com>
2024-06-15 02:07:03 -0400

    Time: 69 ms (61.37%) | Memory: 56.2 MB (31.24%) - LeetSync

--- 207-course-schedule/course-schedule.js
+++ 207-course-schedule/course-schedule.js
@@ -3,33 +3,29 @@
  * @param {number[][]} prerequisites
  * @return {boolean}
  */
-// Topological Sort
-// Time & Space: O(V + E)
 var canFinish = function(numCourses, prerequisites) {
-    const indegrees = new Array(numCourses).fill(0)
-    let adjList = new Map(), queue = [], count = 0, visited = new Set()
-
-    for(let [x, y] of prerequisites) {
-        if (!adjList.has(x)) adjList.set(x, new Set())
-        indegrees[y] = indegrees[y] + 1
-        adjList.get(x).add(y)
+    let count = 0, adjList = new Map(), queue = []
+    const indegrees = [...new Array(numCourses).fill(0)]
+    for (let [e, v] of prerequisites) {
+        indegrees[e]++
+        if (!adjList.has(v)) adjList.set(v, new Set())
+        adjList.get(v).add(e)
     }
-    indegrees.forEach((n, i) => n === 0 ? queue.push(i) : '')
+    for (let i = 0; i < numCourses; i++) {
+        if (indegrees[i] === 0) queue.push(i)
+    }
     while (queue.length) {
         let nde = queue.shift()
-        if (visited.has(nde)) return false
-        count += 1
-        if (numCourses === count) return true
-        visited.add(nde)
+        count++
         if (adjList.has(nde)) {
-            let arr = Array.from(adjList.get(nde))
-            arr.forEach(n => {
-                indegrees[n] = indegrees[n] - 1
-                if (indegrees[n] === 0) {
-                    queue.push(n)
-                }
-            })
+            let set = adjList.get(nde)
+            for (let edge of Array.from(set)) {
+                let eSet = adjList.get(edge)
+                if (eSet && eSet.has(nde)) return false;
+                indegrees[edge]--
+                if (indegrees[edge] === 0) queue.push(edge)
+            }
         }
     }
-    return count === numCourses ? true : false
+    return count === numCourses
 };
