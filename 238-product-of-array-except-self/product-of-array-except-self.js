60d09237325f552d1b6dccfc0014c10eefec8722
Daniel Ball <g.thirteen333@gmail.com>
2024-06-15 20:16:00 -0400

    Time: 89 ms (94.08%) | Memory: 63.7 MB (63.00%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.js
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -2,20 +2,23 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+// Prefix Sums. Dynamic Programming
+// Time: O(n)
+// Space: O(1)
 var productExceptSelf = function(nums) {
     const prefixSums = [], n = nums.length
     let sum = 1
 
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < n; i++) { // cumulative sums left to right
         sum *= nums[i]
         prefixSums.push(sum)
     }
-    sum = 1
+    sum = 1 // after
 
-    for (let i = n-1; i >= 0; i--) {
+    for (let i = n-1; i >= 0; i--) { // cumulative sums right to left
         let prev = 1;
-        if (i-1 >= 0) prev = prefixSums[i-1]
-        prefixSums[i] = prev * sum
+        if (i-1 >= 0) prev = prefixSums[i-1] 
+        prefixSums[i] = prev * sum // multiply prev and after 
         sum *= nums[i]
     }
     return prefixSums
