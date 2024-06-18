565651d888bf6de19ec50732d7895ca9422cddaa
Daniel Ball <g.thirteen333@gmail.com>
2024-06-17 21:40:24 -0400

    Time: 124 ms (10.43%) | Memory: 55.2 MB (39.06%) - LeetSync

--- 200-number-of-islands/number-of-islands.js
+++ 200-number-of-islands/number-of-islands.js
@@ -2,27 +2,24 @@
  * @param {character[][]} grid
  * @return {number}
  */
-// Depth First search
-// Time and Space: O(n * m)...rows * cols...dirs is constant at 4 so it is dropped.
+// Recursive Depth-First-Search
+// Time and Space: O(n * m)
 var numIslands = function(grid) {
-    
-    const n = grid.length, m = grid[0].length
+    let n = grid.length, m = grid[0].length, islands = 0
+    const dirs = [[-1,0],[1,0],[0,-1],[0,1]]
     const visited = [...new Array(n)].map(a => [...new Array(m)].fill(false))
-    const dirs = [[1,0], [-1,0], [0,1], [0, -1]]
-    let islands = 0;
-    const dfs = (i, j) => {
-        if (i < 0 || i >= n || j < 0 || j >= m) return;
-        if (visited[i][j] || grid[i][j] !== '1') return
-        visited[i][j] = true;
 
-        for (let [x, y] of dirs) {
-            dfs(i+x, (y+j))
-        }
+    const dfs = (r, c) => {
+        if (r < 0 || c < 0 || r >= n || c >= m) return;
+        if (visited[r][c] || grid[r][c] !== '1') return;
+        visited[r][c] = true;
+
+        for (let [x, y] of dirs) dfs (r + x, c + y)
     }
 
     for (let r = 0; r < n; r++) {
         for (let c = 0; c < m; c++) {
-            if (grid[r][c] === '1' && !(visited[r][c])) {
+            if (!visited[r][c] && grid[r][c] === '1') {
                 islands++
                 dfs(r, c)
             }
