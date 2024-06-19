b996ea59fc18147146bda920f5f68215393e8958
Daniel Ball <g.thirteen333@gmail.com>
2024-06-19 16:08:56 -0400

    Time: 58 ms (70.46%) | Memory: 51.4 MB (42.69%) - LeetSync

--- 139-word-break/word-break.js
+++ 139-word-break/word-break.js
@@ -1,40 +1,47 @@
-/**
- * @param {string} s
- * @param {string[]} wordDict
- * @return {boolean}
- */
-// Top-down Dynamic Programming
-// Time: O(n * m * k)...n for s length and m for number of words in wordDict...k for word length
-// Space: O(n)
+class TNode {
+    constructor() {
+        this.isWord = false; this.keys = new Map()}
+}
+
 var wordBreak = function(s, wordDict) {
-    let n = s.length
-    let memo = [...new Array(n)]
-    const checkWord = (i, w) => {
-        if (s[i] !== w[0]) return false;
-        let c = 0
-        while (s[i] === w[c] && c < w.length) {
-            i++; c++
+    const trie = new TNode(), n = s.length
+
+    const insert = (w) => {
+        let n = w.length, root = trie
+        for (let i = 0; i < n; i++) {
+            let v = w[i]
+            if (!root.keys.has(v)) {
+                let nde = new TNode()
+                root.keys.set(v, nde)
+            }
+            root = root.keys.get(v)
+            if (i === n-1) root.isWord = true
         }
-        return c === w.length
     }
-    const dp = (idx) => {
-        if (idx === n) {
-            return true
-        }
-        if (idx > n) return false
-        if (memo[idx] !== undefined) {
-            return memo[idx]
-        }
-        let pos = false
-        for (let i = 0; i < wordDict.length; i++) {
-            if (wordDict[i][0] === s[idx]) {
-                let valid = checkWord(idx, wordDict[i])
-                if (valid) {
-                    pos  = pos || dp(idx + wordDict[i].length)
+
+    for (let w of wordDict) insert(w)
+    let set = new Set()
+
+    const search = (idx) => {
+        if (idx >= n) return true;
+        if (set.has(idx)) return
+        set.add(idx)
+        let root = trie;
+
+        for (let i = idx; i < s.length; i++) {
+            let v = s[i]
+            if (!root.keys.has(v)) return false;
+            root = root.keys.get(v)
+            if (root.isWord) {
+                if (i === n-1) {
+                    return true;
+                } else if (search(i+1)) {
+                    return true;
                 }
             }
         }
-        return memo[idx] = pos
+        return false
     }
-   return dp(0)
+
+    return search(0)
 };
