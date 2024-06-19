e3c03f690906276006ece6b7e25cd3ad639f29e3
Daniel Ball <g.thirteen333@gmail.com>
2024-06-19 07:40:39 -0400

    Time: 150 ms (6.51%) | Memory: 61 MB (8.68%) - LeetSync

--- 56-merge-intervals/merge-intervals.js
+++ 56-merge-intervals/merge-intervals.js
@@ -3,21 +3,21 @@
  * @return {number[][]}
  */
 var merge = function(intervals) {
-    if (!intervals.length) return intervals;
-
     intervals.sort((a, b) => a[0] - b[0])
-    let x1, y1;
-    let result = []
-    for (let [x2, y2] of intervals ) {
+
+    let x1, y1, result = []
+
+    for (let [x, y] of intervals) {
 
         if (x1 === undefined) {
-            x1 = x2; y1 = y2;
-        } else if (x2 >= x1 && x2 <= y1) {
-            y1 = Math.max(y1, y2)
+            x1 = x, y1 = y
+        } else if (y1 >= x) {
+            x1 = Math.min(x1, x), y1 = Math.max(y, y1)
         } else {
             result.push([x1, y1])
-            x1 = x2; y1 = y2;
+            x1 = x, y1 = y
         }
+
     }
     if (x1 !== undefined) result.push([x1, y1])
     return result;
