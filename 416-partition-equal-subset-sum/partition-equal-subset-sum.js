3773d06d319fe04d8b1d53c1fa296a4713c001c6
Daniel Ball <g.thirteen333@gmail.com>
2024-06-19 16:54:34 -0400

    Time: 534 ms (18.88%) | Memory: 81.5 MB (37.48%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.js
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.js
@@ -2,31 +2,19 @@
  * @param {number[]} nums
  * @return {boolean}
  */
-// Bottom-Up Dynamic Programming
-// 0/1 Knapsack (Bounded)
-// Time And Space: O(n * m)...n for length of nums and m for half the total sum
 var canPartition = function(nums) {
-    
-    let sum = nums.reduce((acc, v) => acc + v, 0), n = nums.length
+    let sum = nums.reduce((acc, v) => acc + v)
+    if (sum % 2 === 1) return false;
+    let half = Math.floor(sum / 2), n = nums.length;
 
-    if (sum & 1) return false
-    sum = Math.floor(sum / 2)
-    const dp = [...new Array(n+1)].map(a => [...new Array( sum + 1)].fill(false))
+    const memo = [...new Array(half+1)].map(a => [...new Array(n+1)])
 
-    for (let arr of dp) arr[0] = true; // base case
-
-    const row = dp.length, column = dp[0].length; 
-
-    for (let r = 1; r < row; r++) {
-
-        for (let c = 0; c < column; c++) {
-
-            if (c - nums[r - 1] >= 0) {
-                dp[r][c] = dp[r-1][c] || dp[r-1][c - nums[r - 1]]
-            } else {
-                dp[r][c] = dp[r-1][c]
-            }
-        }
+    const dp = (i, a) => {
+        if (a === 0) return true;
+        if (a < 0 || i >= n) return false;
+        if (memo[a][i] !== undefined) return memo[a][i]
+        let val1 = dp(i+1, a), val2 = dp(i+1, a - nums[i])
+        return memo[a][i] = val1 || val2
     }
-    return dp[row-1][column-1]
+    return dp(0, half)
 };
