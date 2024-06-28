b1d1de19726c15b4a114a34d59050632acd11cde
Daniel Ball <g.thirteen333@gmail.com>
2024-06-28 02:02:26 -0400

    Time: 65 ms (29.92%) | Memory: 51.7 MB (28.68%) - LeetSync

---
+++ 861-flipping-an-image/flipping-an-image.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} image
+ * @return {number[][]}
+ */
+var flipAndInvertImage = function(image) {
+    for (let r = 0; r < image.length; r++) {
+
+        let i = 0, j = image[r].length-1
+        while (i < j) {
+            [image[r][i], image[r][j]] = [image[r][j], image[r][i]]
+            i++; j--
+        }
+
+        for (let c = 0; c < image[r].length; c++) {
+            image[r][c] ^= 1
+        }
+    }
+    return image
+};
