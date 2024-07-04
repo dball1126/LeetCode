7976d1797a730975687c48b17d6d5c0f99aa240e
Daniel Ball <g.thirteen333@gmail.com>
2024-07-03 21:45:17 -0400

    Time: 53 ms (53.26%) | Memory: 48 MB (98.84%) - LeetSync

--- 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
+++ 94-binary-tree-inorder-traversal/binary-tree-inorder-traversal.js
@@ -6,23 +6,18 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
-/**
- * @param {TreeNode} root
- * @return {number[]}
- */
-// left, root, right
+// Left, Root, Right
 var inorderTraversal = function(root) {
     let inorder = [], stack = [], curr = root;
 
-    while (curr || stack.length) {
+    while (stack.length || curr) {
         while (curr && curr.left) {
             stack.push(curr)
             curr = curr.left;
         }
         if (!curr) curr = stack.pop();
-
         inorder.push(curr.val)
-        curr = curr.right;
+        curr = curr.right
     }
-    return inorder;
+    return inorder
 };
