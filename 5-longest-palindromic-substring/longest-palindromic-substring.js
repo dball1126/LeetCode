a9bbd5d9e41a3c559ad5831fa739ddcfae57baf7
Daniel Ball <g.thirteen333@gmail.com>
2024-07-05 06:47:08 -0400

    Time: 71 ms (88.59%) | Memory: 51.4 MB (69.83%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -2,32 +2,29 @@
  * @param {string} s
  * @return {string}
  */
-// Top-down Dynamic Programming
-// Palindromes
-// Time & Space: O(n^2)
-var longestPalindrome = function(str) {
-    let longest = ""
-    let n = str.length
-    let memo = [...new Array(n)].map(a => [...new Array(n)])
-
-    const dp  = (i, j, curr) => {
-        if (i < 0 || j >= n) return ""
-        if (str[i] !== str[j]) return ""
-        if (memo[i][j] !== undefined) return memo[i][j]
-        curr = str[i] + curr + str[j]
-
-        if (longest.length < curr.length) longest = curr;
-        dp(i-1, j+1, curr)
-        return memo[i][j] = curr
-    }
-
+// Palindromes. Expand from Center
+// Time: O(n^2)
+// Space: O(1)
+var longestPalindrome = function(s) {
+    let n = s.length, minL = 0, maxL = 0
     for (let i = 0; i < n; i++) {
-        if (longest.length < 1) longest = str[i]
-        dp(i-1, i+1, str[i])
-        if (str[i] === str[i+1]) {
-            if (longest.length < 2) longest = str[i] + str[i+1]
-            dp(i-1, i+2, str[i] + str[i+1])
+        let j = i-1, k = i+1 // handle one char at start
+        while (j >= 0 && k < n && s[j] === s[k]) { 
+            
+            if (k - j > maxL - minL) {
+                minL = j; maxL = k
+            }
+            j-=1; k++
+
+        }
+        j = i; k = i+1 // handle two chars equal at start
+        while (j >= 0 && k < n && s[j] === s[k]) { 
+            if (k - j > maxL - minL) {
+                minL = j; maxL = k
+            }
+            j-=1; k++
         }
     }
-    return longest
+    return  s.substring(minL, maxL+1)
 };
+
