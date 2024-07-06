c6ff381404016ef0cf9fe4087912841eb14f6a88
Daniel Ball <g.thirteen333@gmail.com>
2024-07-06 06:54:58 -0400

    Time: 522 ms (39.32%) | Memory: 51.3 MB (49.47%) - LeetSync

--- 79-word-search/word-search.js
+++ 79-word-search/word-search.js
@@ -3,40 +3,31 @@
  * @param {string} word
  * @return {boolean}
  */
-// Time: O(n * m * w * 4)...n for rows...m for cols...w for length of word and 4 for dirs (we can drop the 4)
-// Space: O(n * m * m)
+// Backtracking
+// TIme: O((n*m)^4)
+// Space: O((n*m) + k)...n for rows...m for cols...k for word length
 var exist = function(board, word) {
-    let n = board.length, m = board[0].length
-    let wL = word.length
-    const dirs = [[1,0],[-1,0],[0,1],[0,-1]]
-    let pathFound = false;
+    
+    const dirs = [[-1,0], [1,0],[0,1],[0,-1]]
+    const n = board.length, m = board[0].length
+    const visit = board.map(a => a.map(v => false))
+    const backtrack = (r, c, idx) => {
+        if (r <0 || c < 0 || r >= n || c >= m || idx >= word.length || board[r][c] !== word[idx] || visit[r][c]) return false
+        if (idx === word.length-1 && board[r][c] === word[idx]) return true;
+        visit[r][c] = true
+        for (let [x, y] of dirs) {
+            if (backtrack(r+x, c+y, idx+1)) return true
 
-    const wordSearch = (r, c, i, visited) => {
-        if (pathFound) return true;
-        if (i === wL-1 && board[r][c] === word[i]) return true;
-
-        visited[r][c] = true;
-        for (let [x, y] of dirs) { // 4
-
-            if ((r+x) < 0 || (c +y) < 0 || (r+x) >= n || (c+y)  >= m) continue;
-            if (board[r+x][c+y] !== word[i+1] || visited[r+x][c+y]) continue;
-
-            visited[r+x][c+y] = true;
-            let result = wordSearch(r+x, c+y, i+1, visited)
-            visited[r+x][c+y] = false;
-            if (result) return pathFound = true;
         }
+        visit[r][c] = false
         return false;
     }
 
-    for (let r = 0; r < n; r++) { 
+    for (let r = 0; r < n; r++) {
         for (let c = 0; c < m; c++) {
-            if (board[r][c] === word[0]) {
-                const visited = [...new Array(n)].map(a => [...new Array(m)].fill(false))
-                let result = wordSearch(r, c, 0, visited)
-                if (result) return true;
-            }
+            if (backtrack(r, c, 0)) return true;
         }
     }
+
     return false;
 };
