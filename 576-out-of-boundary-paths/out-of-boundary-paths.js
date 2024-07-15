a68a631481d7bddb7531dfa565713430b54dc1c0
Daniel Ball <g.thirteen333@gmail.com>
2024-07-14 23:12:17 -0400

    Time: 111 ms (37.84%) | Memory: 60.5 MB (37.84%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.js
+++ 576-out-of-boundary-paths/out-of-boundary-paths.js
@@ -11,6 +11,12 @@ var findPaths = function(m, n, maxMove, startRow, startColumn) {
     let dp = [], dirs = [[-1,0],[1,0],[0, 1], [0, -1]]
     dp = [...new Array(maxMove+1)].map(a => [...new Array(m2)].map(a => [...new Array(n2)].fill(0)))
 
+    let rMin = (startRow - maxMove >= 0) ? startRow - maxMove  : 1
+    let rMax = (startRow + maxMove <= m) ? startRow + maxMove : m
+    let cMin = (startColumn - maxMove >= 0) ? startColumn - maxMove + 1 : 1
+    let cMax = (startColumn + maxMove <= n) ? startColumn + maxMove : n
+
+
     for (let s = 0; s <= maxMove; s++) { // create dp grid
         for (let r = 0; r < m2; r++) {
             for (let c = 0; c < n2; c++) {
