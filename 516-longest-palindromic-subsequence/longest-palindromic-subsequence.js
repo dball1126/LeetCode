c04f8b5455431711ae3b09e464d11c69d4407b6c
Daniel Ball <g.thirteen333@gmail.com>
2024-07-25 21:32:26 -0400

    Time: 251 ms (35.92%) | Memory: 81.1 MB (48.94%) - LeetSync

---
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.js
@@ -0,0 +1,33 @@
+var longestPalindromeSubseq = function(str) {
+    if (!str) return 0;
+    let n = str.length, max = 1;
+
+    const dp = [...new Array(n+1)].map(a =>
+               [...new Array(n+1)].fill(1))
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i-1; j >= 0; j--) {
+            let v1 = 1
+            if (str[i] === str[j]) {
+                if (j+1 === i) {
+                    v1 = 2
+                } else {
+                    v1 = 2 + dp[j+1][i-1]
+                }
+            }
+            let v2 = dp[j+1][i] || 0, v3 = dp[j][i-1] || 0
+
+            if (v1 >= v2 && v1 >= v3) {
+                dp[j][i] = v1
+            } else if (v2 >= v1 && v2 >= v3) {
+                dp[j][i] = v2
+            } else {
+                dp[j][i] = v3
+            }
+            if (dp[j][i] > max) {
+                max = dp[j][i]
+            }
+        }
+    }
+    return max
+};
