7da158923b6ce3684c09e073d679b94d84dcb715
Daniel Ball <g.thirteen333@gmail.com>
2024-07-25 22:31:10 -0400

    Time: 180 ms (16.08%) | Memory: 71 MB (89.01%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.js
+++ 1250-longest-common-subsequence/longest-common-subsequence.js
@@ -4,15 +4,16 @@
  * @return {number}
  */
 var longestCommonSubsequence = function(text1, text2) {
-    const n = text1.length, m = text2.length
-    const dp = [...new Array(n+1)].map(a => [...new Array(m+1)].fill(0))
+    let n = text1.length, m = text2.length
+    const memo = [...new Array(n+1)].map(a => [...new Array(m+1)])
 
-    for (let i = 1; i <= n; i++) {
-        for (let j = 1; j <= m; j++) {
-            let v = text1[i-1] === text2[j-1] ? 1 + dp[i-1][j-1] : 0
-            dp[i][j] = Math.max(v, dp[i-1][j], dp[i][j-1])
-        }
+    const dp = (i, j) => {
+        if (i >= n || j >= m) return 0
+        if (memo[i][j] !== undefined) return memo[i][j]
+        let v1 = 0
+        if (text1[i] === text2[j]) v1 = 1 + dp(i+1, j+1)
+        let v2 = dp(i+1, j), v3 = dp(i, j+1)
+        return memo[i][j] = Math.max(v1,v2,v3)
     }
-    return dp[n][m]
+    return dp(0,0)
 };
-
