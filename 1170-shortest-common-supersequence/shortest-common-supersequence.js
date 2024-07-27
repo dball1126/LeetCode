e73bb7e7b03dd811fca79cc4d0fe984e4df03c05
Daniel Ball <g.thirteen333@gmail.com>
2024-07-27 19:03:19 -0400

    Time: 437 ms (8.86%) | Memory: 102.6 MB (16.46%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.js
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.js
@@ -1,11 +1,8 @@
-/**
- * @param {string} str1
- * @param {string} str2
- * @return {string}
- */
+// Bottom-Up Dynamic Programming
+// Time and Space: O(n * m)
 var shortestCommonSupersequence = function(str1, str2) {
     let n = str1.length, m = str2.length
-    const dp = [...new Array(n+1)].map(a => [...new Array(m+1)].fill(""))
+    const dp = [...new Array(n+1)].map(a => [...new Array(m+1)].fill("")) // this can be converted to string length instead
 
     for (let i = n-1; i >= 0; i--) { // Bottom-Up Longest Common Subsequence
         for (let j = m-1; j >= 0; j--) {
@@ -25,12 +22,12 @@ var shortestCommonSupersequence = function(str1, str2) {
     let i = 0, j = 0, idx = 0, seq = dp[i][j], result = ""
 
     while (idx < seq.length || i < n || j < m) {
-        if (str1[i] === seq[idx] && str2[j] === seq[idx]) {
+        if (str1[i] === seq[idx] && str2[j] === seq[idx]) { // common char
             result += seq[idx];
             idx++; i++; j++
-        } else if (i < n && str1[i] !== seq[idx]) {
+        } else if (i < n && str1[i] !== seq[idx]) { // missing char from str1
             result += str1[i]; i++;
-        } else if (j < m && str2[j] !== seq[idx]) {
+        } else if (j < m && str2[j] !== seq[idx]) { // missing char from str2
             result += str2[j]; j++;
         }
     }
