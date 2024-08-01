0ea93c07b288b86a35cae4be5f0e2a3781547563
Daniel Ball <g.thirteen333@gmail.com>
2024-07-31 21:47:22 -0400

    Time: 70 ms (84.39%) | Memory: 57.8 MB (15.24%) - LeetSync

--- 494-target-sum/target-sum.js
+++ 494-target-sum/target-sum.js
@@ -3,19 +3,23 @@
  * @param {number} target
  * @return {number}
  */
+// Bottom Up Dynamic Programming ( 0/1 Knapsack )
+// Time && Space: O(n * m)...n for # of nums...m for half of total sum + target
 var findTargetSumWays = function(nums, target) {
-    let n = nums.length
-    const memo = new Map()
-    const dp = (i, s) => {
-        if (i >= n) {
-            return s === target ? 1 : 0
-        }
-        let k = i + ":" + s
-        if (memo.has(k)) return memo.get(k)
+    let n = nums.length, half = nums.reduce((acc,v)=>acc+v) + target
+    if (half & 1 || half < 0) return 0 // if odd return zero
+    half = Math.floor( half / 2 )
 
-        let val = dp(i+1, s + nums[i]) + dp(i+1, s - nums[i])
-        memo.set(k, val)
-        return val
+    const dp = [...new Array(n+1)].map(a => [...new Array(half+1)].fill(0))
+    for (let arr of dp) arr[0] = 1;
+
+    for (let r = 1; r <= n; r++) {
+        for (let c = 0; c <= half; c++) {
+            dp[r][c] = dp[r-1][c] // grab the previous result
+            if (c - nums[r-1] >= 0) {
+                dp[r][c] += dp[r-1][c - nums[r-1]]
+            }
+        }
     }
-    return dp(0,0)
+    return dp[n][half]
 };
