89d4edc93a914c320d06222642942c868dea112c
Daniel Ball <g.thirteen333@gmail.com>
2024-08-01 08:33:35 -0400

    Time: 137 ms (27.33%) | Memory: 57 MB (19.22%) - LeetSync

--- 322-coin-change/coin-change.js
+++ 322-coin-change/coin-change.js
@@ -4,19 +4,22 @@
  * @return {number}
  */
 var coinChange = function(coins, amount) {
-    let dp = [...new Array(amount+1)].fill(Infinity)
-    dp[0] = 0
-
-    for (let a = 1; a <= amount; a++) {
+    let n = coins.length
+    let memo = [...new Array(amount + 1)]
+    
+    const dp = (i, a) => {
+        if (a === amount) return 0
+        if (a < 0 || i >= n) return Infinity
+        if (memo[a] !== undefined) return memo[a]
         let min = Infinity
-
-        for (let c of coins) {
-
-            if ((a - c) >= 0 && (a - c) < a) {
-                min = Math.min(min, dp[a - c] + 1)
+        for (let idx = i; idx < n; idx++) {
+            const c = coins[idx]
+            if (a + c <= amount && a + c > a) {
+                min = Math.min(min, 1 + dp(0, a + c))
             }
         }
-        dp[a] = min
+        return memo[a] = min
     }
-    return dp[amount] === Infinity ? -1 : dp[amount]
+    const val = dp(0,0)
+    return val === Infinity ? -1 : val
 };
