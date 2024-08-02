1c2a5caf5274f86cfab7d65a1e2b3d0917584d03
Daniel Ball <g.thirteen333@gmail.com>
2024-08-02 02:12:15 -0400

    Time: 62 ms (100.00%) | Memory: 53.9 MB (32.50%) - LeetSync

--- 2187-vowels-of-all-substrings/vowels-of-all-substrings.js
+++ 2187-vowels-of-all-substrings/vowels-of-all-substrings.js
@@ -2,17 +2,17 @@
  * @param {string} word
  * @return {number}
  */
-// Dynamic Programming
-// Time and Space: O(n)
 var countVowels = function(word) {
-    let n = word.length, max = 0, vowels = new Set(['a','e','i','o','u'])
-    const dp = [...new Array(n)].fill(0)
+    let vowels = new Set(['a','e','i','o','u'])
+    let count = 0, max = 0
+    for (let i = 0; i < word.length; i++) {
 
-    for (let i = 0; i < n; i++) {
-        let prev = dp[i-1] || 0, w = word[i]
-        let v = vowels.has(w) ? 1 + i : 0
-        dp[i] = prev + v
-        max += dp[i]
+        let v = word[i]
+        if (vowels.has(v)) {
+            count += 1
+            count += i
+        }
+        max += count
     }
     return max
 };
