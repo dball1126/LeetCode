cbff87c3ae4061c78468b5c1963d05a1880f1a12
Daniel Ball <g.thirteen333@gmail.com>
2024-08-10 02:53:14 -0400

    Time: 260 ms (34.41%) | Memory: 80.1 MB (46.60%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.js
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.js
@@ -1,34 +1,26 @@
+// Palindromic Subsequence
 // Bottom-Up Dynamic Programming
 // Time and Space: O(n^2)
-var longestPalindromeSubseq = function(str) {
-    if (!str) return 0;
-    let n = str.length, max = 1;
-    const dp = [...new Array(n+1)].map(a =>
-               [...new Array(n+1)].fill(1))
+var longestPalindromeSubseq = function(s) {
+    let n = s.length
 
-    for (let i = 0; i < n; i++) {
-        for (let j = i-1; j >= 0; j--) {
-            let v1 = 1
-            if (str[i] === str[j]) {
+    const dp = [...new Array(n+1)].map(a => [...new Array(n+1)].fill(0))
+    for (let i = 0; i < n; i++) dp[i][i] = 1
+                                        // For loop direction
+    for (let i = 0; i < n; i++) {       // -------------->
+        for (let j = i-1; j >= 0; j--) {// <--------------
+            let v = 0
+            if (s[i] === s[j]) {
                 if (j+1 === i) {
-                    v1 = 2
-                } else {
-                    v1 = 2 + dp[j+1][i-1]
+                    v = 2
+                } else if (i-1 >= 0 && dp[j+1][i-1] !== undefined) {
+                    v += (dp[j+1][i-1] + 2)
+                    
                 }
             }
-            let v2 = dp[j+1][i] || 0, v3 = dp[j][i-1] || 0
-
-            if (v1 >= v2 && v1 >= v3) {
-                dp[j][i] = v1
-            } else if (v2 >= v1 && v2 >= v3) {
-                dp[j][i] = v2
-            } else {
-                dp[j][i] = v3
-            }
-            if (dp[j][i] > max) {
-                max = dp[j][i]
-            }
+            let v2 = dp[j+1][i], v3 = dp[j][i-1] || 0
+            dp[j][i] = Math.max(v, v2, v3)
         }
     }
-    return max
+    return dp[0][n-1]
 };
