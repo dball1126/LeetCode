b059d20f13941702c36ef0ff857abe0381bc5d18
Daniel Ball <g.thirteen333@gmail.com>
2024-08-17 00:44:47 -0400

    Time: 45 ms (84.16%) | Memory: 48.9 MB (27.33%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.js
+++ 96-unique-binary-search-trees/unique-binary-search-trees.js
@@ -3,17 +3,15 @@
  * @return {number}
  */
 var numTrees = function(n) {
-    const memo = [...new Array(n+1)]
-
-    const dp = (num) => {
-        if (num <= 1) return 1;
-        if (memo[num] !== undefined) return memo[num]
+    const dp = [...new Array(n+1)]
+    dp[0] = 1; dp[1] = 1;
+    
+    for (let i = 1; i <= n; i++) {
         let val = 0
-
-        for (let i = 1; i <= num; i++) {
-            val += (dp(i-1) * dp(num - i))
+        for (let j = 1; j <= i; j++) {
+            val += (dp[j-1] * dp[i-j])
         }
-        return memo[num] = val
+        dp[i] = val
     }
-    return dp(n)
+    return dp[n]
 };
