df7a1a80cf97dba1828371751fd8401ea5bdec6f
Daniel Ball <g.thirteen333@gmail.com>
2024-08-17 22:53:42 -0400

    Time: 50 ms (77.91%) | Memory: 49.1 MB (10.52%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.js
+++ 1013-fibonacci-number/fibonacci-number.js
@@ -1,9 +1,10 @@
 var fib = function(n) {
-    const dp = [...new Array(n+1)]
-    dp[0] = 0;
-    dp[1] = 1;
-    for (let i = 2; i <= n; i++) {
-        dp[i] = dp[i-1]  + dp[i-2]
+    const memo = [...new Array(n+1)]
+    const dp = (num) => {
+        if (num === 0) return 0;
+        if (num === 1) return 1;
+        if (memo[num] !== undefined) return memo[num]
+        return memo[num] = dp(num-1)  + dp(num-2)
     }
-    return dp[n]
+    return dp(n)
 };
