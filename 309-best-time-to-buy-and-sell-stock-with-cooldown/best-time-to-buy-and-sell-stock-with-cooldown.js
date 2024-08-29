c1c8ec6f80873fbb3f8a4b059181790b4731980e
Daniel Ball <g.thirteen333@gmail.com>
2024-08-29 00:51:24 -0400

    Time: 58 ms (55.73%) | Memory: 51.2 MB (49.68%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.js
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.js
@@ -1,21 +1,21 @@
-/**
- * @param {number[]} prices
- * @return {number}
- */
-// Bottom-Up Dynamic Programming
-// Time and Space: O(n)
+// Top-Down Dynamic Programming
+// Time and Space: O(n * m)
 var maxProfit = function(prices) {
     let n = prices.length
-    const memo = [...new Array(n+2)].map(a => [...new Array(4)].fill(0))
+    const memo = [...new Array(n+1)].map(a => [...new Array(2)])
 
-    for (let i = n-1; i >= 0; i--) {
-        for(let c = 1; c >= 0;  c--) {
-            if (c === 1) {
-                memo[i][c] = Math.max(prices[i] + memo[i+2][0], memo[i+1][c])
-            } else {
-                memo[i][c] = Math.max(memo[i+1][1] + (-prices[i]), memo[i+1][c])
-            }
+    const dp = (idx, t) => {
+        if (idx >= prices.length && t === 0) return -Infinity
+        if (idx >= prices.length && t === 1) return 0
+        if (memo[idx][t] !== undefined) return memo[idx][t]
+        
+        let nx = dp(idx+1, t), nx2 = 0
+        if (t === 1) {
+            nx2 = dp(idx+1, 0) - prices[idx]
+        } else {
+            nx2 = dp(idx+2, 1) + prices[idx]
         }
+        return memo[idx][t] = Math.max(nx, nx2);
     }
-    return memo[0][0]
-}
+    return dp(0, 1)
+};
