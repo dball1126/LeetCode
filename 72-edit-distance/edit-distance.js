45a1bc3e02a028e6af50c0e3efa4efb7fc9bb328
Daniel Ball <g.thirteen333@gmail.com>
2024-09-14 01:23:44 -0400

    Time: 115 ms (14.53%) | Memory: 56.9 MB (29.76%) - LeetSync

--- 72-edit-distance/edit-distance.js
+++ 72-edit-distance/edit-distance.js
@@ -1,26 +1,25 @@
-/**
- * @param {string} word1
- * @param {string} word2
- * @return {number}
- */
-// Top-Down Dynamic Programming
+// Edit Distance DP Pattern
+// Bottom-Up Dynamic Programming
 // Time and Space: O(n * m)
 var minDistance = function(word1, word2) {
+    if (!word1.length || !word2.length) return Math.max(word1.length, word2.length)
     let n = word1.length, m = word2.length
-    const memo = [...new Array(n+1)].map(a => [...new Array(m+1)])
+    const dp = [...new Array(n+1)].map(a => [...new Array(m+1)].fill(Infinity))
+    for (let i = 0; i < n; i++) dp[i][0] = i;
+    for (let i = 0; i < m; i++) dp[0][i] = i;
 
-    const dp = (i, j) => {
-        if (i >= n && j >= m) return 0 // base case
-        if (i >= n || j >= m) return ((n - i) + (m-j))
-        if (memo[i] && memo[i][j] != undefined) return memo[i][j]
+    for (let i = 1; i <= n; i++) {
+        for (let j = 1; j <= m; j++) {
 
-        let v1 = Infinity, v2 = Infinity, v3 = Infinity
-        if ( word1[i] === word2[j]) {
-            v1 = dp(i+1, j+1)
-        } 
-        let v4 = 1 + dp(i+1, j+1)
-        v2 = 1 + dp(i+1, j),  v3 = 1+ dp(i, j+1)
-        return memo[i][j] = Math.min(v1,v2,v3, v4)
+            let v1 = Infinity, v2 = 1 + dp[i-1][j], v3 = 1 + dp[i][j-1]
+            if (word1[i-1] === word2[j-1]) {
+                v1 = dp[i-1][j-1]
+            } else {
+                v1 = 1 + dp[i-1][j-1]
+            }
+            dp[i][j] = Math.min(v1,v2,v3)
+        }
     }
-    return dp(0,0)
-};
+
+    return dp[n][m] === Infinity ? 0 : dp[n][m]
+}
