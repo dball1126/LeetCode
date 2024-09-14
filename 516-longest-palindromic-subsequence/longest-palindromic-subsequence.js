2e5148a56b8af7e601ac6cf1d519c81fdfdbcff7
Daniel Ball <g.thirteen333@gmail.com>
2024-09-13 23:15:52 -0400

    Time: 258 ms (33.46%) | Memory: 81.2 MB (38.97%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.js
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.js
@@ -1,20 +1,20 @@
-// Palindromic Subsequence
-// Bottom-Up Dynamic Programming
-// Time and Space: O(n^2)
+/**
+ * @param {string} s
+ * @return {number}
+ */
 var longestPalindromeSubseq = function(s) {
-    const n = s.length;
-    const dp = [...new Array(n+1)].map(a => 
-               [...new Array(n+1)].fill(0))
+    let n = s.length
+    const dp = [...new Array(n+1)].map(a => [...new Array(n+1)].fill(0))
     for (let i = 0; i < n; i++) dp[i][i] = 1
 
-    for (let i = 0; i < n; i++) {
+    for (let i = 1; i < n; i++) {
         for (let j = i-1; j >= 0; j--) {
-            let v1 = 0, v2 = dp[j+1][i], v3 = dp[j][i-1]
+            let v1 = dp[j+1][i], v2 = dp[j][i-1], v3 = 0
             if (s[i] === s[j]) {
-                if (j+1 === i){
-                    v1 = 2
+                if (j+1 !== i) {
+                    v3 = 2 + dp[j+1][i-1]
                 } else {
-                    v1 = 2 + dp[j+1][i-1]
+                    v3 = 2
                 }
             }
             dp[j][i] = Math.max(v1,v2,v3)
