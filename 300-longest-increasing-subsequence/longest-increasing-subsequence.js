ccf2ef251a5858a731f6ae2c99c6eddf4d5b0cec
Daniel Ball <g.thirteen333@gmail.com>
2024-09-13 22:04:22 -0400

    Time: 169 ms (21.07%) | Memory: 51 MB (31.28%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.js
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.js
@@ -2,18 +2,20 @@
  * @param {number[]} nums
  * @return {number}
  */
+// Longest Increasing Subsequence DP Pattern
+// Bottom-Up Dynamic Programming
+// Time: O(n^2)
+// Space: O(n)
 var lengthOfLIS = function(nums) {
-    let max = 1, n = nums.length
-    const dp = [...new Array(n+1)].fill(1)
+    let n = nums.length, max = 1
+    const dp = [...new Array(n+1)].fill(1);
 
-    for (let i = 0; i < n; i++) {
-        let v = dp[i]
+    for (let i = 1; i < n; i++) {
         for (let j = 0; j < i; j++) {
             if (nums[j] < nums[i]) {
-                v = Math.max(v, dp[j] + 1)
+                dp[i] = Math.max(dp[i], dp[j]+1)
             }
         }
-        dp[i] = v
         max = Math.max(max, dp[i])
     }
     return max;
