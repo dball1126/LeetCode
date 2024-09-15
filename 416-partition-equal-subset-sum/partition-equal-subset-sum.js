338643cbb44960545f6d6965999bd7685ce26bb3
Daniel Ball <g.thirteen333@gmail.com>
2024-09-15 02:24:55 -0400

    Time: 311 ms (34.06%) | Memory: 86.7 MB (17.08%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.js
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.js
@@ -1,22 +1,19 @@
-/**
- * @param {number[]} nums
- * @return {boolean}
- */
 var canPartition = function(nums) {
-    let sum = nums.reduce((acc, v)=> acc+v)
+    let n = nums.length, sum = nums.reduce((acc,v)=>acc+v)
     if (sum & 1) return false;
     sum = Math.floor(sum / 2)
-    const dp = [...new Array(nums.length+1)].map(a => [...new Array(sum+1)].fill(false))
+    const dp = [...new Array(n+1)].map(a => [...new Array(sum+1)].fill(false))
     for (let arr of dp) arr[0] = true;
 
-    for (let r = 1; r < dp.length; r++) {
-        for (let c = 1; c < dp[r].length; c++) {
-            let v1 = dp[r-1][c], v2 = false
+    for (let r = 1; r <= n; r++) {
+        for (let c = 0; c <= sum; c++) {
+            let v1 = false, v2 = dp[r-1][c]
+
             if (c - nums[r-1] >= 0) {
-                v2 = dp[r-1][c - nums[r-1]]
+                v1 = dp[r-1][c-nums[r-1]]
             }
             dp[r][c] = v1 || v2
         }
     }
-    return dp[dp.length-1][dp[0].length-1]
+    return dp[n][sum]
 };
