56eaedb7b180681d1229b49cf763356702612c87
Daniel Ball <g.thirteen333@gmail.com>
2024-09-15 05:25:01 -0400

    Time: 71 ms (13.95%) | Memory: 51.5 MB (47.63%) - LeetSync

--- 139-word-break/word-break.js
+++ 139-word-break/word-break.js
@@ -1,27 +1,21 @@
-/**
- * @param {string} s
- * @param {string[]} wordDict
- * @return {boolean}
- */
+// String Partition DP Pattern
 // Bottom-Up Dynamic Programming
-// Time: O(n * m * k)....n for s.length...m for number of words...k for the longest word
+// Time: O(n^2 *m)....n for s length, m for # of words
 // Space: O(n)
 var wordBreak = function(s, wordDict) {
-    const dp = [...new Array(s.length+1)].fill(false)
-    dp[s.length] = true
-
-    for (let i = s.length-1; i >= 0; i--) {
-        let valid = false;
-        for (let w of wordDict) {
-            if (i+w.length <= s.length) {
-                let word = s.substring(i, i+w.length)
-                if (word === w) {
-                    valid = dp[i+w.length] || valid
-                    if (valid) break
+    let n = s.length
+    const dp = [...new Array(n+1)].fill(false)
+    dp[n] = true
+    for (let i = n-1; i >= 0; i--) { // O(n)
+        for (let w of wordDict) { // O(m)
+            if ((i + w.length <= n)) {
+                let word = s.substring(i, i+w.length) // O(n)
+                if (w === word) {
+                    dp[i] = dp[i + w.length]
+                    if (dp[i]) break;
                 }
             }
         }
-        dp[i] = valid
     }
     return dp[0]
 };
