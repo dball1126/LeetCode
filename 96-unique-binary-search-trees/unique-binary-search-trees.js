45e26f281b1d46e8b0a3fd28940557b0142724db
Daniel Ball <g.thirteen333@gmail.com>
2024-09-16 22:22:58 -0400

    Time: 46 ms (80.35%) | Memory: 48.9 MB (20.23%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.js
+++ 96-unique-binary-search-trees/unique-binary-search-trees.js
@@ -2,21 +2,19 @@
  * @param {number} n
  * @return {number}
  */
-// Catalan Numbers
-// Top-Down Dynamic Programming
-// Time: O(n^2)
-// Space O(n)
 var numTrees = function(n) {
     let memo = [...new Array(n+1)]
+
     const dp = (num) => {
-        if (memo[num] !== undefined) return memo[num]
         if (num <= 1) return 1;
+        if (memo[num]) return memo[num];
         memo[num] = 0
+
         for (let i = 1; i <= num; i++) {
-            memo[num] += dp(i-1) * dp(num - i)
+            memo[num] += dp(i - 1) * dp(num - i)
         }
+
         return memo[num]
     }
     return dp(n)
 };
-console.log(numTrees(3))
