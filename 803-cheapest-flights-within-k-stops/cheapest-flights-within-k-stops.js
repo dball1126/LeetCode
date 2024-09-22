f2c063076a9210083e269fb9581e852a036b5550
Daniel Ball <g.thirteen333@gmail.com>
2024-09-22 08:10:53 -0400

    Time: 108 ms (23.06%) | Memory: 60.6 MB (17.96%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.js
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.js
@@ -1,124 +1,36 @@
-class MinHeap { // Object version
-    constructor() {
-       this.array = [null];
-    }
+/**
+ * @param {number} n
+ * @param {number[][]} flights
+ * @param {number} src
+ * @param {number} dst
+ * @param {number} k
+ * @return {number}
+ */
+// DP On Graphs
+// Bottom-Up Dynamic Programming
+// Time: O(k * n^2 )
+// Space: O(n^2)
 
-    getParent(idx) {
-        return Math.floor(idx / 2);
-      }
+var findCheapestPrice = function(n, flights, src, dst, k) {
+    
+    let vals = [...new Array(n+1)].map(a => [...new Array(n+1)].fill(Infinity))
+    for (let [t,f,v] of flights) {
+        vals[t][f] = v
+    }
+    let dists = [...new Array(n+1)].fill(Infinity)
+    dists[src] = 0
   
-    getLeftChild(idx) {
-        return idx * 2;
-    }
-  
-    getRightChild(idx) {
-        return idx * 2 + 1;
-    }
+    for (let i = 0; i <= k; i++) {
+        let dists2 = [...dists]
+        for (let [t,f,v] of flights) {
 
-    insert(node) {
-        this.array.push(node);
-        this.siftUp(this.array.length - 1);
-    }
+            if (vals[t][f] !== Infinity) {
 
-    siftUp(idx) {
-        if (idx === 1) return;
-        let pIdx = this.getParent(idx)
-        let pNode = this.array[pIdx]
-        let node = this.array[idx]
-        if (!pNode || !node) return;
-        if (pNode.val > node.val) {
-            [this.array[idx], this.array[pIdx]] = [this.array[pIdx], this.array[idx]];
-            this.siftUp(pIdx);
+                dists2[f] = Math.min(dists2[f], dists[t] + vals[t][f])
+            }
+
         }
+        dists = dists2
     }
-
-    poll() { // delete min/max
-        if (this.array.length <= 1) return null;
-        if (this.array.length === 2) return this.array.pop();
-        let min = this.array[1];
-        this.array[1] = this.array.pop();
-
-        this.siftDown(1);
-        return min;
-    }   
-
-    siftDown(idx) {
-        if (this.array.length <= 2) return;
-        let node = this.array[idx]
-        if (!node) return;
-
-        let leftIdx = this.getLeftChild(idx);
-        let rightIdx = this.getRightChild(idx);
-
-        let lNodeVal = this.array[leftIdx] ? this.array[leftIdx].val : Infinity;
-        let rNodeVal = this.array[rightIdx] ? this.array[rightIdx].val : Infinity
-        
-        if (node.val < lNodeVal && node.val < rNodeVal) return;
-
-        let swapIdx;
-        if (lNodeVal < rNodeVal) {
-            swapIdx = leftIdx
-        }  else {
-            swapIdx = rightIdx
-        }
-
-        [this.array[swapIdx], this.array[idx]] = [this.array[idx], this.array[swapIdx]]
-        this.siftDown(swapIdx);
-        
-    }
-
-    isEmpty() {
-        return this.array.length <= 1
-    }
-
-    peek() {
-        return this.array[1];
-    }
-
-    size() {
-        return this.array.length-1
-    }
-}
-
-class Node {
-    constructor(val, key, stops) {
-        this.val = val; this.key = key; this.stops = stops;
-    }
-}
-// Time: O((V + E) * log(v))
-// Space: O(E)...for edges in heap or adjList
-var findCheapestPrice = function(n, flights, src, dst, stops) {
-    let adjList = new Map(), minHeap = new MinHeap(), result = Infinity, visited = new Map()
-
-    for (let [f, t, p] of flights) {
-        if (!adjList.has(f)) adjList.set(f, [])
-        adjList.get(f).push([t, p])
-    }
-
-    if (adjList.has(src)) minHeap.insert(new Node(0, src, 0))
-
-    while (!minHeap.isEmpty()) {
-        let nde = minHeap.poll();
-        if (nde.key === dst) {
-            return result = Math.min(nde.val, result)
-        }
-        if (!visited.has(nde.key)) {
-            visited.set(nde.key, nde.stops)
-        } else if (visited.get(nde.key) > nde.stops) {
-            visited.set(nde.key, nde.stops)
-        } else {
-            continue;
-        }
-
-        if (adjList.has(nde.key)) {
-            for (let [k, v] of  adjList.get(nde.key)) {
-                if (k === dst) {
-                    result = Math.min(result, nde.val + v)
-                } else if (nde.stops + 1 <= stops) {
-                    minHeap.insert(new Node(nde.val + v, k, nde.stops + 1))
-                }
-            } 
-        }
-    }
-    return result === Infinity ? -1 : result;
+    return dists[dst] === Infinity ? -1 : dists[dst]
 };
