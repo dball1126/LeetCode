79a2e0bb940c8bd224a780b81ea618e398d3fa47
Daniel Ball <g.thirteen333@gmail.com>
2024-09-24 20:51:04 -0400

    Time: 63 ms (10.28%) | Memory: 49.2 MB (14.95%) - LeetSync

--- 233-number-of-digit-one/number-of-digit-one.js
+++ 233-number-of-digit-one/number-of-digit-one.js
@@ -1,24 +1,25 @@
-// Digit DP
-// Top-Down Dynamic Programming
-// Time and Space: O(n * 10) = O(n)...n is the length of our STRING input
+/**
+ * @param {number} n
+ * @return {number}
+ */
 var countDigitOne = function(n) {
-    n += ""
-    let len = n.length
-    const memo = new Map()
+    n += "";
+    let memo = new Map(), len = n.length;
+
     const dp = (idx, tight, sum) => {
-        if (idx >= len) return sum
-        let k  = idx +":"+tight+":"+sum
-        if (memo.has(k)) return memo.get(k)
-        let counter = 0, maxD = 9
+        if (idx >= len) return sum;
+        let k = idx + ":" + tight + ":" + sum;
+        if (memo.has(k)) return memo.get(k);
+        let count = 0, maxD = 9
         if (tight) maxD = parseInt(n[idx])
 
         for (let i = 0; i <= maxD; i++) {
-            let newTight = tight && i === maxD, c = 0
-            if (i === 1) c = 1
-            counter += dp(idx+1, newTight, sum + c)
+            let v = i === 1 ? 1 : 0;
+            let newtight = tight && i === maxD
+            count += dp(idx+1, newtight, v + sum)
         }
-        memo.set(k, counter)
-        return counter;
+        memo.set(k, count)
+        return count;
     }
     return dp(0, true, 0)
 };
