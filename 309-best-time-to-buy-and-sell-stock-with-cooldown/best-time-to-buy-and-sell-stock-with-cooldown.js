fb3b0dc36a0351d7fd49655919b63a62959c60ef
Daniel Ball <g.thirteen333@gmail.com>
2024-09-26 00:36:40 -0400

    Time: 68 ms (21.50%) | Memory: 51.9 MB (22.02%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.js
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.js
@@ -2,25 +2,24 @@
  * @param {number[]} prices
  * @return {number}
  */
+// State Machine DP Pattern
+// Bottom-Down Dynamic Programming
+// Time & Space: O(n)
 var maxProfit = function(prices) {
     const n = prices.length
-    const memo = [...new Array(n+1)].map(a => [...new Array(3)])
+    const dp = [...new Array(n+1)].map(a => [...new Array(3)].fill(0))
+    dp[n-1][1] = 0; dp[n-1][0] = prices[n-1]
 
-    const dp = (idx, state) => { // state = 1 for buy and 0 for sell
-        if (idx >= n) return 0
-        if (idx === n-1) {
-            if (state === 1) return 0
-            return prices[idx]
+    for (let i = n-2; i >= 0; i--) {
+        for (let state = 0; state <= 1; state++) {
+            let nx = dp[i+1][state], v = 0
+            if ( state === 1 ) {
+                v = dp[i+1][0] - prices[i]
+            } else {
+                v = dp[i+2][1] + prices[i]
+            }
+            dp[i][state] = Math.max(nx, v)
         }
-        if (memo[idx][state] !== undefined) return memo[idx][state]
-        let next = dp(idx+1, state)
-        let v = 0
-        if (state === 1) {
-            v = dp(idx+1, 0) - prices[idx]
-        } else {
-            v = dp(idx+2, 1) + prices[idx]
-        }
-        return memo[idx][state] = Math.max(next, v)
     }
-    return dp(0, 1)
-};
+    return dp[0][1]
+}
