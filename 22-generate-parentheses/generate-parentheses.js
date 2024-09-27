4cd08e94efbda47075560c7d5ad4a0fd38f735c0
Daniel Ball <g.thirteen333@gmail.com>
2024-09-27 03:01:35 -0400

    Time: 69 ms (9.20%) | Memory: 51.1 MB (18.53%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.js
+++ 22-generate-parentheses/generate-parentheses.js
@@ -2,8 +2,11 @@
  * @param {number} n
  * @return {string[]}
  */
+// Catalan Number DP Pattern
+// Top-Down Dynamic Programming
+// Time: O(2^n * n^2)
+// Space: O(2^n)
 var generateParenthesis = function(n) {
-    
     const memo = [...new Array(n+1)]
 
     const dp = (p) => {
@@ -11,11 +14,10 @@ var generateParenthesis = function(n) {
         if (memo[p]) return memo[p]
         let result = []
         for (let i = 0; i < p; i++) {
-            let left = dp(i), right = dp(p - i - 1)
+            let left = dp(i), right = dp(p - i - 1) // O(2^n)
 
             for (let l of left) {
                 for (let r of right) {
-
                     result.push("(" + l + ")" + r)
                 }
             }
