7f3aeeabd8fcd39d0ad41ca2401c8de5553b9937
Daniel Ball <g.thirteen333@gmail.com>
2024-09-29 07:14:54 -0400

    Time: 106 ms (23.64%) | Memory: 59.6 MB (11.88%) - LeetSync

--- 72-edit-distance/edit-distance.js
+++ 72-edit-distance/edit-distance.js
@@ -1,25 +1,25 @@
-// Edit Distance DP Pattern
-// Bottom-Up Dynamic Programming
-// Time and Space: O(n * m)
-var minDistance = function(word1, word2) {
-    if (!word1.length || !word2.length) return Math.max(word1.length, word2.length)
-    let n = word1.length, m = word2.length
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {number}
+ */
+var minDistance = function(w1, w2) {
+    if (!w1 || !w2) return Math.max(w1.length, w2.length)
+    let n = w1.length, m = w2.length
+
     const dp = [...new Array(n+1)].map(a => [...new Array(m+1)].fill(Infinity))
-    for (let i = 0; i < n; i++) dp[i][0] = i;
-    for (let i = 0; i < m; i++) dp[0][i] = i;
-
-    for (let i = 1; i <= n; i++) {
-        for (let j = 1; j <= m; j++) {
-
-            let v1 = Infinity, v2 = 1 + dp[i-1][j], v3 = 1 + dp[i][j-1]
-            if (word1[i-1] === word2[j-1]) {
-                v1 = dp[i-1][j-1]
-            } else {
-                v1 = 1 + dp[i-1][j-1]
-            }
-            dp[i][j] = Math.min(v1,v2,v3)
-        }
+    for (let i = 0; i <= n; i++) {
+        dp[0][i] = i; dp[i][0] = i
     }
 
-    return dp[n][m] === Infinity ? 0 : dp[n][m]
-}
+    for (let r = 1; r <= n; r++) {
+        for (let c = 1; c <= m; c++) {
+            let v1 = dp[r-1][c] + 1, v2 = dp[r][c-1] + 1, v3 = dp[r-1][c-1] + 1
+            if (w1[r-1] === w2[c-1]) {
+                v3 = dp[r-1][c-1]
+            }
+            dp[r][c] = Math.min(v1,v2,v3)
+        }   
+    }
+    return dp[n][m]
+};
