0ca64338557e3a8e36f018b3df97f35c2cf50a9a
Daniel Ball <g.thirteen333@gmail.com>
2024-09-29 01:27:32 -0400

    Time: 138 ms (29.32%) | Memory: 75.4 MB (57.50%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.js
+++ 1250-longest-common-subsequence/longest-common-subsequence.js
@@ -3,18 +3,17 @@
  * @param {string} text2
  * @return {number}
  */
-// Longest Common Subsequence DP Pattern
-// Bottom-Up Dyanmic Programming
-// Time and Space: O(n * m)
-var longestCommonSubsequence = function(s1, s2) {
-    let n = s1.length, m = s2.length
-    const dp = [...new Array(n+1)].map(a => [...new Array(m+1)].fill(0))
+var longestCommonSubsequence = function(w1, w2) {
+    let n = w1.length, m = w2.length;
+
+    const dp = [...new Array(n+1)].map(a =>[...new Array(m+1)].fill(0))
+    for (let arr of dp) arr[0] = 0
 
     for (let r = 1; r <= n; r++) {
         for (let c = 1; c <= m; c++) {
-            let v1 = dp[r-1][c], v2  = dp[r][c-1], v3 = 0
-            if (s1[r-1] === s2[c-1]) {
-                v3 = 1 + dp[r-1][c-1]
+            let v1 = dp[r-1][c], v2 = dp[r][c-1], v3 = 0
+            if (w1[r-1] === w2[c-1]) {
+                v3 = dp[r-1][c-1] + 1
             }
             dp[r][c] = Math.max(v1,v2,v3)
         }
