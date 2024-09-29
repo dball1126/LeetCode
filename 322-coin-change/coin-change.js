7a67a703ea68f0c7b3880f1b4171676956137b02
Daniel Ball <g.thirteen333@gmail.com>
2024-09-29 01:05:27 -0400

    Time: 101 ms (55.33%) | Memory: 56.7 MB (22.98%) - LeetSync

--- 322-coin-change/coin-change.js
+++ 322-coin-change/coin-change.js
@@ -1,18 +1,19 @@
-// Unbounded Knapsack DP Pattern
-// Bottom-Up Dynamic Programming
-// Time: O(n * m)...c for amt of coins and m for the amount
-// Space: O(m)
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
 var coinChange = function(coins, amount) {
+    let l = coins.length
     const dp = [...new Array(amount+1)].fill(Infinity)
     dp[0] = 0
 
-    for (let c of coins) {
-        for (let a = 0; a <= amount; a++) {
-            if (a - c >= 0) {
-                dp[a] = Math.min(dp[a], dp[a - c] + 1)
+    for (let c of coins) { // usually the outter loop
+        for (let r = 1; r <= amount; r++) { // usually the inner loop
+            if ( r - c >= 0) {
+                dp[r] = Math.min(dp[r], dp[r - c] + 1)
             }
         }
     }
-
     return dp[amount] === Infinity ? -1 : dp[amount]
 };
