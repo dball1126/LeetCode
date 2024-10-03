29324748ea2bd72ec1c34316c24c8abd54604743
Daniel Ball <g.thirteen333@gmail.com>
2024-10-03 07:56:08 -0400

    Time: 56 ms (76.17%) | Memory: 52.2 MB (19.78%) - LeetSync

--- 139-word-break/word-break.js
+++ 139-word-break/word-break.js
@@ -1,19 +1,17 @@
-// String Partition DP Pattern
-// Bottom-Up Dynamic Programming
-// Time: O(n^2 *m)....n for s length, m for # of words
-// Space: O(n)
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
 var wordBreak = function(s, wordDict) {
     let n = s.length
     const dp = [...new Array(n+1)].fill(false)
     dp[n] = true
-    for (let i = n-1; i >= 0; i--) { // O(n)
-        for (let w of wordDict) { // O(m)
-            if ((i + w.length <= n)) {
-                let word = s.substring(i, i+w.length) // O(n)
-                if (w === word) {
-                    dp[i] = dp[i + w.length]
-                    if (dp[i]) break;
-                }
+    for (let i = n-1; i >= 0; i--) {
+        for (let w of wordDict) {
+            if (i + w.length <= n && (w === s.substring(i, i+ w.length))) {
+                dp[i] = dp[i] || dp[i+ w.length]
+                if (dp[i]) break
             }
         }
     }
