33dcff34bcaceee5df4f8a19ece0e084b2ec38dd
Daniel Ball <g.thirteen333@gmail.com>
2024-10-03 08:05:02 -0400

    Time: 47 ms (75.89%) | Memory: 48.9 MB (31.78%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.js
+++ 96-unique-binary-search-trees/unique-binary-search-trees.js
@@ -2,21 +2,13 @@
  * @param {number} n
  * @return {number}
  */
-// Catalan Numbers DP Pattern
-// Bottom-Up Dynamic Programming
-// Time: O(n^2)
-// Space: O(n)
 var numTrees = function(n) {
-    
-    const dp = [...new Array(n+1)].fill(0);
+    const dp = [...new Array(n+1)].fill(0)
     dp[0] = 1; dp[1] = 1;
 
     for (let i = 2; i <= n; i++) {
         for (let j = 1; j <= i; j++) {
-            let left = dp[j-1]
-            let right = dp[i-j]
-
-            dp[i] += (left * right)
+            dp[i] += (dp[j-1] * dp[i- j])
         }
     }
     return dp[n]
