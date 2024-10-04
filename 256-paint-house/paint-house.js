97f270d88ce919e71288e0f5d4b3835ac5f8defd
Daniel Ball <g.thirteen333@gmail.com>
2024-10-04 02:54:35 -0400

    Time: 61 ms (34.57%) | Memory: 51.3 MB (43.21%) - LeetSync

--- 256-paint-house/paint-house.js
+++ 256-paint-house/paint-house.js
@@ -5,21 +5,18 @@
 var minCost = function(costs) {
     let n = costs.length
     const memo = [...new Array(n+1)].map(a => [...new Array(4)])
-    const visited = [...new Array(n+1)].map(a => [...new Array(4)])
     
-    const dp = (idx, col, vis) => {
+    const dp = (idx, col) => {
         if (idx >= n) return 0
         if (memo[idx][col] !== undefined) return memo[idx][col]
 
         let min = Infinity
         for (let i = 0; i < 3; i++) {
-            if (col !== i && !vis[idx][i]) {
-                // vis[idx][i] = true
-                min = Math.min(min, costs[idx][i] + dp(idx+1, i, vis))
-                // vis[idx][i] = false
+            if (col !== i) {
+                min = Math.min(min, costs[idx][i] + dp(idx+1, i))
             }
         }
         return memo[idx][col] = min
     }
-    return dp(0,-1,visited)
+    return dp(0,-1)
 };
