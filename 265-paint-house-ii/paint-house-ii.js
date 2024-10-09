39bc908dc5f0cba1f8d60eba7f093afcfa4c72d3
Daniel Ball <g.thirteen333@gmail.com>
2024-10-08 22:48:13 -0400

    Time: 64 ms (63.41%) | Memory: 50.2 MB (97.56%) - LeetSync

---
+++ 265-paint-house-ii/paint-house-ii.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[][]} costs
+ * @return {number}
+ */
+var minCostII = function(costs) {
+    const n = costs.length, m = costs[0].length;
+    let memo = [...new Array(n+1)].map(a => [...new Array(m+1)])
+
+    const dp = (i, j, init) => {
+        if (i >= n) return 0;
+        if (memo[i][j] !== undefined) return memo[i][j];
+        let min = Infinity;
+
+        for (let c = 0; c < m; c++) {
+            if (j !== c || init) {
+                min = Math.min(min, costs[i][c] + dp(i+1, c, false))
+            }
+        }
+        return memo[i][j] = min;
+    }
+    return dp(0, 0, true)
+};
