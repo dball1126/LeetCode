e395f6c99feed9cad2ce0b4f4f22049af79332f7
Daniel Ball <g.thirteen333@gmail.com>
2024-10-10 01:50:05 -0400

    Time: 80 ms (18.51%) | Memory: 53.2 MB (56.15%) - LeetSync

--- 55-jump-game/jump-game.js
+++ 55-jump-game/jump-game.js
@@ -6,15 +6,15 @@
 // Time: O(n)
 // Space: O(1)
 var canJump = function(nums) {
-    if (nums.length <= 1) return true;
-    let n = nums.length, curr = nums[0];
-
-    for (let i = 0; i < n; i++) {
-        if (curr < 0) return false;
-
-        curr = Math.max(curr, nums[i])
-        if ((curr + i) >= n-1) return true;
-        curr -= 1
+    let n = nums.length
+    let prev = nums[0]
+    if (prev >= n-1) return true;
+    for (let i = 1; i < n; i++) {
+        if (prev === 0) return false;
+        let v = nums[i]
+        prev = Math.max(prev-1, v)
+        if (i + prev >= n-1) return true;
     }
+    
     return false;
 };
