36daf82f797d9174fee10c1f631325c451f2f30b
Daniel Ball <g.thirteen333@gmail.com>
2024-10-09 23:28:30 -0400

    Time: 45 ms (98.40%) | Memory: 51.4 MB (30.30%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,13 +4,12 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    let map = new Map(), n = nums.length
+    const map = new Map()
 
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < nums.length; i++) {
         if (map.has(target - nums[i])) {
             return [i, map.get(target - nums[i])]
-        } else {
-            map.set(nums[i], i)
         }
-    }  
+        map.set(nums[i], i)
+    }
 };
