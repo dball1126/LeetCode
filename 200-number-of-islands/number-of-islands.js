a169a5078d3b573df44b91b8f1ed1fd5a897b61d
Daniel Ball <g.thirteen333@gmail.com>
2024-10-11 03:47:22 -0400

    Time: 96 ms (28.94%) | Memory: 59.3 MB (32.20%) - LeetSync

--- 200-number-of-islands/number-of-islands.js
+++ 200-number-of-islands/number-of-islands.js
@@ -2,26 +2,31 @@
  * @param {character[][]} grid
  * @return {number}
  */
-// Recursive Depth-First-Search
-// Time and Space: O(n * m)
+// Depth-First-Search
+// Time and Space: O(n * m)...rows * columns
 var numIslands = function(grid) {
     let n = grid.length, m = grid[0].length, islands = 0
-    const dirs = [[-1,0],[1,0],[0,-1],[0,1]]
-    const visited = [...new Array(n)].map(a => [...new Array(m)].fill(false))
+    const memo = [...new Array(n+1)].map(a => [...new Array(m+1)])
+    const dirs = [[1,0],[-1,0],[0,1],[0,-1]]
+    visited = [...new Array(n+1)].map(a => [...new Array(m+1)].fill(false))
+    
+    const dp = (r, c, visited) => {
+        if (r < 0 || c < 0 || r >= n || c >= m || visited[r][c] || grid[r][c] !== "1") return 0
+        if (memo[r][c] !== undefined) return memo[r][c]
+        let num = grid[r][c] === "1" ? 1 : 0
+        visited[r][c] = true
+        for (let [y, x] of dirs) {
+            let ry = r + y, cx = c + x
 
-    const dfs = (r, c) => {
-        if (r < 0 || c < 0 || r >= n || c >= m) return;
-        if (visited[r][c] || grid[r][c] !== '1') return;
-        visited[r][c] = true;
-
-        for (let [x, y] of dirs) dfs (r + x, c + y)
+            dp(ry, cx, visited)
+        }
+        return memo[r][c] = num
     }
-
-    for (let r = 0; r < n; r++) {
-        for (let c = 0; c < m; c++) {
-            if (!visited[r][c] && grid[r][c] === '1') {
-                islands++
-                dfs(r, c)
+    for (let r = 0; r < grid.length; r++) {
+        for (let c = 0; c < grid[r].length; c++) {
+            if (grid[r][c] === "1" && visited[r][c] === false) {
+                islands+=1
+                dp(r, c, visited)
             }
         }
     }
