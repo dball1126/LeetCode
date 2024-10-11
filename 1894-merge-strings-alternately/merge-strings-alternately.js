6f02b580c3c59fe11833db77732511b8cbfa4e6c
Daniel Ball <g.thirteen333@gmail.com>
2024-10-11 01:46:48 -0400

    Time: 51 ms (71.88%) | Memory: 48 MB (99.77%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.js
@@ -0,0 +1,18 @@
+// Merge Srings
+// Time: O(k)...k is the maxixmum length between n and m
+// Space: O(1)
+var mergeAlternately = function(word1, word2) {
+    let result = "", i = 0, j = 0, n = word1.length, m = word2.length
+
+    while (i < n || j < m) {
+        if (i < n) {
+            result += word1[i]
+            i++
+        }
+        if (j < m) {
+            result += word2[j]
+            j++
+        }
+    }
+    return result;
+};
