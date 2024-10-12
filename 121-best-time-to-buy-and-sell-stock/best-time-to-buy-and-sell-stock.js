79e9bc031cffe3b11a80b65984c1821fa669fd67
Daniel Ball <g.thirteen333@gmail.com>
2024-10-11 21:43:29 -0400

    Time: 76 ms (49.16%) | Memory: 58.7 MB (77.78%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -2,18 +2,15 @@
  * @param {number[]} prices
  * @return {number}
  */
-// Greedy
-// Time: O(n)
-// Space: O(1)
 var maxProfit = function(prices) {
-    let profit = -Infinity, n = prices.length
-    let min = prices[0]  
-    for (let i = 1; i < n; i++) {
-        let v = prices[i]
-        if (min < v) {
-            profit = Math.max ( profit, v - min)
+    let low = prices[0]
+    let max = 0
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] < low) {
+            low = prices[i]
+        } else {
+            max = Math.max(max, prices[i] - low)
         }
-        min = Math.min(min, v)
-    } 
-    return profit === -Infinity ? 0 : profit
+    }
+    return max
 };
