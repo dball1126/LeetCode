3674c6b58594c5159b713f76080ee4f33aa0df6d
Daniel Ball <g.thirteen333@gmail.com>
2024-10-12 02:12:07 -0400

    Time: 97 ms (38.27%) | Memory: 57.5 MB (16.75%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.js
+++ 5-longest-palindromic-substring/longest-palindromic-substring.js
@@ -1,31 +1,28 @@
-/**
- * @param {string} s
- * @return {string}
- */
-// Bottom-Up Dynamic Programming
-// Time and Space: O(n^2)
-var longestPalindrome = function(str) {
-    if (!str) return ""
-    let l = 0, r = 0, n = str.length
+// Expand From Centers
+// Time: O(n^2)
+// Space: O(1)
+var longestPalindrome = function(s) {
+    let n = s.length, longestSub = ""
 
-    const dp = [...new Array(n+1)].map(a =>
-               [...new Array(n+1)].fill(false))
-
-    for (let i = 0; i < n; i++) { // check all substrings
-        for (let j = i; j >= 0; j--) {
-
-            if (i === j) { // 1 letter is a palindrome
-                dp[j][i] = true
-            } else if (str[j] === str[i] && (j+1 === i || dp[j+1][i-1])) {
-                // is the current pair a palindrome and is previous a palindrome?
-                dp[j][i] = true
-            }
-            if (dp[j][i]) { // if true these bounds are a palindrome
-                if ((i-j) > (r -l)) {
-                    l = j; r = i
-                }
+    const expandFromCenter = (j, i) => {
+        let str = ""
+        while (j >= 0 && i < n ) {
+            if (i === j) {
+                str += s[i]
+                j--; i++;
+            } else {
+                if (s[i] !== s[j]) break;
+                str = (s[j] + str + s[i])
+                j--; i++;
             }
         }
+        if (str.length > longestSub.length) {
+            longestSub = str
+        }
     }
-    return str.substring(l, r +1)
+    for (let i = 0; i < n; i++) {
+        expandFromCenter(i, i)
+        expandFromCenter(i, i+1)
+    }
+    return longestSub
 };
