596099d8199e271e9818dbeb48414cdbd2f3c606
Daniel Ball <g.thirteen333@gmail.com>
2024-10-13 09:16:54 -0400

    Time: 72 ms (75.22%) | Memory: 58.1 MB (67.15%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.js
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.js
@@ -3,21 +3,17 @@
  * @param {number} k
  * @return {number}
  */
-// Prefix Sum
-// Time and Space: O(n)
 var subarraySum = function(nums, k) {
-    if (!nums.length) return 0;
-    let result = 0, map = new Map([[0,1]]), sum = 0;
+    let sum = 0, total = 0, map = new Map([[0, 1]])
 
     for (let i = 0; i < nums.length; i++) {
         sum += nums[i]
-        
-        if (map.has(sum - k)) {
-            result += map.get(sum - k)
-        }
 
+        if (map.has(sum - k)) {
+            total += map.get(sum - k)
+        }
         if (!map.has(sum)) map.set(sum, 0)
         map.set(sum, map.get(sum) + 1)
     }
-    return result;
+    return total
 };
