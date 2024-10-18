ba0c39c562c2701e65274a409c3b099c6d74d0e0
Daniel Ball <g.thirteen333@gmail.com>
2024-10-18 00:10:40 -0400

    Time: 38 ms (100.00%) | Memory: 65.3 MB (5.75%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -2,9 +2,10 @@
  * @param {string[]} strs
  * @return {string[][]}
  */
+// Hash Map. Character Codes.
+// Time & Space: O(n * l)...n for # of works and l for longest word
 var groupAnagrams = function(strs) {
     let groups = new Map()
-
     for (let str of strs) {
         let chars = [...new Array(26)].fill(0)
         for (let i = 0 ; i < str.length; i++) {
@@ -17,15 +18,8 @@ var groupAnagrams = function(strs) {
                 k += (idx + ":" + v)
             }
         })
-
         if (!groups.has(k)) groups.set(k, [])
         groups.get(k).push(str)
     }
-
-    let result = []
-
-    for (let [k, v] of groups) {
-        result.push(Array.from(v))
-    }
-    return result
+    return Array.from(groups.values())
 };
