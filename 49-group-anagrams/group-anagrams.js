2e102c0179d993e100459c745431aa61b46402e3
Daniel Ball <g.thirteen333@gmail.com>
2024-10-18 00:05:01 -0400

    Time: 43 ms (100.00%) | Memory: 66.3 MB (5.28%) - LeetSync

--- 49-group-anagrams/group-anagrams.js
+++ 49-group-anagrams/group-anagrams.js
@@ -1,32 +1,31 @@
-// Hash Map
-// Time: O((n * m)^2)...n for # of words, m for longest word
-// Space: O(n * m)
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
 var groupAnagrams = function(strs) {
-    const groups = []
-    for (let s of strs) {
-        let map = new Map(), merged = false
-        for (let i = 0; i < s.length; i++) { // populate curr string map
-            if (!map.has(s[i])) map.set(s[i], 0)
-            map.set(s[i], map.get(s[i]) + 1)
+    let groups = new Map()
+
+    for (let str of strs) {
+        let chars = [...new Array(26)].fill(0)
+        for (let i = 0 ; i < str.length; i++) {
+            let idx =str[i].charCodeAt() - "a".charCodeAt()
+            chars[idx]+=1
         }
-        for (let grp of groups) {
-            for (let w of grp) { // find matching group
-                if (w.length !== s.length) continue;
-
-                const m = new Map(map)
-                for (let i = 0; i < w.length; i++) { // check if anagram
-                    if (!m.has(w[i])) break;
-
-                    m.set(w[i], m.get(w[i]) - 1)
-                    if (m.get(w[i]) === 0) m.delete(w[i])
-                }
-                if (!m.size) {
-                    grp.push(s);  merged = true; break;
-                }
+        let k = ""
+        chars.forEach((v, idx) => {
+            if (v > 0) {
+                k += (idx + ":" + v)
             }
-            if (merged) break;
-        }
-        if (!merged) groups.push([s])
+        })
+
+        if (!groups.has(k)) groups.set(k, [])
+        groups.get(k).push(str)
     }
-    return groups
+
+    let result = []
+
+    for (let [k, v] of groups) {
+        result.push(Array.from(v))
+    }
+    return result
 };
