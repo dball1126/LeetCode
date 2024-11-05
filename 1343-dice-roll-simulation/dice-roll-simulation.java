5f33433882c05285bfbd1621902e37221528421c
Daniel Ball <g.thirteen333@gmail.com>
2024-11-04 22:14:36 -0500

    Time: 45 ms (15.38%) | Memory: 45.4 MB (44.94%) - LeetSync

--- 1343-dice-roll-simulation/dice-roll-simulation.java
+++ 1343-dice-roll-simulation/dice-roll-simulation.java
@@ -1,4 +1,6 @@
 class Solution {
+    // Top-Down-Dynamic Programming
+    // Time & Space: O(n)...(7 and 16 are constant)
     public int dieSimulator(int n, int[] rollMax) {
         int mod = (int) Math.floor(Math.pow(10, 9) + 7);
         int[][][] memo = new int[n+1][7][16];
@@ -14,20 +16,19 @@ class Solution {
     }
 
     public int dfs(int n, int[][][] memo, int[] rollMax, int curr, int currL, int mod) {
-        if (n <= 0) return 1;
+        if (n <= 0) return 1; // base case
         if (memo[n][curr][currL] != mod+2) return memo[n][curr][currL];
         int ways = 0;
         for (int k = 0; k < 6; k++) {
 
             if (k != curr) {
                 ways += dfs(n-1, memo, rollMax, k, 1, mod);
-            } else if (rollMax[curr] >= (currL + 1)) {
+            } else if (rollMax[curr] >= (currL + 1)) { // validation
 
                 ways += dfs(n-1, memo, rollMax, curr, currL + 1, mod);
             }
             ways %= mod;
         }
-
         memo[n][curr][currL] = ways;
         return ways;
     }
