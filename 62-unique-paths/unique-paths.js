1027271fa35c52ce6a37097608fbf09f2f5ddb75
Daniel Ball <g.thirteen333@gmail.com>
2024-11-10 05:06:39 -0500

    Time: 0 ms (100.00%) | Memory: 48.8 MB (83.89%) - LeetSync

--- 62-unique-paths/unique-paths.js
+++ 62-unique-paths/unique-paths.js
@@ -3,10 +3,12 @@
  * @param {number} n
  * @return {number}
  */
+// Bottom-Up Dynamic Programming
+// Time: O(m * m)...m for # of rows and n for # of columns
+// Space: O(n)
 var uniquePaths = function(m, n) {
     let row = [...new Array(n)].fill(0)
     row[0] = 1
-
     for (let r = 0; r < m; r++) {
         for (let c = 0; c < n; c++) {
             if (r === 0 && c === 0) continue;
