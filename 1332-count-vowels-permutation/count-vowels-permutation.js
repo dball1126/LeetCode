4d22df07816ff2084ba313dc3d0350aedb3e3853
Daniel Ball <g.thirteen333@gmail.com>
2024-11-16 07:53:55 -0500

    Time: 666 ms (8.00%) | Memory: 77.9 MB (6.35%) - LeetSync

---
+++ 1332-count-vowels-permutation/count-vowels-permutation.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countVowelPermutation = function(n) {
+    const memo = new Map(), mod = (10 ** 9) + 7
+    const vowels = ['a', 'e', 'i', 'o', 'u']
+    
+    const dp = (curr, len) => {
+        if (len >= n) return 1
+        let k = curr + ":" + len
+        if (memo.has(k)) {
+
+            return memo.get(k)
+        }
+        let sum = 0
+        for (let v of vowels) {
+            if (curr === "") {
+                sum += dp(v, len + 1)
+            } else if (v === "a" && curr === "e") {
+                sum += dp(v, len+1)
+            } else if (v === "e" && (curr === "a"  || curr === "i")) {
+                sum += dp(v, len+1)
+            } else if (v === "i" && curr !== "i") {
+                sum += dp(v, len+1)
+            } else if (v === "o" && (curr === "i" || curr === "u")) {
+                sum += dp(v, len+1)
+            } else if ( v === "u" && curr === "a") {
+                sum += dp(v, len+1)
+            }
+            sum %= mod
+        }
+        memo.set(k, sum)
+        return sum
+    }
+    return dp("", 0)
+}
