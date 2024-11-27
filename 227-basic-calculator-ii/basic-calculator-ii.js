cfb749adeff72a924afb6ea3d781064a8a59ec26
Daniel Ball <g.thirteen333@gmail.com>
2024-11-27 14:06:19 -0500

    Time: 35 ms (41.45%) | Memory: 65.5 MB (13.38%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.js
+++ 227-basic-calculator-ii/basic-calculator-ii.js
@@ -1,51 +1,48 @@
-/**
- * @param {string} s
- * @return {number}
- */
 // Stack
-// Time: O(n)
+// Time and space: O(n)
 var calculate = function(s) {
-    let arr = []
-    for (let i = 0; i < s.length; i++) { // pre-processing  // O(n)
-        if (s[i] === " ") continue;
+    const stack = [], n = s.length, operators = new Set(["*","+","/","-"])
+    for (let i = 0; i < n; i++) { // handle multiplication and division
+        let v = s[i]
+        if (v === ' ') continue;
 
-        if (s[i] >= "0" && s[i] <= "9") {
-            let v = s[i]
+        if (v === '*' || v === '/') {
+            i++;
+            while (s[i] == " ") i++;
+            let val = s[i]
+            while (s[i+1] >= "0" && s[i+1] <= "9") {
+                i++;
+                val += s[i]
+            }
+
+            if (v === "*") {
+                stack.push(stack.pop() * parseInt(val))
+            } else {
+                stack.push(Math.floor(stack.pop() / parseInt(val)))
+            }
+        } else if (v === "+" || v === "-") {
+            stack.push(v)
+        } else  {
             while (s[i+1] >= "0" && s[i+1] <= "9") {
                 i++;
                 v += s[i]
             }
-            arr.push(parseInt(v))
-        } else {
-            arr.push(s[i])
+            stack.push(parseInt(v))
         }
     }
-    let stack = []
-    for (let i = 0; i < arr.length; i++) {
-
-        if (arr[i] === "*") {
-            let v1 = stack.pop();
+    let newStack = []
+    for (let i = 0; i < stack.length; i++) { // handle addition and substraction
+        let v = stack[i]
+        if (v === '+' || v === '-') {
             i++;
-            let v2 = arr[i]
-            stack.push(v1 * v2)
-        } else if (arr[i] === "/") {
-            let v1 = stack.pop();
-            i++;
-            let v2 = arr[i]
-            stack.push(Math.floor(v1 / v2))
+            if (v === "+") {
+                newStack.push(newStack.pop() + stack[i])
+            } else {
+                newStack.push(newStack.pop() - stack[i])
+            }
         } else {
-            stack.push(arr[i])
+            newStack.push(v)            
         }
     }
-
-    stack.reverse()
-    while (stack.length) {
-        if (stack.length === 1) return stack[0]
-        let v1 = stack.pop();
-        let opt = stack.pop();
-        let v2 = stack.pop();
-        if (opt === "-") stack.push(v1 - v2)
-        if (opt === "+") stack.push(v1 + v2)
-    }
-
+    return newStack[0]
 };
