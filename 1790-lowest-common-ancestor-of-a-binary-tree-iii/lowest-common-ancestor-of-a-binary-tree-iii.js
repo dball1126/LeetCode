50638bb67d042e9fc96203944ab8fd2da184f870
Daniel Ball <g.thirteen333@gmail.com>
2024-11-26 23:04:31 -0500

    Time: 66 ms (56.75%) | Memory: 55.5 MB (11.98%) - LeetSync

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.js
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.js
@@ -1,6 +1,6 @@
 /**
- * // Definition for a Node.
- * function Node(val) {
+ * // Definition for a _Node.
+ * function _Node(val) {
  *    this.val = val;
  *    this.left = null;
  *    this.right = null;
@@ -9,42 +9,34 @@
  */
 
 /**
- * @param {Node} node
- * @return {Node}
+ * @param {_Node} p
+ * @param {_Node} q
+ * @return {_Node}
  */
-// Recursive Depth-First-Search
-// two pointers
 var lowestCommonAncestor = function(p, q) {
-    let ancestor, pNode, qNode
-    let root;
-    const getParent = (nde) => { // O(n) time, O(h) space if tree is balanced
-        if (!nde) return;
-        root = nde;
-        getParent(nde.parent)
-    }
-    getParent(p)
-    const dfs = (nde) => { // O(n) time, O(h) space if the tree is balanced
-        if (ancestor || !nde) return;
 
-        if (!pNode && nde.val === p.val) {
-            pNode = nde;
-        } else if (!qNode && nde.val === q.val) {
-            qNode = nde;
+    const getParent = (n) => !n.parent ? n : getParent(n.parent)
+
+    let n1 = getParent(p), result = null
+
+    const dfs = (n) => {
+        if (!n || result) return false
+
+        let left = dfs(n.left), right = dfs(n.right)
+        let curr = false;
+
+        if (q.val === n.val || p.val === n.val) {
+            curr = true;
         }
-
-        let left = dfs(nde.left), right = dfs(nde.right)
-
-        if ((left && nde.val === q.val) || 
-            (left && nde.val === p.val) ||
-            (left && right) || 
-            (right && nde.val === p.val) ||
-            (right && nde.val === q.val)
-            ) {
-            if (!ancestor) ancestor = nde;
+        if (!result) {
+            if (left && right ||
+                left && curr ||
+                right && curr) {
+                    result = n
+                }
         }
-
-        return left || right || nde.val === q.val || nde.val === p.val;
+        return left || right || curr
     }
-    dfs(root)
-    return ancestor;
+    dfs(n1)
+    return result;
 };
