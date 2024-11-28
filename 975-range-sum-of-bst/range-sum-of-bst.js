fe5335b0394d1069c1e1ed9f4a1cb880920693a7
Daniel Ball <g.thirteen333@gmail.com>
2024-11-28 03:37:06 -0500

    Time: 0 ms (100.00%) | Memory: 80.9 MB (6.61%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.js
+++ 975-range-sum-of-bst/range-sum-of-bst.js
@@ -1,31 +1,14 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
- * @param {TreeNode} root
- * @param {number} low
- * @param {number} high
- * @return {number}
- */
 var rangeSumBST = function(root, low, high) {
-    
-    const dfs = (nde, min, max) => {
+    const dfs = (nde) => {
         if (!nde) return 0;
         let result = (nde.val >= low && nde.val <= high )? nde.val : 0;
-
         if (high > nde.val ) {
-            result += dfs(nde.right, min, nde.val)
+            result += dfs(nde.right)
         }
         if (low < nde.val) {
-            result += dfs(nde.left, nde.val, max)
+            result += dfs(nde.left)
         }
         return result
     }
-
     return dfs(root, -Infinity, Infinity)
 };
