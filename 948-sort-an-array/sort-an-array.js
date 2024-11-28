8b6204c8163112b8f843a5439ee442024bc3ed9c
Daniel Ball <g.thirteen333@gmail.com>
2024-11-28 02:17:05 -0500

    Time: 119 ms (33.71%) | Memory: 76.4 MB (11.34%) - LeetSync

--- 948-sort-an-array/sort-an-array.js
+++ 948-sort-an-array/sort-an-array.js
@@ -2,26 +2,33 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-
-// Iterative in-place Quick Sort:
-// Time: O(n * log(n))
-// Space: O(log(n)) 
-const sortArray = (nums) => {
-   const n = nums.length;
-   const queue = [[0, n-1]]
-   while (queue.length) {
-    const [LC, RC] = queue.shift();
-    let l = LC, r = RC, pivot = nums[Math.floor((RC + LC) / 2)]
-    while (l <= r) {
-        while (nums[l] < pivot) l++
-        while (nums[r] > pivot) r--
-        if (l <= r) {
-            [nums[l], nums[r]] = [nums[r], nums[l]]
-            l++; r--;
+var sortArray = function(nums) {
+    if (nums.length <= 1) return nums
+    let m = Math.floor(nums.length / 2)
+    let left = [], right = []
+    for (let i = 0; i < nums.length; i++) {
+        if (i < m) {
+            left.push(nums[i])
+        } else {
+            right.push(nums[i])
         }
     }
-    if ((l-1) > LC) queue.push([LC, l-1])
-    if (l < RC) queue.push([l, RC])
-   }
-   return nums;
+    return merge(sortArray(left), sortArray(right))
+};
+const merge = (n1, n2) => {
+    let result = [], i = 0, j = 0, n = n1.length, m = n2.length
+    if (!n) return n2
+    if (!m) return n1
+    while (i < n || j < m) {
+        let v1 = i < n ? n1[i] : Infinity
+        let v2 = j < m ? n2[j] : Infinity
+        if (v1 <= v2) {
+            result.push(n1[i]); 
+            i++;
+        } else {
+            result.push(n2[j]); 
+            j++;
+        }
+    }
+    return result;
 }
