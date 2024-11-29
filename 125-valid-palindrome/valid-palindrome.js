7a2729a99a1f39eac67ef283f571834786fac638
Daniel Ball <g.thirteen333@gmail.com>
2024-11-29 00:40:41 -0500

    Time: 7 ms (36.63%) | Memory: 54.8 MB (25.17%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.js
+++ 125-valid-palindrome/valid-palindrome.js
@@ -2,22 +2,21 @@
  * @param {string} s
  * @return {boolean}
  */
-// Two pointer
-// Time & Space: O(n)
 var isPalindrome = function(s) {
-    //pre-processing
-    let newStr = ""
+    let str = "";
+
     for (let i = 0; i < s.length; i++) {
-        if (s[i] >= "a" && s[i] <= "z" || s[i] >= "A" && s[i] <= "Z" || s[i] >= "0" && s[i] <= "9") {
-            newStr += s[i].toLocaleLowerCase()
+        let v = s[i].toLowerCase();
+        if ((v >= "a" && v <= "z") || 
+            (v >= "0" && v <= "9")) {
+                str += v;
         }
     }
 
-    let i = 0, j = newStr.length-1;
+    let i = 0, j = str.length-1
+
     while (i < j) {
-        if (newStr[i] !== newStr[j]) {
-            return false;
-        }
+        if (str[i] !== str[j]) return false;
         i++; j--;
     }
     return true;
