a86cb9a42bea6f23c1f8fd25ff12733b56fab63e
Daniel Ball <g.thirteen333@gmail.com>
2024-11-30 02:18:51 -0500

    Time: 46 ms (97.06%) | Memory: 50.3 MB (96.83%) - LeetSync

--- 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.js
+++ 758-convert-binary-search-tree-to-sorted-doubly-linked-list/convert-binary-search-tree-to-sorted-doubly-linked-list.js
@@ -1,33 +1,29 @@
-/**
- * // Definition for a Node.
- * function Node(val, left, right) {
- *      this.val = val;
- *      this.left = left;
- *      this.right = right;
- *  };
- */
-
-/**
- * @param {Node} root
- * @return {Node}
- */
-// InOrder Traversal
-// Time and Space: O(n)
+// Inorder Traversal
 var treeToDoublyList = function(root) {
     if (!root) return root
+    let stack = [], head = null, prev = null
 
+    while (stack.length || root) {
+        while (root && root.left) {
+            stack.push(root)
+            root = root.left
+        }
+        if (!root) root = stack.pop()
+        
+        if (!head) {
+            head = root;
+            prev = root
+        } else {
+            prev.right = root;
+            root.left = prev
+            prev = root
+        }
+        if (root.right) {
+            root = root.right
+        } else { root = null }
+    }
+    head.left = prev;
+    prev.right = head
 
-    const inOrderDfs = (nde) => { // O(n)
-        if (!nde) return []
-        return [...inOrderDfs(nde.left), nde, ...inOrderDfs(nde.right)]
-    }
-    const inorder = inOrderDfs(root)
-    for (let i = 0; i < inorder.length; i++) { // O(n)
-        let next = i+1 < inorder.length ? inorder[i+1] : inorder[0]
-        let prev = i-1 >= 0 ? inorder[i-1] : inorder[inorder.length-1]
-        let curr = inorder[i]
-        curr.left = prev;
-        curr.right = next;
-    }
-    return inorder[0]
+    return head;
 };
