0debc685b9a36559abcd3ce19ecbed55d0f24cab
Daniel Ball <g.thirteen333@gmail.com>
2024-11-30 04:56:35 -0500

    Time: 1 ms (55.62%) | Memory: 52 MB (36.49%) - LeetSync

---
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.js
@@ -0,0 +1,28 @@
+// Level Order Traversal
+// Time and Space: O(n)
+var isCompleteTree = function(root) {
+    let queue = [[root]], placed = false
+    while (queue.length) {
+        let lvl = queue.shift();
+        let nxLvl = []
+        for (let n of lvl) {
+            if (n.right && !n.left) {
+                return false;
+                }
+            
+            if (placed &&( n.left || n.right)) {
+                return false;
+                }
+            if (n.left && n.right) {
+                nxLvl.push(n.left, n.right)
+            } else if (n.left) {
+                nxLvl.push(n.left)
+                placed = true;
+            } else if (!n.left && !n.right) {
+                placed = true;
+            }
+        }
+        if (nxLvl.length) queue.push(nxLvl)
+    }
+    return true;
+};
