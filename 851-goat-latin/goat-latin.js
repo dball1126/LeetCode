612f4dfa2628c56e3789cd46fe2baaf3c2c3e438
Daniel Ball <g.thirteen333@gmail.com>
2024-12-06 03:05:46 -0500

    Time: 2 ms (15.13%) | Memory: 50.8 MB (21.21%) - LeetSync

---
+++ 851-goat-latin/goat-latin.js
@@ -0,0 +1,28 @@
+/**
+ * @param {string} sentence
+ * @return {string}
+ */
+var toGoatLatin = function(sentence) {
+    
+    const vowels = new Set(["a","e", "i", "o", "u"])
+    sentence = sentence.split(" ")
+    return sentence.map((s, idx) => {
+
+        let newStr = ""
+
+        if (vowels.has(s[0].toLowerCase())) {
+            newStr = s + "ma"
+        } else {
+            newStr = s.slice(1) + s[0] + "ma"
+
+        }
+
+        if (newStr.endsWith("a")) {
+            let v = idx + 1
+            while (v !== 0) {
+                v--; newStr += "a"
+            }
+        }
+        return newStr
+    }).join(" ")
+};
