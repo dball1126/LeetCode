40909adf6de6cd88fba0b4cd69dba56fe4ae6f33
Daniel Ball <g.thirteen333@gmail.com>
2024-12-07 23:08:50 -0500

    Time: 5 ms (40.27%) | Memory: 58.9 MB (34.44%) - LeetSync

--- 1028-interval-list-intersections/interval-list-intersections.js
+++ 1028-interval-list-intersections/interval-list-intersections.js
@@ -9,23 +9,13 @@
 var intervalIntersection = function(list1, list2) {
     if (!list1.length || !list2.length) return [];
     let result = [], idx1 = 0, idx2 = 0
-
     while (idx1 < list1.length && idx2 < list2.length) {
-        let val1x = Infinity, val1y = Infinity, val2x = Infinity, val2y = Infinity
-        if (idx1 < list1.length) {
-            [val1x, val1y] = list1[idx1]
-        }
-        if (idx2 < list2.length) {
-            [val2x, val2y] = list2[idx2]
-        }
+        let [val1x, val1y] = list1[idx1]
+        let [val2x, val2y] = list2[idx2]
         if (val1x >= val2x && val1x <= val2y || val2x >= val1x && val2x <= val1y) {
             result.push([Math.max(val1x, val2x), Math.min(val1y,val2y)])
         }
-        if (val2y <= val1y) {
-            idx2++
-        } else {
-            idx1++
-        }
+        val2y <= val1y ? idx2++ : idx1++
     }
     return result
 };
