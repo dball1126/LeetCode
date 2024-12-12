5d20642175777f3cf1d9336048390b1f2e2ad27c
Daniel Ball <g.thirteen333@gmail.com>
2024-12-12 01:44:42 -0500

    Time: 2 ms (47.72%) | Memory: 50.1 MB (39.77%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.js
+++ 14-longest-common-prefix/longest-common-prefix.js
@@ -8,10 +8,8 @@ var longestCommonPrefix = function(strs) {
     for (let str of strs) {
         maxLength = Math.min(maxLength, str.length)
     }
-
     let long = ""
     for (let i = 0; i < maxLength; i++) {
-
         for (let j = 0; j < strs.length; j++) {
             if (j > 0 && strs[j][i] !== strs[j-1][i]) {
                 return longest
@@ -20,6 +18,5 @@ var longestCommonPrefix = function(strs) {
         long += strs[0][i]
         longest = long
     }
-    
     return longest
 };
