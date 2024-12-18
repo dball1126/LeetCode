85595c8463adf72325f092ccef45c696b7d10c43
Daniel Ball <g.thirteen333@gmail.com>
2024-12-18 02:35:01 -0500

    Time: 53 ms (5.13%) | Memory: 59.7 MB (5.13%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.js
+++ 695-max-area-of-island/max-area-of-island.js
@@ -10,20 +10,24 @@ var maxAreaOfIsland = function(grid) {
     const dirs = [[-1, 0], [1,0], [0,1], [0,-1]]
     const visited = [...new Array(rowLen + 1)].map(a => [...new Array(colLen + 1)].fill(false))
 
-    const dfs = (r, c) => {
-        if (r < 0 || c < 0 || r >= rowLen || c >= colLen || visited[r][c] || grid[r][c] !== 1) return 0
-        let islands = 1;
-        visited[r][c] = true;
-        for (let [x, y] of dirs) {
-            let row = r + x, col = c + y
-            islands += dfs(row, col)
-        }
-        return islands
-    }
-
     for (let r = 0; r < rowLen; r++) {
         for (let c = 0; c < colLen; c++) {
-            maxArea = Math.max(maxArea, dfs(r, c))
+            let islandCount = 0
+            if (grid[r][c] === 1) {
+                let stack = [[r, c]]
+                while (stack.length) {
+                    let [row, col] = stack.pop()
+                    islandCount++
+                    visited[r][c] = true;
+                    for (let [x, y] of dirs) {
+                        let newRow = row + x, newcol = col + y
+                        if (newRow < 0 || newcol < 0 || newRow >= rowLen || newcol >= colLen || visited[newRow][newcol] || grid[newRow][newcol] !== 1) continue
+                        visited[newRow][newcol] = true
+                        stack.push([newRow, newcol])
+                    }
+                }
+            }
+            maxArea = Math.max(maxArea, islandCount)
         }
     }
     return maxArea
