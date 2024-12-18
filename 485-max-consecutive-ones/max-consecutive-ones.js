003faf9cd1aaa0998c9df5922167f988357a2ab7
Daniel Ball <g.thirteen333@gmail.com>
2024-12-18 16:20:24 -0500

    Time: 4 ms (28.53%) | Memory: 53.9 MB (51.19%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.js
+++ 485-max-consecutive-ones/max-consecutive-ones.js
@@ -6,17 +6,15 @@
 // Time: O(n)
 // Space: O(1)
 var findMaxConsecutiveOnes = function(nums) {
-    let idx = 0, max = 0
+    let idx = 0, max = 0;
     while (idx < nums.length) {
         let tempIdx = idx;
-        while (nums[idx] === 1) { 
-            idx++
-        }
+        while (nums[idx] === 1) idx++;
         if (nums[tempIdx] === 1) {
-            max = Math.max(max, idx - tempIdx)
+            max = Math.max(max, idx - tempIdx);
         } else {
             idx++;
         }
     }
-    return max
+    return max;
 };
