3829e550b32c726ff36dc1f999d59a903c3dd437
Daniel Ball <g.thirteen333@gmail.com>
2024-12-18 01:20:11 -0500

    Time: 0 ms (100.00%) | Memory: 48.8 MB (85.48%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.js
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.js
@@ -1,8 +1,33 @@
-
-// Postorder Traversal
-// LEFt, RIGHT, ROOT
-// Recursive
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
 var postorderTraversal = function(root) {
-    if (!root) return []
-    return [...postorderTraversal(root.left), ...postorderTraversal(root.right), root.val]
-}
+    if (!root) return [];
+    
+    let postorder = [];
+    let stack = [root];
+    
+    while (stack.length > 0) {
+        let node = stack.pop();
+        
+        // Add the current node's value to the postorder array
+        postorder.push(node.val);
+        
+        // Push the left and right children to the stack
+        if (node.left) stack.push(node.left);
+        if (node.right) stack.push(node.right);
+    }
+    
+    // Since we were adding nodes to the postorder array in reverse order,
+    // we need to reverse the array to get the correct postorder traversal
+    return postorder.reverse();
+};
