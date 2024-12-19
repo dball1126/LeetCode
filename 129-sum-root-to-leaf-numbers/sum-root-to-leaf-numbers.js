752b5da7a16b7b452f3a621ca3ed8d31cdfc05e3
Daniel Ball <g.thirteen333@gmail.com>
2024-12-18 20:25:32 -0500

    Time: 0 ms (100.00%) | Memory: 50.6 MB (58.82%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.js
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.js
@@ -1,11 +1,15 @@
 var sumNumbers = function(root) {
-    let totalSum = 0
-    const dfs = (nde, sum) => {
-        if (!nde) return totalSum += parseInt(sum)
-        if (!nde.left && !nde.right) return totalSum += parseInt(sum + nde.val)
-        if (nde.left) dfs(nde.left, sum + nde.val)
-        if (nde.right) dfs(nde.right, sum + nde.val)
+    if (!root) return 0
+    let totalSum = 0, stack = [[root, ""]]
+
+    while (stack.length) {
+        let [nde, sum] = stack.pop();
+        if (!nde.left && !nde.right) {
+            totalSum += parseInt(sum + nde.val)
+            continue;
+        }
+        if (nde.left) stack.push([nde.left, sum + nde.val])
+        if (nde.right) stack.push([nde.right, sum + nde.val])
     }
-    dfs(root, "")
     return totalSum
 };
