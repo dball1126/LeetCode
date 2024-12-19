65a7be9cc528ecedf60f017eb87a3d84a5e0faa3
Daniel Ball <g.thirteen333@gmail.com>
2024-12-19 05:23:27 -0500

    Time: 0 ms (100.00%) | Memory: 53.4 MB (13.21%) - LeetSync

--- 792-binary-search/binary-search.js
+++ 792-binary-search/binary-search.js
@@ -3,15 +3,19 @@
  * @param {number} target
  * @return {number}
  */
+// Binary Search
+// Time: O(log(n))
+// Space: O(1)
 var search = function(nums, target) {
-    let low = 0, high = nums.length-1
-    while ( low < high) {
-        let m = Math.floor((high + low) / 2) + 1
-
-        if (target >= nums[m]) {
-            low = m
+    let low = 0, high = nums.length -1
+    while (low < high) {
+        let m = Math.floor((high + low) / 2) + 1;
+        if (nums[m] === target) {
+            return m
+        } else if (target < nums[m]) {
+            high = m - 1;
         } else {
-            high = m - 1
+            low = m;
         }
     }
     return nums[low] === target ? low : -1
