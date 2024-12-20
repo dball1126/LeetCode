5cd61b481d8f4aef9e3e7bbaec82dafff928e632
Daniel Ball <g.thirteen333@gmail.com>
2024-12-19 22:08:33 -0500

    Time: 1 ms (83.80%) | Memory: 50 MB (72.67%) - LeetSync

--- 807-custom-sort-string/custom-sort-string.js
+++ 807-custom-sort-string/custom-sort-string.js
@@ -1,24 +1,32 @@
+/**
+ * @param {string} order
+ * @param {string} s
+ * @return {string}
+ */
 // Hash Map
-// Time: 2n = O(n)
-// Space: O(n)
+// Time: O(n)...for the input string...order is constant at 26
+// Space: O(1)...constant at size 26;
 var customSortString = function(order, s) {
-    if (!order) return s;
-    let map = new Map(), perm = "";
-    for (let c of order) {
-        if (!map.has(c)) map.set(c,[])
+    let charMap = new Map(), result = "";
+    for (let i = 0; i < s.length; i++) {
+        if (!charMap.has(s[i])) charMap.set(s[i], 0);
+        charMap.set(s[i], charMap.get(s[i]) + 1);
     }
-    map.set("any", [])
-    for (let c of s) {
-        if (map.has(c)) {
-            map.get(c).push(c)
-        } else {
-            map.get("any").push(c)
+    for (let i = 0; i < order.length; i++) {
+        if (charMap.has(order[i])) {
+            let value = charMap.get(order[i]);
+            while (value !== 0) {
+                result += order[i];
+                value--;
+            }
+            charMap.delete(order[i]);
         }
     }
-    for (let i = order.length-1; i >= 0; i--) {
-        let arr = map.get(order[i])
-        perm = arr.join("") + perm
+    for (let [k, v] of charMap) {
+        while (v !== 0) {
+            result += k;
+            v--;
+        }
     }
-    perm = perm + map.get("any").join("");
-    return perm
+    return result;
 };
