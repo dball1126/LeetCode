f7c3f4dcdf8e987a9a0fa46dbfa117af75adb874
Daniel Ball <g.thirteen333@gmail.com>
2024-12-20 11:31:47 -0500

    Time: 413 ms (9.17%) | Memory: 58.6 MB (20.55%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.js
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.js
@@ -1,118 +1,40 @@
-class MinHeapNum {
-    constructor() {
-       this.array = [undefined];
-    }
-
-    getParent(idx) {
-        return Math.floor(idx / 2);
-      }
-  
-    getLeftChild(idx) {
-        return idx * 2;
-    }
-  
-    getRightChild(idx) {
-        return idx * 2 + 1;
-    }
-
-    insert(node) {
-        this.array.push(node);
-        this.siftUp(this.array.length - 1);
-    }
-
-    siftUp(idx) {
-        if (idx === 1) return;
-        let pIdx = this.getParent(idx)
-
-        if (this.array[pIdx] > this.array[idx]) {
-            [this.array[idx], this.array[pIdx]] = [this.array[pIdx], this.array[idx]];
-            this.siftUp(pIdx);
-        }
-    }
-
-    poll() { 
-        if (this.array.length <= 1) return undefined;
-        if (this.array.length === 2) return this.array.pop();
-        let max = this.array[1];
-        this.array[1] = this.array.pop();
-
-        this.siftDown(1);
-        return max;
-    }   
-
-    siftDown(idx) {
-        if (this.array.length <= 2) return;
-        let node = this.array[idx]
-
-        let leftIdx = this.getLeftChild(idx);
-        let rightIdx = this.getRightChild(idx);
-
-        let lNodeVal = this.array[leftIdx] !== undefined ? this.array[leftIdx] : Infinity;
-        let rNodeVal = this.array[rightIdx] !== undefined ? this.array[rightIdx] : Infinity
-        
-        if (node < lNodeVal && node < rNodeVal) return;
-
-        let swapIdx;
-        if (lNodeVal < rNodeVal) {
-            swapIdx = leftIdx
-        }  else {
-            swapIdx = rightIdx
-        }
-
-        [this.array[swapIdx], this.array[idx]] = [this.array[idx], this.array[swapIdx]]
-        this.siftDown(swapIdx);
-        
-    }
-
-    isEmpty() {
-        return this.array.length <= 1
-    }
-
-    peek() {
-        return this.array[1];
-    }
-
-    size() {
-        return this.array.length
-    }
-}
-
-
-
-function ListNode(val, next) {
-    this.val = (val===undefined ? 0 : val)
-    this.next = (next===undefined ? null : next)
-}
-
-/**
- * @param {ListNode[]} lists
- * @return {ListNode}
- */
+// Merge Lists
+// Time: O(n * log(k))...n is the number of elements, and k is the number of lists
 var mergeKLists = function(lists) {
-    const minHeap = new MinHeapNum();
-    let result = null
-
-    for (let list of lists) {
-        while (list) {
-            minHeap.insert(list.val)
-            list = list.next;
-        }
+    if (!lists.length) return null;
+    
+    while (lists.length > 1) {
+        let list1 = lists.pop();
+        let list2 = lists.pop();
+        let newList = mergeList(list1, list2);
+        lists.push(newList);
     }
-    let curr = null;
-    while (!minHeap.isEmpty()) {
-        let node = new ListNode(minHeap.poll())
-        if (!result) {
-            result = node
-            curr = node
+    return lists[0]
+};
+
+const mergeList = (list1, list2) => {
+    let newList = null, curr = null;
+
+    while (list1 || list2) {
+        let v1 = list1 ? list1.val : Infinity;
+        let v2 = list2 ? list2.val : Infinity;
+        let val;
+        if (v1 <= v2) {
+            val = v1;
+            list1 = list1.next;
+        } else {
+            val = v2;
+            list2 = list2.next;
+        }
+        let node = new ListNode(val);
+        if (!newList) {
+            newList = node;
+            curr = node;
         } else {
             curr.next = node;
             curr = node;
         }
     }
 
-    return result
-};
-
-
-
-
+    return newList;
+}
