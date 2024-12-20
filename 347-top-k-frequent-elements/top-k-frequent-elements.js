fdb776067db3b9240914925cde7b9fc4667cc99e
Daniel Ball <g.thirteen333@gmail.com>
2024-12-20 14:26:14 -0500

    Time: 7 ms (92.74%) | Memory: 53.1 MB (81.83%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -1,21 +1,31 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number[]}
- */
 var topKFrequent = function(nums, k) {
-    let map = new Map()
-
-    nums.forEach(v => {
-        if (!map.has(v)) map.set(v, 0)
-        map.set(v, map.get(v) + 1)
-    })
-    let arr = Array.from(map)
-    arr.sort((a,b) => b[1] - a[1])
-    let freq = [], i = 0;
-    while (i < k && i < arr.length) {
-        freq.push(arr[i][0])
-        i++;
+    const map = new Map()
+    for (let n of nums) {
+        if (!map.has(n)) map.set(n, 0)
+        map.set(n, map.get(n) + 1)
     }
-    return freq
+    const pairs = Array.from(map)
+    return quickSelect(pairs, 0, pairs.length-1, pairs.length - k)
 };
+
+const quickSelect = (nums, left, right, k) => {
+    let pivotIndex = partition(nums, left, right);
+
+    if (pivotIndex > k) return quickSelect(nums, left, pivotIndex - 1, k);
+    else if (pivotIndex < k) return quickSelect(nums, pivotIndex+1, right, k);
+    return nums.slice(pivotIndex).map(pair => pair[0]);
+}
+
+const partition = (nums, left, right) => {
+    let pivot = nums[right][1], i = left, j = right -1;
+    while (i <= j) {
+        while (nums[i] && nums[i][1] < pivot) i++;
+        while (nums[j] && nums[j][1] > pivot) j--;
+        if (i <= j) {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            i++; j--;
+        }
+    }
+    [nums[i], nums[right]] = [nums[right], nums[i]];
+    return i;
+}
