689d2a41e2f6b43dccbab636b69fdeb69914e4e2
Daniel Ball <g.thirteen333@gmail.com>
2024-12-22 20:07:06 -0500

    Time: 33 ms (85.30%) | Memory: 59 MB (74.50%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -1,93 +1,36 @@
-class MinHeapNum {
-    constructor() {
-       this.array = [undefined];
-    }
-
-    getParent(idx) {
-        return Math.floor(idx / 2);
-      }
-  
-    getLeftChild(idx) {
-        return idx * 2;
-    }
-  
-    getRightChild(idx) {
-        return idx * 2 + 1;
-    }
-
-    insert(node) {
-        this.array.push(node);
-        this.siftUp(this.array.length - 1);
-    }
-
-    siftUp(idx) {
-        if (idx === 1) return;
-        let pIdx = this.getParent(idx)
-
-        if (this.array[pIdx] > this.array[idx]) {
-            [this.array[idx], this.array[pIdx]] = [this.array[pIdx], this.array[idx]];
-            this.siftUp(pIdx);
-        }
-    }
-
-    poll() { 
-        if (this.array.length <= 1) return undefined;
-        if (this.array.length === 2) return this.array.pop();
-        let max = this.array[1];
-        this.array[1] = this.array.pop();
-
-        this.siftDown(1);
-        return max;
-    }   
-
-    siftDown(idx) {
-        if (this.array.length <= 2) return;
-        let node = this.array[idx]
-
-        let leftIdx = this.getLeftChild(idx);
-        let rightIdx = this.getRightChild(idx);
-
-        let lNodeVal = this.array[leftIdx] !== undefined ? this.array[leftIdx] : Infinity;
-        let rNodeVal = this.array[rightIdx] !== undefined ? this.array[rightIdx] : Infinity
-        
-        if (node < lNodeVal && node < rNodeVal) return;
-
-        let swapIdx;
-        if (lNodeVal < rNodeVal) {
-            swapIdx = leftIdx
-        }  else {
-            swapIdx = rightIdx
-        }
-
-        [this.array[swapIdx], this.array[idx]] = [this.array[idx], this.array[swapIdx]]
-        this.siftDown(swapIdx);
-        
-    }
-
-    isEmpty() {
-        return this.array.length <= 1
-    }
-
-    peek() {
-        return this.array[1];
-    }
-
-    size() {
-        return this.array.length
-    }
-}
 /**
  * @param {number[]} nums
  * @param {number} k
  * @return {number}
  */
+// QuickSelect Algorithm
+// Time: O(n)...on average...worst case which is rare is O(n^2)
+// Space: O(log(n))...recursive call stack on overage...worse case O(n) which is rare
 var findKthLargest = function(nums, k) {
-    const minHeap = new MinHeapNum()
-    for(let n of nums) {
-        minHeap.insert(n)
-        if (minHeap.size()-1 > k) {
-            minHeap.poll()
+    return quickSelect(nums, 0, nums.length-1, nums.length - k);
+};
+
+const quickSelect = (nums, left, right, k) => {
+    let pivot = partition(nums,left,right);
+    if (pivot < k) {
+        return quickSelect(nums, pivot + 1, right, k);
+    } else if (pivot > k) {
+        return quickSelect(nums, left, pivot - 1 , k);
+    } else {
+        return nums[pivot]
+    }
+}
+const partition = (nums, left, right) => {
+    let pivot = nums[right], i = left, j = right-1;
+    while (i <= j) {
+        while (nums[i] !== undefined && nums[i] < pivot) i++;
+        while (nums[j] !== undefined && nums[j] > pivot) j--;
+
+        if (i <= j) {
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            i++; j--;
         }
     }
-    return minHeap.poll()
-};
+    [nums[i], nums[right]] = [nums[right], nums[i]];
+    return i;
+}
