bd91c3d655bc11cba940de0936efb59ecf6b1f24
Daniel Ball <g.thirteen333@gmail.com>
2024-12-26 01:14:43 -0500

    Time: 31 ms (92.18%) | Memory: 60 MB (92.46%) - LeetSync

--- 721-accounts-merge/accounts-merge.js
+++ 721-accounts-merge/accounts-merge.js
@@ -2,14 +2,14 @@
  * @param {string[][]} accounts
  * @return {string[][]}
  */
+// Union Find
+// Time: O((n * k) * log(n * k))...n for accounts, k for the longest account
+// Space: O(n * k)
 var accountsMerge = function(accounts) {
-    let acctsLen = accounts.length;
+    let acctsLen = accounts.length, emails = new Map()
     let roots = [...(new Array(acctsLen)).keys()];
     let ranks = [...new Array(acctsLen)].fill(1);
-    let emails = new Map();
-
     const findRoot = (n) => roots[n] === n ? n : findRoot(roots[n]);
-
     const union = (n1, n2) =>{
         let p1 = findRoot(n1), p2 = findRoot(n2);
         if (p1 === p2) return;
@@ -22,7 +22,6 @@ var accountsMerge = function(accounts) {
             ranks[p1]++
         }
     }
-
     for (let i = 0; i < acctsLen; i++) {
         for (let j = 1; j < accounts[i].length; j++) {
             let email = accounts[i][j];
@@ -35,20 +34,14 @@ var accountsMerge = function(accounts) {
             }
         }
     }
-
-    let groups = new Map()
+    let groups = new Map(), result = []
     for (let [email, root] of emails) {
         let newRoot = findRoot(root)
         if (!groups.has(newRoot)) groups.set(newRoot, new Set())
         groups.get(newRoot).add(email)
     }
-    let result = []
     for (let [root, set] of groups) {
-        let groupEmails = []
-        groupEmails.push(accounts[root][0])
-        groupEmails.push(...Array.from(set).sort())
-        result.push(groupEmails)
+        result.push([accounts[root][0], ...Array.from(set).sort()])
     }
-
     return result;
 };
