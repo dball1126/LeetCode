da738eb9ae3b6452d5c84a507278aac0e34a7f11
Daniel Ball <g.thirteen333@gmail.com>
2024-12-29 01:36:20 -0500

    Time: 129 ms (24.03%) | Memory: 93.9 MB (27.26%) - LeetSync

--- 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.js
+++ 2232-adding-spaces-to-a-string/adding-spaces-to-a-string.js
@@ -1,25 +1,20 @@
-/**
- * @param {string} s
- * @param {number[]} spaces
- * @return {string}
- */
+// Two pointers
+// Time: O(n)
+// Space: O(1)
 var addSpaces = function(s, spaces) {
-    let i = 0, j = 0
-    let result = "";
-
+    let i = 0, j = 0, result = "";
     while (i < s.length || j < spaces.length) {
         if (j >= spaces.length) {
-            result += s[i]
+            result += s[i];
             i++;
         } else {
             while (i !== spaces[j]) {
-                result += s[i]
+                result += s[i];
                 i++
             }
-            result += " "
-            j++
+            result += " ";
+            j++;
         }
     }
-
     return result;
 };
