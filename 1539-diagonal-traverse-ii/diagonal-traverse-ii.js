986f39655262e7657b6ce780d2eff000ae56bdf8
Daniel Ball <g.thirteen333@gmail.com>
2024-12-31 01:46:59 -0500

    Time: 171 ms (13.86%) | Memory: 99.1 MB (5.94%) - LeetSync

--- 1539-diagonal-traverse-ii/diagonal-traverse-ii.js
+++ 1539-diagonal-traverse-ii/diagonal-traverse-ii.js
@@ -3,20 +3,18 @@
  * @return {number[]}
  */
 var findDiagonalOrder = function(nums) {
-    const diags = new Map()
-    let result = [];
+    let diags = new Map, result = [];
     for (let r = nums.length-1; r >=0; r--) {
         for (let c = nums[r].length-1; c >= 0; c--) {
-            let key = r + c
-            if (!diags.has(key)) diags.set(key, [])
-            diags.get(key).push(nums[r][c])
+            let key = r + c;
+            if (!diags.has(key)) diags.set(key, []);
+            diags.get(key).push(nums[r][c]);
         }
     }
-
     let i = 0;
     while (diags.has(i)) {
-        result.push(...diags.get(i))
-        i++
+        result.push(...diags.get(i));
+        i++;
     }
     return result;
 };
