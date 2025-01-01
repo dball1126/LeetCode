120f7fa8becdd8635860842f3e7533743e6d68fe
Daniel Ball <g.thirteen333@gmail.com>
2025-01-01 08:11:05 -0500

    Time: 302 ms (5.03%) | Memory: 77.8 MB (5.03%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.js
+++ 6-zigzag-conversion/zigzag-conversion.js
@@ -2,11 +2,13 @@ var convert = function(s, numRows) {
     if (numRows <= 1) return s
     let grid = [...new Array(numRows + 1)].map(a =>
         [...new Array(s.length+1)].fill(""));
-    let result = "";
-
+    let result = "", maxColLen = -Infinity;
     let d = 'd', c = 0, r = 0, i = 0;
+    for (let i = 0; i < numRows; i++) {
+        maxColLen = Math.max(maxColLen, grid[i].length);
+    }
 
-    while (r < numRows && c < s.length && i < s.length) {
+    while (r < numRows && c < maxColLen && i < s.length) {
         if (d === 'd') {
             if (grid[r]) grid[r][c] = s[i];
 
@@ -18,16 +20,16 @@ var convert = function(s, numRows) {
         } else {
             if (grid[r]) grid[r][c] = s[i];
 
-            while (r-1 >= 0 && c+1 < s.length && i < s.length) {
+            while (r-1 >= 0 && c+1 < maxColLen && i < s.length) {
                 grid[r][c] = s[i];
                 i++; r--; c++;
             } 
             d = 'd'
         }
-        if (r === numRows -1 && c === s.length-1) break;
+        if (r === numRows -1 && c === maxColLen-1) break;
     }
     for (let r = 0; r < numRows; r++) {
-        for (let c = 0; c < s.length; c++) {
+        for (let c = 0; c < maxColLen; c++) {
             result += grid[r][c] || "";
         }
     }
