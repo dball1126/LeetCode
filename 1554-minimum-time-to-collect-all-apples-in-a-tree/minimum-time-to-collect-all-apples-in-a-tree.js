61b2052aa7ee1b34c763dd8b089c132b8598ac34
Daniel Ball <g.thirteen333@gmail.com>
2025-01-05 01:27:03 -0500

    Time: 91 ms (56.10%) | Memory: 96.1 MB (8.54%) - LeetSync

---
+++ 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.js
@@ -0,0 +1,41 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @param {boolean[]} hasApple
+ * @return {number}
+ */
+// Depth-First-Search
+// Time and Space: O(e)...for edges
+var minTime = function(n, edges, hasApple) {
+    let visited = new Set(), adjList = new Map();
+    for (let [x, y] of edges) {
+        if (!adjList.has(x)) adjList.set(x, []);
+        if (!adjList.has(y)) adjList.set(y, []);
+        adjList.get(x).push(y);
+        adjList.get(y).push(x);
+
+    }
+    const dfs = (nde) => {
+        visited.add(nde);
+        let path = 0;
+
+        if (adjList.has(nde)) {
+            for (let n of adjList.get(nde)) {
+                if (!visited.has(n)) {
+                    visited.add(n);
+                    path += dfs(n);
+                }
+            }
+        }
+        if (nde === 0) {
+            return path;
+        } else {
+            if (path !== 0 || hasApple[nde]) {
+                return path + 2;
+            } else {
+                return 0;
+            }
+        }
+    }
+    return dfs(0);
+};
