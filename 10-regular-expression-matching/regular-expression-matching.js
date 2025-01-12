97583aec4a095513afb54ac5b3383038e1ef5f05
Daniel Ball <g.thirteen333@gmail.com>
2025-01-12 06:23:23 -0500

    Time: 1965 ms (5.11%) | Memory: 50.1 MB (93.37%) - LeetSync

---
+++ 10-regular-expression-matching/regular-expression-matching.js
@@ -0,0 +1,22 @@
+var isMatch = function(s, p) {
+    const recurse = (i, j) => {
+        if (i === s.length && j >= p.length) return true;
+        if (j >= p.length) return false;
+
+        let v1 = false, v2 = false, v3 = false;
+
+        if (s[i] === p[j] || p[j] === '.') {
+            v1 = recurse(i+1, j+1);
+            if (v1) return true;
+        }
+        if (p[j+1] === '*') {
+            v2 = recurse(i, j+2);
+            if (v2) return true;
+            if (i < s.length && (p[j] === s[i] || p[j] === '.')) {
+                v3 = recurse(i+1, j);
+            }
+        }
+        return v1 || v2 || v3;
+    }
+    return recurse(0 , 0);
+};
