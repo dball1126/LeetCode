82215d36fbaf7411ca26134d2d2c9b7b01c76835
Daniel Ball <g.thirteen333@gmail.com>
2025-01-16 00:28:07 -0500

    Time: 147 ms (62.99%) | Memory: 82.6 MB (63.68%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.js
+++ 295-find-median-from-data-stream/find-median-from-data-stream.js
@@ -1,40 +1,40 @@
-
-// this.one = new MaxPriorityQueue();
-// this.two = new MinPriorityQueue();
-
-
 var MedianFinder = function() {
-    this.minHeap = new MinPriorityQueue();
-    this.maxHeap = new MaxPriorityQueue();
-};
-
-/** 
- * @param {number} num
- * @return {void}
- */
-MedianFinder.prototype.addNum = function(num) {
-    if (this.minHeap.isEmpty() || num > this.minHeap.front().element) {
-        this.minHeap.enqueue(num)
-    }else {
-        this.maxHeap.enqueue(num)
-    }
-
-    if (this.minHeap.size() - this.maxHeap.size() > 1) {
-        this.maxHeap.enqueue(this.minHeap.dequeue().element)
-    } else if (this.maxHeap.size() - this.minHeap.size() > 1) {
-        this.minHeap.enqueue(this.maxHeap.dequeue().element)
-    }
-};
-
-/**
- * @return {number}
- */
-MedianFinder.prototype.findMedian = function() {
-    if (this.minHeap.size() === this.maxHeap.size()) {
-        return (this.minHeap.front().element + this.maxHeap.front().element) / 2
-    } else if (this.minHeap.size() > this.maxHeap.size()) {
-        return this.minHeap.front().element
+    this.lo = new MinPriorityQueue();
+    this.hi = new MaxPriorityQueue();
+  };
+  
+  /** 
+   * @param {number} num
+   * @return {void}
+   */
+  MedianFinder.prototype.addNum = function(n) {
+    if (this.lo.isEmpty() || n > this.lo.front().element) {
+        this.lo.enqueue(n);
     } else {
-        return this.maxHeap.front().element
+        this.hi.enqueue(n);
+    } 
+
+    if (this.lo.size() > (this.hi.size()+1)) {
+
+        this.hi.enqueue(this.lo.dequeue().element)
+    } else if (this.hi.size() > (this.lo.size() + 1) ) {
+
+        this.lo.enqueue(this.hi.dequeue().element)
     }
-};
+  };
+  
+  /**
+   * @return {number}
+   */
+  MedianFinder.prototype.findMedian = function() {
+  
+    if (this.lo.size() > this.hi.size()) {
+        return this.lo.front().element
+    } else if (this.hi.size() > this.lo.size()) {
+        return this.hi.front().element
+    } else {
+        let v1 = this.lo.front().element || 0, v2 = this.hi.front().element || 0
+        return (v1 + v2) / 2
+    }
+  };
+  
