76f08b763097c5784dbcf20bedfddd0cf06bd46a
Daniel Ball <g.thirteen333@gmail.com>
2025-01-17 01:29:48 -0500

    Time: 4 ms (82.46%) | Memory: 54.9 MB (68.44%) - LeetSync

---
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -0,0 +1,32 @@
+var findMedianSortedArrays = function(nums1, nums2) {
+    
+    if (nums1.length > nums2.length) {
+        [nums1, nums2] = [nums2, nums1];
+    }
+    let m = nums1.length;
+    let n = nums2.length;
+    let left = 0, right = m;
+
+    while (left <= right) {
+        let partitionA = Math.floor((left + right) / 2);
+        let partitionB = Math.floor((n + m + 1) / 2 - partitionA);
+
+        let maxLeftA = partitionA === 0 ? -Infinity : nums1[partitionA-1];
+        let maxLeftB = partitionB === 0 ? -Infinity : nums2[partitionB-1];
+        let minLeftA = partitionA === m ? Infinity : nums1[partitionA];
+        let minLeftB = partitionB === n ? Infinity : nums2[partitionB];
+
+        if (maxLeftA <= minLeftB && maxLeftB <= minLeftA) {
+            if (((n+m)%2) === 0) {
+                return ((Math.max(maxLeftA, maxLeftB) + Math.min(minLeftA, minLeftB)) / 2)
+            } else {
+                return Math.max(maxLeftA, maxLeftB);
+            }
+        } else if (minLeftA > maxLeftB) {
+            right = partitionA -1
+        } else {
+            left = partitionA + 1
+        }
+    }
+
+};
