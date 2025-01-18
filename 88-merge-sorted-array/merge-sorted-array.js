1cc29da5d270560016a584644b0a2fd4b12d13ba
Daniel Ball <g.thirteen333@gmail.com>
2025-01-18 04:47:41 -0500

    Time: 0 ms (100.00%) | Memory: 51.5 MB (7.03%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -1,18 +1,21 @@
-// Merge Sorted Arrays
-// Time: O(n + m)
-// Space: O(1)
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
 var merge = function(nums1, m, nums2, n) {
-    let idx = nums1.length-1;
-    m-=1; n-=1;
-    while (m >= 0 || n >= 0) {
-        if (m < 0 || (n >= 0 && nums2[n] >= nums1[m])) {
-            [nums1[idx], nums2[n]] = [nums2[n], nums1[idx]];
-            n--;
-        } else if (n < 0 || (m >= 0 && nums1[m] >= nums2[n])) {
-            [nums1[idx], nums1[m]] = [nums1[m], nums1[idx]];
-            m--;
+    let i = m-1, j = n-1, k = nums1.length-1;
+
+    while (i >= 0 || j >= 0) {
+        if (i < 0 || (j >= 0 && nums2[j] >= nums1[i])) {
+            [nums1[k], nums2[j]] = [nums2[j], nums1[k]]
+            j--
+        } else if (j < 0 || (i >= 0 && nums1[i] >= nums2[j])) {
+            [nums1[k], nums1[i]] = [nums1[i], nums1[k]]
+            i--
         }
-        idx--;
+        k--
     }
-    return nums1
 };
