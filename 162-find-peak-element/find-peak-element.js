84f2dd2cf011ef36866f34e5011bdb9bb8e0f1a9
Daniel Ball <g.thirteen333@gmail.com>
2025-01-18 08:50:37 -0500

    Time: 0 ms (100.00%) | Memory: 48.9 MB (75.70%) - LeetSync

--- 162-find-peak-element/find-peak-element.js
+++ 162-find-peak-element/find-peak-element.js
@@ -1,20 +1,18 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
 var findPeakElement = function(nums) {
-    let low = 0, high = nums.length-1
-    while (low < high) {
-        let m = Math.floor((high + low) / 2) + 1
-        let prev = nums[m-1] || -Infinity, nx =nums[m+1] || -Infinity
-        
-        if (nums[m] > prev && nums[m] > nx) {
+    let lo = 0, hi = nums.length-1;
+
+    while (lo < hi) {
+        let m = Math.floor((hi + lo) / 2);
+        let prev = (m-1) >= 0 ? nums[m-1] : -Infinity;
+        let next = (m+1) < nums.length ? nums[m+1] : -Infinity;
+
+        if (nums[m] > prev && nums[m] > next) {
             return m
-        } else if (prev > nums[m]) {
-            high = m - 1
+        } else if (nums[m] < next) {
+            lo = m+1
         } else {
-            low = m
+            hi = m -1;
         }
     }
-    return low
+    return lo;
 };
