03ac1ad1ab6074a7c6e31f79b9062f2b53a7fd53
Daniel Ball <g.thirteen333@gmail.com>
2025-01-19 01:17:42 -0500

    Time: 35 ms (82.36%) | Memory: 59 MB (72.76%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.js
@@ -1,27 +1,19 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
- */
-// QuickSelect Algorithm
-// Time: O(n)...on average...worst case which is rare is O(n^2)
-// Space: O(log(n))...recursive call stack on overage...worse case O(n) which is rare
 var findKthLargest = function(nums, k) {
-    return quickSelect(nums, 0, nums.length-1, nums.length - k);
+    return quickSelect(0, nums.length-1, nums.length - k, nums)
 };
-
-const quickSelect = (nums, left, right, k) => {
-    let pivot = partition(nums,left,right);
-    if (pivot < k) {
-        return quickSelect(nums, pivot + 1, right, k);
-    } else if (pivot > k) {
-        return quickSelect(nums, left, pivot - 1 , k);
+const quickSelect = (left, right, k, nums) => {
+    let pivot = partition(left, right, nums);
+    if (pivot > k) {
+        return quickSelect(left, pivot -1, k, nums);
+    } else if (pivot < k) {
+        return quickSelect(pivot + 1, right, k, nums);
     } else {
-        return nums[pivot]
+        return nums[k];
     }
 }
-const partition = (nums, left, right) => {
+const partition = (left, right, nums) => {
     let pivot = nums[right], i = left, j = right-1;
+
     while (i <= j) {
         while (nums[i] !== undefined && nums[i] < pivot) i++;
         while (nums[j] !== undefined && nums[j] > pivot) j--;
