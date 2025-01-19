a50bdf6e5524bcbdcd85484594c640cb5c295f0e
Daniel Ball <g.thirteen333@gmail.com>
2025-01-19 05:21:20 -0500

    Time: 0 ms (100.00%) | Memory: 48.8 MB (85.46%) - LeetSync

--- 408-valid-word-abbreviation/valid-word-abbreviation.js
+++ 408-valid-word-abbreviation/valid-word-abbreviation.js
@@ -1,23 +1,28 @@
-// Two-Pointer Technique
-// Time: O(n)
-// Space: O(1)
 var validWordAbbreviation = function(word, abbr) {
-    let i = 0, j = 0;
-    while (j < abbr.length) {
-        if (i >= word.length) return false;
-        if (isNaN(abbr[j])) {
-            if (word[i] !== abbr[j]) return false;
-            i++; j++;
-        } else {
-            let v = "";
-            if (abbr[j] === "0") return false;
-            while (j < abbr.length && !isNaN(abbr[j])) {
-                v += abbr[j];
-                j++;
+    let wLen = word.length, aLen = abbr.length;
+
+    let validate = (i, j) => {
+        let chance1 = false, chance2 = false
+        while (i < wLen && j < aLen) {
+            if (isNaN(abbr[j]) && abbr[j] === word[i]) {
+                i++; j++;
+            } else if (!isNaN(abbr[j])) {
+                let v1 = parseInt(abbr[j])
+                if (v1 === 0) return false
+                if (!isNaN(abbr[j+1])) {
+                    let v2 = parseInt(abbr[j] + abbr[j+1])
+                    if (j+2 < aLen && !isNaN(abbr[j+2])) return false;
+                    return validate(i + v2, j+2)
+                    if (chance2) return true;
+                } else {
+
+                    return validate(i + v1, j+1)
+                }
+            } else {
+                return false;
             }
-            v = parseInt(v)
-            i += v;
         }
+        return i === wLen && j === aLen;
     }
-    return i === word.length && j === abbr.length;
+    return validate(0,0);
 };
