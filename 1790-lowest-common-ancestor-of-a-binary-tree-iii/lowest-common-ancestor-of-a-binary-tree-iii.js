0d6c55d05e7bf615b91c9ce8c563809dd8734246
Daniel Ball <g.thirteen333@gmail.com>
2025-01-19 01:44:58 -0500

    Time: 68 ms (44.93%) | Memory: 54.7 MB (25.22%) - LeetSync

--- 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.js
+++ 1790-lowest-common-ancestor-of-a-binary-tree-iii/lowest-common-ancestor-of-a-binary-tree-iii.js
@@ -1,42 +1,21 @@
-/**
- * // Definition for a _Node.
- * function _Node(val) {
- *    this.val = val;
- *    this.left = null;
- *    this.right = null;
- *    this.parent = null;
- * };
- */
-
-/**
- * @param {_Node} p
- * @param {_Node} q
- * @return {_Node}
- */
 var lowestCommonAncestor = function(p, q) {
-
-    const getParent = (n) => !n.parent ? n : getParent(n.parent)
-
-    let n1 = getParent(p), result = null
-
-    const dfs = (n) => {
-        if (!n || result) return false
-
-        let left = dfs(n.left), right = dfs(n.right)
-        let curr = false;
-
-        if (q.val === n.val || p.val === n.val) {
-            curr = true;
-        }
-        if (!result) {
-            if (left && right ||
-                left && curr ||
-                right && curr) {
-                    result = n
-                }
-        }
-        return left || right || curr
+    const getRoot = (nde) => {
+        if (!nde.parent) return nde;
+        return getRoot(nde.parent);
     }
-    dfs(n1)
+    let root = getRoot(p), result = null;
+
+    const dfs = (nde) => {
+        if (result || !nde) return false;
+        let left = dfs(nde.left), right = dfs(nde.right);
+        let curr = (nde.val === p.val) || (nde.val === q.val);
+        if (!result) {
+            if ((left && right) || (curr && left) || (curr && right)) {
+                result = nde;
+            }
+        }
+        return left || right || curr;
+    }
+    dfs(root)
     return result;
 };
