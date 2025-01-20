31b0843fc5cfba3746635c79e2706b78a3c218b5
Daniel Ball <g.thirteen333@gmail.com>
2025-01-19 20:24:29 -0500

    Time: 347 ms (6.07%) | Memory: 62.1 MB (89.58%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.js
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.js
@@ -1,27 +1,37 @@
-/**
- * @param {number[][]} points
- * @param {number} k
- * @return {number[][]}
- */
- class Node {
-    constructor(value,row,col) {
-        this.value = value;
-        this.row = row;
-        this.col = col;
-    }
-}
 var kClosest = function(points, k) {
-    const maxHeap = new MaxPriorityQueue(), closestPoints = [];
+    return quickSelect(0, points.length-1, points.length - k, points)
+}
 
-    for (let [x, y] of points) {
-        let dist = (x-0)**2 + (y-0)**2;
-        maxHeap.enqueue(new Node(dist,x,y), dist);
-        if (maxHeap.size() > k) maxHeap.dequeue();
-    }
+const quickSelect = (left, right, k, points) => {
+    
+    let partition = getPartition(left,right,points);
 
-    while (!maxHeap.isEmpty()) {
-        let nde = maxHeap.dequeue();
-        closestPoints.push([nde.element.row, nde.element.col])
+    while (partition !== k) {
+        partition = getPartition(left,right,points)
+        if (partition > k) {
+            right = partition -1;
+        } else {
+            left = partition + 1;
+        }
     }
-    return closestPoints;
-};
+    return points.slice(k);
+}
+const getDist = (arr) => {
+    let [x, y] = arr;
+    return (x-0)**2  + (y-0)**2
+}
+const getPartition = (left,right,points) => {
+    let pivot = getDist(points[right]), i = left, j = right-1;
+    
+    while (i <= j) {
+        while (points[i] !== undefined && getDist(points[i]) > pivot) i++;
+        while (points[j] !== undefined && getDist(points[j]) < pivot) j--;
+
+        if (i <= j) {
+            [points[i], points[j]] = [points[j], points[i]];
+            i++; j--;
+        }
+    }
+    [points[i], points[right]] = [points[right], points[i]];
+    return i;
+}
