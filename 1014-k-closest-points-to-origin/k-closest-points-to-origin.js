d0f30fa56e86f86b2be8abbca643d73e894836ef
Daniel Ball <g.thirteen333@gmail.com>
2025-01-19 19:57:24 -0500

    Time: 78 ms (39.35%) | Memory: 70.2 MB (20.24%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.js
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.js
@@ -1,104 +1,27 @@
-class HeapNode {
-    constructor(val, key) {
-        this.val = val;
-        this.key = key
+/**
+ * @param {number[][]} points
+ * @param {number} k
+ * @return {number[][]}
+ */
+ class Node {
+    constructor(value,row,col) {
+        this.value = value;
+        this.row = row;
+        this.col = col;
     }
 }
-
-class MaxHeapO { // Object version
-    constructor() {
-       this.array = [null];
-    }
-
-    getParent(idx) {
-        return Math.floor(idx / 2);
-      }
-  
-    getLeftChild(idx) {
-        return idx * 2;
-    }
-  
-    getRightChild(idx) {
-        return idx * 2 + 1;
-    }
-
-    insert(node) {
-        this.array.push(node);
-        this.siftUp(this.array.length - 1);
-    }
-
-    siftUp(idx) {
-        if (idx === 1) return;
-        let pIdx = this.getParent(idx)
-        let pNode = this.array[pIdx]
-        let node = this.array[idx]
-        if (pNode === undefined || node === undefined) return;
-        if (pNode.val < node.val) {
-            [this.array[idx], this.array[pIdx]] = [this.array[pIdx], this.array[idx]];
-            this.siftUp(pIdx);
-        }
-    }
-
-    poll() { // delete min/max
-        if (this.array.length <= 1) return null;
-        if (this.array.length === 2) return this.array.pop();
-        let min = this.array[1];
-        this.array[1] = this.array.pop();
-
-        this.siftDown(1);
-        return min;
-    }   
-
-    siftDown(idx) {
-        if (this.array.length <= 2) return;
-        let node = this.array[idx]
-        if (!node) return;
-
-        let leftIdx = this.getLeftChild(idx);
-        let rightIdx = this.getRightChild(idx);
-
-        let lNodeVal = this.array[leftIdx] ? this.array[leftIdx].val : -Infinity;
-        let rNodeVal = this.array[rightIdx] ? this.array[rightIdx].val : -Infinity
-        
-        if (node.val > lNodeVal && node.val > rNodeVal) return;
-
-        let swapIdx;
-        if (lNodeVal > rNodeVal) {
-            swapIdx = leftIdx
-        }  else {
-            swapIdx = rightIdx
-        }
-
-        [this.array[swapIdx], this.array[idx]] = [this.array[idx], this.array[swapIdx]]
-        this.siftDown(swapIdx);
-        
-    }
-
-    isEmpty() {
-        return this.array.length <= 1
-    }
-
-    peek() {
-        return this.array[1];
-    }
-}
-
-// Max Heap
-// Time: O(n * log(k))
-// Space: O(k)
-var kClosest = function(points, k) {    
-    const maxHeap = new MaxHeapO(), result = [];
+var kClosest = function(points, k) {
+    const maxHeap = new MaxPriorityQueue(), closestPoints = [];
 
     for (let [x, y] of points) {
-        let distance = (x - 0)**2 + (y - 0)**2;
-        let nde = new HeapNode(distance, [x, y]);
-        maxHeap.insert(nde);
-        if (maxHeap.array.length-1 > k) maxHeap.poll();
+        let dist = (x-0)**2 + (y-0)**2;
+        maxHeap.enqueue(new Node(dist,x,y), dist);
+        if (maxHeap.size() > k) maxHeap.dequeue();
     }
 
     while (!maxHeap.isEmpty()) {
-        let node = maxHeap.poll();
-        result.push(node.key)
+        let nde = maxHeap.dequeue();
+        closestPoints.push([nde.element.row, nde.element.col])
     }
-    return result;
+    return closestPoints;
 };
