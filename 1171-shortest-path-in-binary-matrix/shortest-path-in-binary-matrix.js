bbab862a8a30b90c754a79e39bba62f8ab598815
Daniel Ball <g.thirteen333@gmail.com>
2025-01-21 22:54:37 -0500

    Time: 149 ms (16.86%) | Memory: 67 MB (9.15%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.js
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.js
@@ -1,111 +1,31 @@
+
 class Node {
-    constructor(r, c,val = 0) {
-        this.r = r;
-        this.c = c;
-        this.val = val
-
+    constructor(val, row, col) {
+        this.val = val;
+        this.row = row;
+        this.col = col;
     }
 }
-
-class MinHeap { // Object version
-    constructor() {
-       this.array = [null];
-    }
-
-    getParent(idx) {
-        return Math.floor(idx / 2);
-      }
-  
-    getLeftChild(idx) {
-        return idx * 2;
-    }
-  
-    getRightChild(idx) {
-        return idx * 2 + 1;
-    }
-
-    insert(node) {
-        this.array.push(node);
-        this.siftUp(this.array.length - 1);
-    }
-
-    siftUp(idx) {
-        if (idx === 1) return;
-        let pIdx = this.getParent(idx)
-        let pNode = this.array[pIdx]
-        let node = this.array[idx]
-        if (!pNode || !node) return;
-        if (pNode.val > node.val) {
-            [this.array[idx], this.array[pIdx]] = [this.array[pIdx], this.array[idx]];
-            this.siftUp(pIdx);
-        }
-    }
-
-    poll() { // delete min/max
-        if (this.array.length <= 1) return null;
-        if (this.array.length === 2) return this.array.pop();
-        let min = this.array[1];
-        this.array[1] = this.array.pop();
-
-        this.siftDown(1);
-        return min;
-    }   
-
-    siftDown(idx) {
-        if (this.array.length <= 2) return;
-        let node = this.array[idx]
-        if (!node) return;
-
-        let leftIdx = this.getLeftChild(idx);
-        let rightIdx = this.getRightChild(idx);
-
-        let lNodeVal = this.array[leftIdx] ? this.array[leftIdx].val : Infinity;
-        let rNodeVal = this.array[rightIdx] ? this.array[rightIdx].val : Infinity
-        
-        if (node.val < lNodeVal && node.val < rNodeVal) return;
-
-        let swapIdx;
-        if (lNodeVal < rNodeVal) {
-            swapIdx = leftIdx
-        }  else {
-            swapIdx = rightIdx
-        }
-
-        [this.array[swapIdx], this.array[idx]] = [this.array[idx], this.array[swapIdx]]
-        this.siftDown(swapIdx);
-        
-    }
-
-    isEmpty() {
-        return this.array.length <= 1
-    }
-
-    peek() {
-        return this.array[1];
-    }
-}
-// Dijkstra Algorithm
-// Time: O(V + E)...vertex + edges...worst Case O(n * m)...rows * columns
-// Space: O(n * m)
 var shortestPathBinaryMatrix = function(grid) {
-    const minHeap = new MinHeap();
-    let n = grid.length;
-    if (grid[0][0] !== 0 || grid[n-1][n-1] !== 0) return -1;
-    let nde = new Node(0,0,1)
-    minHeap.insert(nde);
-    const visited = [...new Array(n+1)].map(a => [...new Array(n+1)].fill(false))
-    const dirs = [[-1,-1],[-1,0],[-1,1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
-
+    let n = grid.length, m = grid[0].length;
+    if (grid[0][0] !== 0 || grid[n-1][m-1] !== 0) return -1;
+    const dirs = [[-1,0],[1,0],[0,-1],[0,1],
+                  [-1,-1],[-1,1],[1,1],[1,-1]]
+    const visited = new Set();
+    const minHeap = new MinPriorityQueue();
+    minHeap.enqueue(new Node(1, 0, 0), 1)
     while (!minHeap.isEmpty()) {
-        let nde = minHeap.poll();
-        let r = nde.r, c = nde.c, v = nde.val;
-        if (r === n-1 && c === n-1) return v;
+        let node = minHeap.dequeue().element;
+        if (node.row === n-1 && node.col === m-1) return node.val;
+        visited.add(node.row + ":" + node.col);
         for (let [x, y] of dirs) {
-            let row = r + x, col = c + y;
-            if (row >= 0 && row < n && col >= 0 && col < n && !visited[row][col] && grid[row][col] === 0) {
-                if (row === n-1 && col === n-1) return v +1;
-                visited[row][col] = true;
-                minHeap.insert(new Node(row,col,v + 1))
+            let r = node.row + x, c = node.col + y;
+            if (r >= 0 && r < n && c >= 0 && c < m && grid[r][c] === 0) {
+                let key = r + ":" + c;
+                if (!visited.has(key)) {
+                    visited.add(key);
+                    minHeap.enqueue(new Node(node.val+ 1, r, c), node.val+ 1);
+                }
             }
         }
     }
