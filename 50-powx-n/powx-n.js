44f73a2c2ebd8c3277094b9393dcd2115116b752
Daniel Ball <g.thirteen333@gmail.com>
2025-01-22 21:59:49 -0500

    Time: 0 ms (100.00%) | Memory: 49.5 MB (9.86%) - LeetSync

--- 50-powx-n/powx-n.js
+++ 50-powx-n/powx-n.js
@@ -4,20 +4,20 @@
  * @return {number}
  */
 var myPow = function(x, n) {
-    if (n === 0) return 1
+
     if (n < 0) {
         x = 1 / x
         n *= -1
     }
-    let result = 1, pow = x
+    let pow = x, result = 1;
 
     while (n > 0) {
-        if (n & 1) {
-            result *= pow
-            n -= 1
+        if (n % 2 === 1) {
+            result *= pow;
+            n-=1;
         }
-        pow *= pow
-        n = Math.floor(n / 2)
+        pow *= pow;
+        n = Math.floor( n / 2);
     }
-    return result
+    return result;
 };
