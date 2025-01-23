cf15dcda74f37985db079ed2dfeac9e874ba0213
Daniel Ball <g.thirteen333@gmail.com>
2025-01-23 02:15:45 -0500

    Time: 20 ms (31.08%) | Memory: 66.9 MB (5.50%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.js
+++ 498-diagonal-traverse/diagonal-traverse.js
@@ -1,37 +1,26 @@
+/**
+ * @param {number[][]} mat
+ * @return {number[]}
+ */
 var findDiagonalOrder = function(mat) {
-    let result = [], n = mat.length, m = mat[0].length
-    let stack = [[0, 0, "up"]]
+    let diags = new Map();
 
-    while (stack.length) {
-        let [r, c, v] = stack.pop()
-
-        result.push(mat[r][c])
-        if (result.length === n * m) return result
-        if (v === "up") {
-            while (r > 0 && c < m-1) {
-                r--, c++
-                result.push(mat[r][c])
-            }
-            if (c + 1 < m) {
-                c++
-            } else {
-                r++
-            }
-            v = "dwn"
-        } else {
-            while (r < n-1 && c > 0) {
-                r++; c--
-                result.push(mat[r][c])
-            }
-            if (r+1 < n) {
-                r++
-            } else {
-                c++
-            }
-            v = "up"
+    for (let r = 0; r < mat.length; r++) {
+        for (let c = 0; c < mat[r].length; c++) {
+            let k = r + c;
+            if (!diags.has(k)) diags.set(k, []);
+            diags.get(k).push(mat[r][c]);
         }
-
-        stack.push([r, c, v])
+    }
+    let result = [];
+    let order = 'u'
+    for (let [k, v] of diags) {
+        if (order === 'u') {
+            result.push(...v.reverse())
+        } else {
+            result.push(...v)
+        }
+        order = order === 'u' ? 'd' : 'u'
     }
     return result;
 };
