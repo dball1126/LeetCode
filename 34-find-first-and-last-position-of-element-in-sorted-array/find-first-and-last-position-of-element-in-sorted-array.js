7169752ed98ac71bab7b8917e6a3f20e9bd72d0b
Daniel Ball <g.thirteen333@gmail.com>
2025-01-22 22:34:44 -0500

    Time: 0 ms (100.00%) | Memory: 49.3 MB (91.53%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.js
@@ -1,45 +1,39 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
 var searchRange = function(nums, target) {
-    if (!nums.length) return [-1,-1]
-    if (nums.length === 1 && nums[0] === target) return [0,0]
-    let low = 0, high = nums.length-1;
-    let leftSide = 0;
 
-    while (low < high) { // get right side
-        let m = Math.floor((high + low) / 2) + 1;
-        if (nums[m] === target) {
-            if (m+1 < nums.length && nums[m+1] === target) {
-                low = m +1;
+    const bSearch = (lo, hi, dir) => {
+        while (lo < hi) {
+            let m = Math.floor((hi + lo) / 2) + 1;
+
+            if (nums[m] > target) {
+                hi = m -1;
+            } else if (nums[m] < target) {
+                lo = m + 1;
             } else {
-                low = m;
-                break;
-            }
-        } else if (nums[m] > target) {
-            high = m -1;
-        } else {
-            low = m;
-        }
-    }
-    let rightSide = low;
-    if (nums[low] === target) { // get left side
-        high = low;
-        low = 0;
-        while (low < high) {
-            let mid = Math.floor((high + low) / 2)  + 1
-            if (nums[mid] === target) {
-                if (mid-1 >= 0 && nums[mid-1] === target) {
-                    high = mid -1;
+                if (dir === 'left') {
+                    if (nums[m-1] === target) {
+                        hi = m -1;
+                    } else {
+                        return m;
+                    }
                 } else {
-                    low = mid;
+                    if (nums[m+1] === target) {
+                        lo = m + 1;
+                    } else {
+                        return m;
+                    }
                 }
-            } else if (nums[mid] > target) {
-                high = mid - 1
-            } else {
-                low = mid;
             }
         }
+        return nums[lo] === target ? lo : -1;
     }
-    leftSide = low;
-    if (nums[rightSide] !== target || nums[leftSide] !== target) return [-1,-1]
 
-    return [leftSide, rightSide]
+    let left = bSearch(0, nums.length-1, 'left');
+    if (left === -1) return [-1, -1];
+    let right = bSearch(left, nums.length-1, 'right');
+    return [left, right]
 };
