dbe6f9ddf0da7c45056440fdee2b4449bbc66c60
Daniel Ball <g.thirteen333@gmail.com>
2025-01-23 01:38:33 -0500

    Time: 16 ms (50.60%) | Memory: 53.9 MB (65.00%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -1,31 +1,34 @@
 var topKFrequent = function(nums, k) {
-    const freq = new Map();
+    if (k === nums.length) return nums
+    let map = new Map();
     for (let n of nums) {
-        if (!freq.has(n)) freq.set(n, 0);
-        freq.set(n, freq.get(n) + 1);
+        if (!map.has(n)) map.set(n, 0);
+        map.set(n, map.get(n) + 1);
     }
-    const newNums = Array.from(freq)
-    return quickSelect(newNums, 0, newNums.length-1, newNums.length - k);
+    let arr = Array.from(map);
+    return quickSelect(0, arr.length-1, arr, arr.length - k);
 };
 
-const quickSelect = (nums, left, right, k) => {
-    let pivot = partition(nums, left, right);
+const quickSelect = (left, right, nums, k) => {
+    let partition = getPartition(left, right, nums);
 
-    if (pivot < k) {
-        return quickSelect(nums, pivot + 1, right, k);
-    } else if (pivot > k) {
-        return quickSelect(nums, left, pivot - 1, k);
-    } else {
-        return nums.slice(k).map(a => a[0]);
+    while (partition !== k) {
+        partition = getPartition(left, right, nums);
+        if (partition > k) {
+            right = partition - 1;
+        } else if (partition < k) {
+            left = partition + 1;
+        }
     }
+    return nums.slice(k).map(a => a[0])
 }
 
-const partition = (nums, left, right) => {
-    let i = left, j = right - 1, pivot = nums[right][1];
+const getPartition = (left, right, nums) => {
+    let partition = nums[right][1], i = left, j = right -1;
 
     while (i <= j) {
-        while (nums[i] && nums[i][1] < pivot) i++;;
-        while (nums[j] && nums[j][1] > pivot) j--;
+        while (nums[i] && nums[i][1] < partition) i++;
+        while (nums[j] && nums[j][1] > partition) j--;
 
         if (i <= j) {
             [nums[i], nums[j]] = [nums[j], nums[i]];
