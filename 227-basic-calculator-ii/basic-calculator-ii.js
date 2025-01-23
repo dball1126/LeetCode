e8dc5229b0127d03fb7ae226114ebab82a9438c0
Daniel Ball <g.thirteen333@gmail.com>
2025-01-22 23:10:30 -0500

    Time: 29 ms (64.65%) | Memory: 55.6 MB (85.55%) - LeetSync

--- 227-basic-calculator-ii/basic-calculator-ii.js
+++ 227-basic-calculator-ii/basic-calculator-ii.js
@@ -1,26 +1,25 @@
 var calculate = function(s) {
     
-    let lastNum = 0, currNum = 0, result = 0, opt = '+', len = s.length
+    let lastNum = 0, currNum = 0, result = 0, opt = "+";
 
     for (let i = 0; i < s.length; i++) {
-        if (s[i] !== ' ' && !isNaN(s[i])) {
-            currNum = (10 * currNum) + parseInt(s[i]);
+
+        if (s[i] !== " " && !isNaN(s[i])) {
+            currNum = (currNum * 10) + parseInt(s[i]);
         }
 
-        if (i === len-1 || (isNaN(s[i]) && s[i] !== ' ')) {
-
+        if (i === s.length-1 || (s[i] !== " " && isNaN(s[i]))) {
             if (opt === "+" || opt === "-") {
                 result += lastNum;
-                lastNum = (opt === '+' ? currNum : -currNum);
-            } else if (opt === "*") {
-                lastNum *= currNum;
+                lastNum = opt === "+" ? currNum : -currNum;
             } else if (opt === "/") {
                 lastNum = Math.trunc(lastNum / currNum);
+            } else if (opt === "*") {
+                lastNum*=currNum
             }
             currNum = 0;
-            opt = s[i]
+            opt = s[i];
         }
-
     }
-    return result + lastNum
+    return result + lastNum;
 };
