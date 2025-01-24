056666ed07c49e9a171470c56e0b701395313df2
Daniel Ball <g.thirteen333@gmail.com>
2025-01-24 03:10:33 -0500

    Time: 6 ms (88.78%) | Memory: 55.3 MB (83.64%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.js
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.js
@@ -1,39 +1,52 @@
-// Merge Lists
-// Time: O(n * log(k))...n is the number of elements, and k is the number of lists
+
+
+function ListNode(val, next) {
+    this.val = (val===undefined ? 0 : val)
+    this.next = (next===undefined ? null : next)
+}
+ 
+/**
+ * @param {ListNode[]} lists
+ * @return {ListNode}
+ */
 var mergeKLists = function(lists) {
     if (!lists.length) return null;
-    
+    if (lists.length === 1) return lists[0];
+
     while (lists.length > 1) {
-        for (let i = 0; i < Math.floor(lists.length / 2); i++ ) {
-            lists[i] = mergeList(lists[i], lists.pop());
+        for (let i = 0; i < (Math.floor(lists.length/ 2)); i++) {
+            lists[i] = mergeLists(lists[i], lists.pop());
         }
     }
     return lists[0]
 };
 
-const mergeList = (list1, list2) => {
-    let newList = null, curr = null;
+const mergeLists = (list1, list2) => {
+    let head = null, curr = null;
 
     while (list1 || list2) {
-        let v1 = list1 ? list1.val : Infinity;
-        let v2 = list2 ? list2.val : Infinity;
-        let val;
-        if (v1 <= v2) {
-            val = v1;
+        let val1 = Infinity, val2  = Infinity
+        if (list1) val1 = list1.val;
+        if (list2) val2 = list2.val;
+        if (val1 <= val2) {
+            if (!head) {
+                head = list1;
+                curr = head;
+            } else {
+                curr.next = list1;
+                curr = list1
+            }
             list1 = list1.next;
         } else {
-            val = v2;
+            if (!head) {
+                head = list2;
+                curr = head;
+            } else {
+                curr.next = list2;
+                curr = list2;
+            }
             list2 = list2.next;
         }
-        let node = new ListNode(val);
-        if (!newList) {
-            newList = node;
-            curr = node;
-        } else {
-            curr.next = node;
-            curr = node;
-        }
     }
-
-    return newList;
+    return head;
 }
