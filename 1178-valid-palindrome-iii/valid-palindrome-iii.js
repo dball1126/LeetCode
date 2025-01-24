653ab75ea913e71c67c6175ed9a60b6f53b441b4
Daniel Ball <g.thirteen333@gmail.com>
2025-01-24 18:25:58 -0500

    Time: 31 ms (75.14%) | Memory: 60.4 MB (63.01%) - LeetSync

--- 1178-valid-palindrome-iii/valid-palindrome-iii.js
+++ 1178-valid-palindrome-iii/valid-palindrome-iii.js
@@ -1,8 +1,3 @@
-/**
- * @param {string} s
- * @param {number} k
- * @return {boolean}
- */
 var isValidPalindrome = function(s, k) {
     let n = s.length;
     let dp = [...new Array(n)].map(a => [...new Array(n)].fill(0));
@@ -10,16 +5,11 @@ var isValidPalindrome = function(s, k) {
     for (let i = 1; i < n; i++) {
         for (let j = i-1; j >= 0; j--) {
             let val = s[j] === s[i] ? 0 : 1;
-            if (j+1 === i) {
-                dp[j][i] = val;
+            if (val === 0) {
+                dp[j][i] = dp[j+1][i-1]
             } else {
-                if (val === 0) {
-                    dp[j][i] = dp[j+1][i-1]
-                } else {
-                    dp[j][i] = val + Math.min(dp[j+1][i], dp[j][i-1]);
-                }
+                dp[j][i] = val + Math.min(dp[j+1][i], dp[j][i-1]);
             }
-
         }
     }
     return dp[0][n-1] <= k;
