bd06e509333456398543e3631847f453796cbc96
Daniel Ball <g.thirteen333@gmail.com>
2025-01-24 07:19:10 -0500

    Time: 0 ms (100.00%) | Memory: 50.9 MB (44.64%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.js
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.js
@@ -1,15 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
 var sumNumbers = function(root) {
-    if (!root) return 0
-    let totalSum = 0, stack = [[root, ""]]
-
-    while (stack.length) {
-        let [nde, sum] = stack.pop();
-        if (!nde.left && !nde.right) {
-            totalSum += parseInt(sum + nde.val)
-            continue;
-        }
-        if (nde.left) stack.push([nde.left, sum + nde.val])
-        if (nde.right) stack.push([nde.right, sum + nde.val])
+    if (!root) return 0;
+    let sum = 0;
+    const dfs = (nde, num) => {
+        if (!nde.left && !nde.right) return sum += parseInt(num + nde.val);
+        if (nde.left) dfs(nde.left, num + nde.val)
+        if (nde.right) dfs(nde.right, num + nde.val)
     }
-    return totalSum
+    dfs(root, "");
+    return sum;
 };
