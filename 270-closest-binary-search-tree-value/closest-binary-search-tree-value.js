387a75ec90a4dfc7b698874e40754d8ac5fea152
Daniel Ball <g.thirteen333@gmail.com>
2025-01-24 16:40:00 -0500

    Time: 0 ms (100.00%) | Memory: 53.6 MB (10.54%) - LeetSync

--- 270-closest-binary-search-tree-value/closest-binary-search-tree-value.js
+++ 270-closest-binary-search-tree-value/closest-binary-search-tree-value.js
@@ -1,32 +1,37 @@
-var closestValue = function(root, target) {
-    let min = Infinity, minAvg = Infinity
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} target
+ * @return {number}
+ */
+var closestValue = function(root, tgt) {
+    let closestVal = root.val;
     const dfs = (nde) => {
-        if (!nde) return Infinity
-
-        if (target === nde.val) {
-            minAvg = 0
-            min = nde.val
-            return;
-        } else if (target > nde.val) {
-            let newAvg = target - nde.val
-            if (newAvg < minAvg) {
-                minAvg = newAvg
-                min = nde.val
-            } else if ( newAvg === minAvg && nde.val < min) {
-                min = nde.val
-            }
-            dfs(nde.right)
-        } else {
-            let newAvg = nde.val - target
-            if (newAvg < minAvg) {
-                minAvg = newAvg
-                min = nde.val
-            }else if ( newAvg === minAvg && nde.val < min) {
-                min = nde.val
-            }
-            dfs(nde.left)
+        if (!nde) return;
+        if (nde.val === tgt) {
+            return closestVal = nde.val;
         }
+        let diff = Math.abs(tgt - nde.val);
+        let curr = Math.abs(tgt - closestVal);
+
+        if (diff < curr) {
+            closestVal = nde.val;
+        } else if (diff === curr) {
+            closestVal = Math.min(nde.val, closestVal);
+        }
+        if (nde.val > tgt) {
+            dfs(nde.left);
+        } else {
+            dfs(nde.right);
+         }
     }
-    dfs(root)
-    return min;
+    dfs(root);
+    return closestVal;
 };
