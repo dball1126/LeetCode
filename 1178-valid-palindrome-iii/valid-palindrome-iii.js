17d2e084435d67eb250ecc6272dd7e25465718d8
Daniel Ball <g.thirteen333@gmail.com>
2025-01-24 18:22:00 -0500

    Time: 36 ms (71.68%) | Memory: 58.5 MB (65.32%) - LeetSync

--- 1178-valid-palindrome-iii/valid-palindrome-iii.js
+++ 1178-valid-palindrome-iii/valid-palindrome-iii.js
@@ -1,24 +1,26 @@
-// Bottom-Up Dynamic Programming
-// Time and Space: O(n * m)...for rows * cols
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {boolean}
+ */
 var isValidPalindrome = function(s, k) {
-    const dp = [...new Array(s.length+1)].map(a => [...new Array(s.length+1)].fill(0))
-    for (let i = 0; i < s.length; i++) {
+    let n = s.length;
+    let dp = [...new Array(n)].map(a => [...new Array(n)].fill(0));
+
+    for (let i = 1; i < n; i++) {
         for (let j = i-1; j >= 0; j--) {
-            if (s[j] === s[i]) {
-                if (j+1 === i) {
-                    dp[j][i] = 0
-                } else {
-                    dp[j][i] = dp[j+1][i-1]
-                }
+            let val = s[j] === s[i] ? 0 : 1;
+            if (j+1 === i) {
+                dp[j][i] = val;
             } else {
-                if (j+1 === i) {
-                    dp[j][i] = Math.min(dp[j+1][i], dp[j][i-1]) + 1
+                if (val === 0) {
+                    dp[j][i] = dp[j+1][i-1]
                 } else {
-                    dp[j][i] = Math.min(dp[j+1][i], dp[j][i-1]) +1
-                    dp[j][i] = Math.min(dp[j][i], dp[j+1][i-1] + 2)
+                    dp[j][i] = val + Math.min(dp[j+1][i], dp[j][i-1]);
                 }
             }
+
         }
     }
-    return dp[0][s.length-1] <= k
+    return dp[0][n-1] <= k;
 };
