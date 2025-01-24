0f77784708d94ba834233634047a09e6bac7b5f5
Daniel Ball <g.thirteen333@gmail.com>
2025-01-24 02:20:33 -0500

    Time: 2 ms (25.96%) | Memory: 51.2 MB (33.17%) - LeetSync

--- 777-toeplitz-matrix/toeplitz-matrix.js
+++ 777-toeplitz-matrix/toeplitz-matrix.js
@@ -3,12 +3,10 @@
  * @return {boolean}
  */
 var isToeplitzMatrix = function(matrix) {
-    let n = matrix.length, m = matrix[0].length
-    for (let r = 0; r < n; r++) {
-        for (let c  = 0; c < m; c++) {
-            if (r -1 >= 0 && c - 1 >= 0) {
-                if (matrix[r-1][c-1] !== matrix[r][c]) return false
-            }
+    let n = matrix.length, m = matrix[0].length;
+    for (let r = 1; r < n; r++) {
+        for (let c = 1; c < m; c++) {
+            if (matrix[r-1][c-1] !== matrix[r][c]) return false;
         }
     }
     return true;
