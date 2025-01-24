5442324acaade0d9ccced7752abf39f2ca9509dd
Daniel Ball <g.thirteen333@gmail.com>
2025-01-24 17:04:28 -0500

    Time: 0 ms (100.00%) | Memory: 48.9 MB (56.76%) - LeetSync

--- 163-missing-ranges/missing-ranges.js
+++ 163-missing-ranges/missing-ranges.js
@@ -1,31 +1,14 @@
-/**
- * @param {number[]} nums
- * @param {number} lower
- * @param {number} upper
- * @return {number[][]}
- */
-// Two-pointer technique
-// Time: O(n)
-// Space: O(1)...not counting output array
 var findMissingRanges = function(nums, lower, upper) {
-    const result = [], n = nums.length;
-    if (!nums.length) result.push([lower,upper])
-    for (let i = 0; i < n; i++) {
-        
-        if ((lower === nums[i] && i !== n-1) || (i === n-1 && lower === upper)) {
-            lower++;
-            continue;
-        } else if (i === n-1 && lower === nums[i] && lower !== upper) {
-            result.push([lower+1, upper])
-            continue;
+    if (!nums.length) return [[lower,upper]]
+    const missingRanges = [];
+    for (let n of nums) {
+        if (lower < n) {
+            missingRanges.push([lower,n-1]);
         }
-
-        result.push(([lower, nums[i] - 1]))
-        lower = nums[i]+1
-
-        if (i + 1 >= n && nums[i] !== upper) {
-            result.push([lower, upper])
-        }
+        lower = n+1;
     }
-    return result;
+    if (lower <= upper) {
+        missingRanges.push([lower, upper]);
+    }
+    return missingRanges;
 };
