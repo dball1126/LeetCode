7ca5fbf85121ab3272beb9146ebaec5906c3d6f4
Daniel Ball <g.thirteen333@gmail.com>
2025-01-25 01:48:29 -0500

    Time: 2 ms (76.18%) | Memory: 53.6 MB (54.34%) - LeetSync

--- 65-valid-number/valid-number.js
+++ 65-valid-number/valid-number.js
@@ -1,35 +1,22 @@
-/**
- * @param {string} s
- * @return {boolean}
- */
-/// Follow the 'Rules'
-// Time: O(n)
-// Space: O(1)
 var isNumber = function(s) {
-    const isNum = (i) => s[i] >= "0" && s[i] <= "9"
-    const isSign = (i) => s[i] === "+" || s[i] === "-"
-    const isE = (i) => s[i] === "E" || s[i] === "e" 
-    const isDot = (i) => s[i] === '.'
-    let seenSign = false, seenE = false, seenDot = false, seenNum = false;
-    let n = s.length;
+    let seenNum = false, seenDot = false, seenE = false;
 
-    for (let i = 0; i < n; i++) {
+    for (let i = 0; i < s.length; i++) {
+        let v = s[i];
 
-        if (isE(i)) {
-            if (!seenNum || seenE) return false
-            seenNum = false; seenSign = false; seenDot = false
-        } else if (isDot(i)) {
+        if (!isNaN(v)) {
+            seenNum = true;
+        } else if (v === 'e' || v === 'E') {
+            if (seenE || !seenNum) return false;
+            seenE = true; seenNum = false;
+        } else if (v === ".") {
             if (seenDot || seenE) return false;
-        } else if (isSign(i)) {
-            if ((seenSign && !seenE) || seenNum || seenDot) return false;
-        } else if (!isNum(i)) {
+            seenDot = true;
+        } else if (v === "+" || v === "-") {
+            if (i !== 0 && !(s[i-1] === "e" || s[i-1] === "E")) return false;
+        } else {
             return false;
         }
-
-        if (isE(i)) seenE = true;
-        if (isNum(i)) seenNum = true;
-        if (isSign(i)) seenSign = true;
-        if (isDot(i)) seenDot = true;
     }
     return seenNum;
 };
