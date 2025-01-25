65c97486284a7c9d09921df1d7fda516db17afde
Daniel Ball <g.thirteen333@gmail.com>
2025-01-24 19:20:57 -0500

    Time: 57 ms (75.66%) | Memory: 51.3 MB (87.14%) - LeetSync

--- 133-clone-graph/clone-graph.js
+++ 133-clone-graph/clone-graph.js
@@ -1,41 +1,39 @@
 /**
- * // Definition for a Node.
- * function Node(val, neighbors) {
+ * // Definition for a _Node.
+ * function _Node(val, neighbors) {
  *    this.val = val === undefined ? 0 : val;
  *    this.neighbors = neighbors === undefined ? [] : neighbors;
  * };
  */
 
 /**
- * @param {Node} node
- * @return {Node}
+ * @param {_Node} node
+ * @return {_Node}
  */
 var cloneGraph = function(node) {
-    if (!node) return node;
-    let graph, map = new Map(), stack = [node]
-
+    if (!node) return null;
+    const nodeMap = new Map();
+    const stack = [node];
+    const visited = new Set();
+    let head;
     while (stack.length) {
-        let nde = stack.pop()
-        let newNde;
-        if (!map.has(nde)) {
-            newNde = new Node(nde.val)
-            map.set(nde, newNde)
-        } else {
-            newNde = map.get(nde)
+        let curr = stack.pop();
+        if (!nodeMap.has(curr)) {
+            nodeMap.set(curr, new _Node(curr.val));
         }
-        if (!graph) graph = newNde;
-        
-        for (let n of nde.neighbors) {
-            let child;
-            if (!map.has(n)) {
-                child = new Node(n.val)
-                stack.push(n)
-                map.set(n, child)
-            } else {
-                child = map.get(n)
+        let copy = nodeMap.get(curr);
+        visited.add(curr);
+        for (let n of curr.neighbors) {
+            if (!nodeMap.has(n)) nodeMap.set(n, new _Node(n.val));
+            copy.neighbors.push(nodeMap.get(n));
+            if (!visited.has(n)) {
+                stack.push(n);
+                visited.add(n);
             }
-            newNde.neighbors.push(child)
+        }
+        if (!head) {
+            head = copy;
         }
     }
-    return graph;
+    return head;
 };
