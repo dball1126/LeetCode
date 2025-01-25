16a8379dd473a5bbb757fd8fadd4ca63a477a300
Daniel Ball <g.thirteen333@gmail.com>
2025-01-25 07:03:50 -0500

    Time: 1 ms (56.93%) | Memory: 53.3 MB (6.02%) - LeetSync

--- 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.js
+++ 998-check-completeness-of-a-binary-tree/check-completeness-of-a-binary-tree.js
@@ -1,23 +1,35 @@
-// Level Order Traversal
-// Time and Space: O(n)
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
 var isCompleteTree = function(root) {
-    let queue = [[root]], placed = false
+    if (!root) return true;
+
+    let allowChildren = true;
+    const queue = [[[root, allowChildren]]];
     while (queue.length) {
-        let lvl = queue.shift();
-        let nxLvl = []
-        for (let n of lvl) {
+        let level = queue.shift()
+        let newLevel = []
+        for (let [n, allowChild] of level) {
+            if (!allowChild) allowChildren = false;
             if (n.right && !n.left) return false;
-            if (placed &&( n.left || n.right)) return false;
-            if (n.left && n.right) {
-                nxLvl.push(n.left, n.right)
-            } else if (n.left) {
-                nxLvl.push(n.left)
-                placed = true;
-            } else if (!n.left && !n.right) {
-                placed = true;
-            }
+            if ((n.left || n.right) && !allowChildren) {
+                return false;
+                }
+            if (n.left && !n.right || (!n.left)) allowChildren = false;
+            if (n.left) newLevel.push([n.left, allowChildren]);
+            if (n.right) newLevel.push([n.right, allowChildren]);
         }
-        if (nxLvl.length) queue.push(nxLvl)
+        if (newLevel.length) queue.push(newLevel)
     }
+
     return true;
 };
