2a735656937f8f8ac23973553dcd9b8d8873e697
Daniel Ball <g.thirteen333@gmail.com>
2025-01-25 02:49:09 -0500

    Time: 0 ms (100.00%) | Memory: 52.2 MB (22.15%) - LeetSync

--- 31-next-permutation/next-permutation.js
+++ 31-next-permutation/next-permutation.js
@@ -2,37 +2,30 @@
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
  */
-// Array Manipulation
-// Time: O(n), Space: O(1)
 var nextPermutation = function(nums) {
-    let len = nums.length
-    let reverse = (i, j) => {
+    let switched = false;
+    const reverse = (i, j) => {
         while (i < j) {
             [nums[i], nums[j]] = [nums[j], nums[i]]
-            i++; j--;
+            i++; j--
         }
     }
-    let findSmallestVal = (val, idx) => {
-        let small = idx;
-        while (idx < len) {
-            if (nums[idx] <= nums[small] && nums[idx] > val) {
-                small = idx;
+    for (let i = nums.length-2; i >= 0; i--) {
+        if (nums[i] < nums[i+1]) {
+            let j = nums.length -1;
+            while (j >= i+1) {
+                if (nums[j] > nums[i]) {
+                    break
+                }
+                j--
             }
-            idx++;
-        }
-        return small;
-    }
-    for (let i = nums.length -1; i >= 0; i--) {  
-   
-        if (i - 1 >= 0 && nums[i-1] < nums[i]) {
-            let j = i;
-            let minIdx = findSmallestVal(nums[i-1], j)
-            let newIdx = i-1;
-            [nums[newIdx], nums[minIdx]] = [nums[minIdx],nums[newIdx]]
-            reverse(i, len - 1)
-            return nums;
+            [nums[i], nums[j]] = [nums[j], nums[i]];
+            reverse(i+1, nums.length-1)
+            switched = true;
+            break;
         }
     }
-    reverse(0, len - 1)
-    return nums;
+    if (!switched){ // do in place reversal
+        reverse(0, nums.length-1)
+    }
 };
