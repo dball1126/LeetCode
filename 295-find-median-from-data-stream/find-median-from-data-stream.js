dbe4dabcd94c7e5aee8ebeb3695caba0fab15d67
Daniel Ball <g.thirteen333@gmail.com>
2025-01-26 07:11:07 -0500

    Time: 158 ms (58.57%) | Memory: 86.6 MB (5.21%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.js
+++ 295-find-median-from-data-stream/find-median-from-data-stream.js
@@ -1,40 +1,49 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+    }
+  }
+
+
+  
 var MedianFinder = function() {
     this.lo = new MinPriorityQueue();
     this.hi = new MaxPriorityQueue();
-  };
-  
-  /** 
-   * @param {number} num
-   * @return {void}
-   */
-  MedianFinder.prototype.addNum = function(n) {
-    if (this.lo.isEmpty() || n > this.lo.front().element) {
-        this.lo.enqueue(n);
+};
+
+/** 
+ * @param {number} num
+ * @return {void}
+ */
+MedianFinder.prototype.addNum = function(num) {
+    if (this.lo.isEmpty() || num > this.lo.front().element.value) {
+        this.lo.enqueue(new Node(num), num);
     } else {
-        this.hi.enqueue(n);
-    } 
-
-    if (this.lo.size() > (this.hi.size()+1)) {
-
-        this.hi.enqueue(this.lo.dequeue().element)
-    } else if (this.hi.size() > (this.lo.size() + 1) ) {
-
-        this.lo.enqueue(this.hi.dequeue().element)
+        this.hi.enqueue(new Node(num), num);
     }
-  };
-  
-  /**
-   * @return {number}
-   */
-  MedianFinder.prototype.findMedian = function() {
-  
+
+    if (this.lo.size() > this.hi.size()+1) {
+        let node = this.lo.dequeue().element;
+        this.hi.enqueue(node, node.value);
+    } else if (this.hi.size() > this.lo.size()+1) {
+        let node = this.hi.dequeue().element;
+        this.lo.enqueue(node, node.value);
+    }
+};
+
+/**
+ * @return {number}
+ */
+MedianFinder.prototype.findMedian = function() {
+    
     if (this.lo.size() > this.hi.size()) {
-        return this.lo.front().element
+        return this.lo.front().element.value;
     } else if (this.hi.size() > this.lo.size()) {
-        return this.hi.front().element
+        return this.hi.front().element.value;
     } else {
-        let v1 = this.lo.front().element || 0, v2 = this.hi.front().element || 0
+        let v1 = 0, v2 = 0;
+        if (!this.lo.isEmpty()) v1 = this.lo.front().element.value
+        if (!this.hi.isEmpty()) v2 = this.hi.front().element.value
         return (v1 + v2) / 2
     }
-  };
-  
+};
