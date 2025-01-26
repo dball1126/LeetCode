f4054e2c9bce2d51a0496f497bf4607d9eced55b
Daniel Ball <g.thirteen333@gmail.com>
2025-01-26 05:22:19 -0500

    Time: 0 ms (100.00%) | Memory: 49.7 MB (31.90%) - LeetSync

--- 62-unique-paths/unique-paths.js
+++ 62-unique-paths/unique-paths.js
@@ -1,21 +1,18 @@
-/**
- * @param {number} m
- * @param {number} n
- * @return {number}
- */
-// Bottom-Up Dynamic Programming
-// Time: O(m * m)...m for # of rows and n for # of columns
-// Space: O(n)
 var uniquePaths = function(m, n) {
-    let row = [...new Array(n)].fill(0)
-    row[0] = 1
+    
+    let dp = [...new Array(m)].map(a => [...new Array(n)].fill(0))
+    dp[0][0] = 1
     for (let r = 0; r < m; r++) {
         for (let c = 0; c < n; c++) {
-            if (r === 0 && c === 0) continue;
-            let prevUp = row[c], prevLeft = 0
-            if (c-1 >= 0) prevLeft = row[c-1]
-            row[c] = prevLeft + prevUp
+            let prev1 = 0, prev2 = 0
+            if (r -1 >= 0) {
+                prev1 = dp[r-1][c]
+            }
+            if (c -1 >= 0) {
+                prev2 = dp[r][c-1]
+            }
+            dp[r][c]+= prev1 + prev2;
         }
     }
-    return row[n-1]
+    return dp[m-1][n-1];
 };
