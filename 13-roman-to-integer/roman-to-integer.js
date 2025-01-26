42ca8d367d5dacdf1389a2ac333ce074fb026601
Daniel Ball <g.thirteen333@gmail.com>
2025-01-26 02:09:28 -0500

    Time: 11 ms (20.67%) | Memory: 58 MB (7.86%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.js
+++ 13-roman-to-integer/roman-to-integer.js
@@ -1,20 +1,20 @@
 var romanToInt = function(s) {
-    let i = 0, total = 0
+    let i = s.length-1, total = 0
     const map = new Map([["I", 1], ["V", 5], ["X", 10], ["L", 50],
     ["C", 100], ["D", 500], ["M", 1000], ["IV", 4], ["IX", 9], ["XL", 40], ["XC", 90],
     ["CD", 400], ["CM", 900]]);
 
-    while (i < s.length) {
-        let prev = "", next = "";
+    while (i >= 0) {
+        let prev = ""
         if (i-1 >= 0) prev = s[i-1] + s[i];
-        if (i+1 < s.length) next = s[i] + s[i+1];
 
         if (map.has(prev)) {
             total += map.get(prev);
-        } else if (!map.has(next)) {
+            i-=2
+        } else {
             total += map.get(s[i]);
+            i--;
         }
-        i++;
     }
     return total;
 }
