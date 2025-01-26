575eddc15a101f4c88e57b7aabc5f19da080b091
Daniel Ball <g.thirteen333@gmail.com>
2025-01-25 20:58:25 -0500

    Time: 41 ms (41.32%) | Memory: 65.6 MB (70.86%) - LeetSync

--- 15-3sum/3sum.js
+++ 15-3sum/3sum.js
@@ -1,35 +1,24 @@
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
-// Loop and Two-Pointer approach.
-// Time: O(n^2)
-// Space: O(1)...not counting output array
 var threeSum = function(nums) {
-    nums.sort((a,b) => a - b)
-    const result = [], n = nums.length;
-
-    for (let i = 0; i < n-2; i++) {
-        let j = i+1, k = n-1
+    nums.sort((a,b) => a-b);
+    let triplets = [];
+    for (let i = 0; i < nums.length; i++) {
+        let j = i+1;
+        let k = nums.length-1;
 
         while (j < k) {
-            let v1 = nums[i], v2 = nums[j], v3 = nums[k]
-            let sum = v1 + v2 + v3
-            if (sum === 0) {
-                result.push([v1,v2,v3]);
+            let val = nums[i] + nums[j] + nums[k];
+            if ((nums[i] + nums[j] + nums[k]) === 0) {
+                triplets.push([nums[i] , nums[j] , nums[k]]);
+            }
+            if (val >= 0) {
+                while(k > j && nums[k-1] === nums[k]) k--;
+                k--;
+            } else {
                 while (j < k && nums[j] === nums[j+1]) j++;
                 j++;
-            } else if (sum < 0) {
-                while (j < k && nums[j] === nums[j+1]) j++;
-                j++
-            } else {
-                while (k > j && nums[k-1] === nums[k]) k--;
-                k--
             }
         }
-        while (i < n && nums[i] === nums[i+1]) {
-            i++
-        }
+        while (i < nums.length && nums[i] === nums[i+1]) i++;
     }
-    return result;
+    return triplets;
 };
