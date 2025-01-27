72234ab1cfeea8826b4bb784e3f751b8ed96a7cc
Daniel Ball <g.thirteen333@gmail.com>
2025-01-27 01:06:56 -0500

    Time: 5 ms (71.19%) | Memory: 53.9 MB (92.91%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.js
@@ -1,32 +1,29 @@
 var findMedianSortedArrays = function(nums1, nums2) {
-    
     if (nums1.length > nums2.length) {
         [nums1, nums2] = [nums2, nums1];
     }
-    let m = nums1.length;
-    let n = nums2.length;
-    let left = 0, right = m;
+    let n = nums1.length, m = nums2.length;
+    let left = 0, right = n;
 
     while (left <= right) {
-        let partitionA = Math.floor((left + right) / 2);
+        let partitionA = Math.floor((right + left) / 2);
         let partitionB = Math.floor((n + m + 1) / 2 - partitionA);
 
-        let maxLeftA = partitionA === 0 ? -Infinity : nums1[partitionA-1];
-        let maxLeftB = partitionB === 0 ? -Infinity : nums2[partitionB-1];
-        let minLeftA = partitionA === m ? Infinity : nums1[partitionA];
-        let minLeftB = partitionB === n ? Infinity : nums2[partitionB];
+        let maxLeftA = partitionA === 0 ? -Infinity : nums1[partitionA - 1];
+        let maxLeftB = partitionB === 0 ? -Infinity : nums2[partitionB - 1];
+        let minLeftA = partitionA === n ? Infinity : nums1[partitionA];
+        let minLeftB = partitionB === m ? Infinity : nums2[partitionB];
 
         if (maxLeftA <= minLeftB && maxLeftB <= minLeftA) {
-            if (((n+m)%2) === 0) {
-                return ((Math.max(maxLeftA, maxLeftB) + Math.min(minLeftA, minLeftB)) / 2)
+            if ((n+m)% 2 === 0) {
+                return (Math.max(maxLeftA, maxLeftB) + Math.min(minLeftA, minLeftB) ) / 2
             } else {
                 return Math.max(maxLeftA, maxLeftB);
             }
-        } else if (minLeftA > maxLeftB) {
-            right = partitionA -1
+        } else if (minLeftA > minLeftB) {
+            right = partitionA - 1;
         } else {
-            left = partitionA + 1
+            left = partitionA + 1;
         }
     }
-
 };
