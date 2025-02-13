ed027040b1003d79409e53e0f572d4305b105196
Daniel Ball <g.thirteen333@gmail.com>
2025-02-12 23:05:17 -0500

    Time: 11 ms (42.31%) | Memory: 54.7 MB (11.54%) - LeetSync

---
+++ 830-largest-triangle-area/largest-triangle-area.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} points
+ * @return {number}
+ */
+// Geometry
+// Time: O(n^3)
+// Space: O(1)
+var largestTriangleArea = function(points) {
+    let maxArea = -1
+
+    for (let i = 0; i < points.length; i++) {
+        for (let j = i+1; j < points.length; j++) {
+            for (let k = j+1; k < points.length; k++) {
+
+                let [x1,y1] = points[i],
+                    [x2,y2] = points[j],
+                    [x3,y3] = points[k]
+                    
+                // shoelace formual
+                let sum = (Math.abs(((x1 * y2) + (x2 * y3) + (x3 * y1)) - ((y1 * x2) + (y2 * x3) + (y3 * x1))) / 2)
+                maxArea = Math.max(maxArea, sum)
+            } 
+        }
+    }
+    return maxArea
+};
