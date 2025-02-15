82354a6991c2c5d293eaf834255d147ec7347d41
Daniel Ball <g.thirteen333@gmail.com>
2025-02-15 01:06:40 -0500

    Time: 380 ms (29.30%) | Memory: 147.9 MB (15.16%) - LeetSync

--- 204-count-primes/count-primes.js
+++ 204-count-primes/count-primes.js
@@ -1,20 +1,22 @@
-// Sieve of Eratosthenes
-// Time: O(n * log(n))
-// Space: O(n)
 var countPrimes = function(n) {
     const primes = [...new Array(n)].fill(true);
-    primes[0] = false; primes[1] = false;
+    primes[0] = false;
+    primes[1] = false;
+    let count = 0;
 
     for (let i = 2; i < n; i++) {
-        let j = i;
-        while (j+i < n) {
-            j+= i;
-            primes[j] = false;
+        if (primes[i]) {
+            count++;
+            let j = i, k = i;
+            while (j * i < n) {
+                j *= i;
+                primes[j] = false;
+            }
+            while (k + i < n) {
+                k += i;
+                primes[k] = false;
+            }
         }
     }
-    let count = 0;
-    primes.forEach(v => {
-        if (v) count++;
-    })
     return count;
 };
