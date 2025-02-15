010670a75b3ecb703f907fdbf061c90061bcba8a
Daniel Ball <g.thirteen333@gmail.com>
2025-02-15 00:39:00 -0500

    Time: 2 ms (48.92%) | Memory: 52.7 MB (56.38%) - LeetSync

---
+++ 69-sqrtx/sqrtx.js
@@ -0,0 +1,18 @@
+var mySqrt = function(x) {
+    if (x === 1) return 1;
+    let half = Math.floor(x / 2);
+    let low = 0, high = half;
+    while (low < high) {
+        let m = Math.floor((high + low) / 2) + 1;
+
+        let val = m * m;
+        if (val === x) {
+            return m;
+        } else if (val > x) {
+            high = m - 1;
+        } else {
+            low = m;
+        }
+    }
+    return low;
+};
