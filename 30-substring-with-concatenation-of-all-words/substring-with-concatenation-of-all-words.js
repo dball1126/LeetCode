0a5bd054906ca05315f9346ee6288cb8ff3eb76c
Daniel Ball <g.thirteen333@gmail.com>
2025-02-16 04:04:23 -0500

    Time: 1702 ms (48.39%) | Memory: 57.5 MB (62.00%) - LeetSync

--- 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.js
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.js
@@ -1,19 +1,18 @@
+// Sliding Window
+// Time: O((n * k) + m)...n for s.length, k for the length of every word
+// m for the number of words 
+// Space: O(m)
 var findSubstring = function(s, words) {
-    const wrdMap = new Map(), wrdLength = words[0].length;
+    const wrdMap = new Map(), wrdLength = words[0].length, allIdxs = [];;
     let allWrdLength = 0;
     for (let w of words) {
         allWrdLength += w.length
         if (!wrdMap.has(w)) wrdMap.set(w, 0);
         wrdMap.set(w, wrdMap.get(w) + 1);
     }
-
-    const allIdxs = [];
-
     const recurse = (startIdx, i, map) => {
-
         while (i < s.length) {
             if (!map.size)return allIdxs.push(startIdx);
-
             let wrd = s.substring(i, i+ wrdLength);
             if (map.has(wrd)) {
             map.set(wrd, map.get(wrd) - 1)
@@ -28,9 +27,7 @@ var findSubstring = function(s, words) {
     }
     for (let i = 0; i < s.length; i++) {
         if (i+allWrdLength >= s.length+1) break;
-
         let wrd = s.substring(i, i+ wrdLength);
-
         if (wrdMap.has(wrd)) {
             let copy = new Map(wrdMap);
             copy.set(wrd, copy.get(wrd) - 1)
