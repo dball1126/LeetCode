5b34fed3a28f7a0e68c8af445c3c4bca197ce400
Daniel Ball <g.thirteen333@gmail.com>
2025-02-16 03:37:11 -0500

    Time: 1743 ms (47.15%) | Memory: 57.7 MB (50.49%) - LeetSync

---
+++ 30-substring-with-concatenation-of-all-words/substring-with-concatenation-of-all-words.js
@@ -0,0 +1,42 @@
+var findSubstring = function(s, words) {
+    const wrdMap = new Map(), wrdLength = words[0].length;
+    let allWrdLength = 0;
+    for (let w of words) {
+        allWrdLength += w.length
+        if (!wrdMap.has(w)) wrdMap.set(w, 0);
+        wrdMap.set(w, wrdMap.get(w) + 1);
+    }
+
+    const allIdxs = [];
+
+    const recurse = (startIdx, i, map) => {
+
+        while (i < s.length) {
+            if (!map.size)return allIdxs.push(startIdx);
+
+            let wrd = s.substring(i, i+ wrdLength);
+            if (map.has(wrd)) {
+            map.set(wrd, map.get(wrd) - 1)
+            if (map.get(wrd) === 0) map.delete(wrd);
+
+                i+= wrdLength;
+            } else {
+                break;
+            }
+        }
+        if (!map.size) allIdxs.push(startIdx);
+    }
+    for (let i = 0; i < s.length; i++) {
+        if (i+allWrdLength >= s.length+1) break;
+
+        let wrd = s.substring(i, i+ wrdLength);
+
+        if (wrdMap.has(wrd)) {
+            let copy = new Map(wrdMap);
+            copy.set(wrd, copy.get(wrd) - 1)
+            if (copy.get(wrd) === 0) copy.delete(wrd);
+            recurse(i, i+wrdLength, copy);
+        }
+    }
+    return allIdxs
+};
