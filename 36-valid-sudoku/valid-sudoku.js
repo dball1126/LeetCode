feacd1098df6938d6529d4ca3383717a672635f4
Daniel Ball <g.thirteen333@gmail.com>
2025-03-07 23:56:33 -0500

    Time: 4 ms (78.67%) | Memory: 58.3 MB (14.98%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.js
+++ 36-valid-sudoku/valid-sudoku.js
@@ -1,44 +1,32 @@
-/**
- * @param {character[][]} board
- * @return {boolean}
- */
 // Time: O(n)..n being the number of cells
 // Space: O(n)
 var isValidSudoku = function(board) {
     let rows = new Map(), cols = new Map()
     for (let i = 0; i < 9; i++) {
-        rows.set(i, new Set())
-        cols.set(i, new Set())
+        rows.set(i, 0);
+        cols.set(i, 0);
     }
-
+    const hasBit = (n, mask) => (mask & n) >= 1;
+    const setBit = (n, mask) => mask | n;
     const validateSudoku = (row, col) => {
-        let curr = [...new Array(10)].fill(false);
+        let mask = 0;
         for (let r = row; r < row+3; r++) {
             for (let c = col; c < col+3; c++) {
                 if (board[r][c] === ".") continue
-                let v = parseInt(board[r][c]);
-                if (rows.get(r).has(v)) {
-                    return false;
-                }
-                if (cols.get(c).has(v)) {
-                    return false;
-                }
-                if (curr[v]) {
-                    return false;
-                }
-                rows.get(r).add(v)
-                cols.get(c).add(v)
-                curr[v] = true;
+                let v = (1 << parseInt(board[r][c]));
+                if (hasBit(v, rows.get(r))) return false;
+                if (hasBit(v, cols.get(c))) return false;
+                if (hasBit(v, mask)) return false
+                rows.set(r, setBit(rows.get(r), v));
+                cols.set(c, setBit(cols.get(c), v));
+                mask = setBit(v, mask);
             }
         }
         return true
     }
-
     for (let i = 0; i < 9; i+=3) {
         for (let j = 0; j < 9; j+=3) {
-           if (!validateSudoku(i, j)) {
-            return false;
-            }
+           if (!validateSudoku(i, j)) return false;
         }
     }
     return true;
