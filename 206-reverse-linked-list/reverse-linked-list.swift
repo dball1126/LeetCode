14dac1a69068fe0150d9bc099a10f015d603d0e7
Daniel Ball <g.thirteen333@gmail.com>
2025-03-19 23:06:54 -0400

    Time: 0 ms (100.00%) | Memory: 20 MB (35.19%) - LeetSync

---
+++ 206-reverse-linked-list/reverse-linked-list.swift
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init() { self.val = 0; self.next = nil; }
+ *     public init(_ val: Int) { self.val = val; self.next = nil; }
+ *     public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+ * }
+ */
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        var prev: ListNode? = nil
+        var curr: ListNode? = head
+        while let nde = curr {
+            var next: ListNode? = nde.next
+            nde.next = prev
+            prev = nde
+            curr = next
+        }
+        return prev
+    }
+}
