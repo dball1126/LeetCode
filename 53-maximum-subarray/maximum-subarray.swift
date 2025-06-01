aded4d434d444b3fb24501825fac54cfa0e2fa41
Daniel Ball <g.thirteen333@gmail.com>
2025-05-31 23:02:34 -0400

    Time: 0 ms (100.00%) | Memory: 21.8 MB (9.38%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.swift
+++ 53-maximum-subarray/maximum-subarray.swift
@@ -1,12 +1,13 @@
 class Solution {
     func maxSubArray(_ nums: [Int]) -> Int {
-        guard !nums.isEmpty else { return 0 }
-        var maximum = nums[0]
-        var currMax = nums[0]
-        for idx in 1..<nums.count  {
-            currMax = max(nums[idx], currMax + nums[idx])
-            maximum = max(maximum, currMax)
+        var maxSum = nums[0]
+        var currSum = nums[0]
+
+        for i in 1..<nums.count {
+            currSum = max(nums[i], currSum + nums[i])
+            maxSum = max(maxSum, currSum)
         }
-        return maximum
+
+        return maxSum
     }
 }
