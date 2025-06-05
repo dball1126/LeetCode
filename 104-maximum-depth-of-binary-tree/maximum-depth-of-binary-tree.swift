1ecf053f0d01b2b443c99ffcf93449b9d8f2cb52
Daniel Ball <g.thirteen333@gmail.com>
2025-06-05 12:44:01 -0400

    Time: 2 ms (11.76%) | Memory: 20 MB (61.47%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.swift
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.swift
@@ -15,9 +15,7 @@
  */
 class Solution {
     func maxDepth(_ root: TreeNode?) -> Int {
-        guard var node = root  else {
-            return 0
-        }
-        return max(maxDepth(node.left), maxDepth(node.right)) + 1
+        guard var node = root else { return 0 }
+        return max(maxDepth(node.left), maxDepth(node.right)) + 1;
     }
 }
