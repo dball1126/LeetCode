c773afe92bbeb5dae33cf979e31df7d4926f06e9
Daniel Ball <g.thirteen333@gmail.com>
2025-06-05 02:11:37 -0400

    Time: 11 ms (84.55%) | Memory: 20.2 MB (70.12%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.swift
+++ 125-valid-palindrome/valid-palindrome.swift
@@ -1,37 +1,33 @@
 class Solution {
     func isPalindrome(_ s: String) -> Bool {
-       
-
+        
       // FIX: Get a String.Index from the integer 'lo'
-        guard var lo = s.index(s.startIndex, offsetBy: 0, limitedBy: s.endIndex) else {
+        guard var l = s.index(s.startIndex, offsetBy: 0, limitedBy: s.endIndex) else {
             // This guard is important for safety, though for lo=0 it won't fail unless s is empty.
             // If s is empty, s.count - 1 will be -1, and this guard would prevent a crash
             // if you tried to access an invalid offset.
             return true // An empty string is a palindrome
         }
-        guard var hi = s.index(s.startIndex, offsetBy: s.count-1, limitedBy: s.endIndex) else {
+        guard var h = s.index(s.startIndex, offsetBy: s.count-1, limitedBy: s.endIndex) else {
             return true
         }
 
-           
-        while lo < hi {
-            if !isAlphanumeric(char: s[lo]) {
-                lo = s.index(after: lo)
-            } else if !isAlphanumeric(char: s[hi]){
-                hi = s.index(before: hi)
+        while l < h {
+            var lC: Character = s[l], rC: Character = s[h]
+
+            if !(lC.isLetter || lC.isNumber) {
+                l = s.index(after: l)
+            } else if !(rC.isLetter || rC.isNumber) {
+                h = s.index(before: h)
             } else {
-                if s[lo].lowercased() != s[hi].lowercased() {
+                if String(lC).lowercased() != String(rC).lowercased() {
                     return false
                 }
-                lo = s.index(after: lo)
-                hi = s.index(before: hi)
+                l = s.index(after: l)
+                h = s.index(before: h)
             }
         }
 
         return true
-    }
-
-    func isAlphanumeric(char: Character) -> Bool {
-        return char.isLetter || char.isNumber
     }
 }
