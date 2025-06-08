7ee0998ad3af563f6d136531bb9bb1980d3d2410
Daniel Ball <g.thirteen333@gmail.com>
2025-06-08 10:11:16 -0400

    Time: 7 ms (23.60%) | Memory: 25.4 MB (32.40%) - LeetSync

--- 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.swift
+++ 1909-buildings-with-an-ocean-view/buildings-with-an-ocean-view.swift
@@ -1,11 +1,12 @@
 class Solution {
     func findBuildings(_ heights: [Int]) -> [Int] {
-        var stack: [Int] = []
+        var stack:[Int] = []
 
-        for i in 0..<heights.count {
-            var curr = heights[i]
+        for (i, n) in heights.enumerated() {
+            let curr: Int = heights[i]
+
             while !stack.isEmpty && heights[stack[stack.count - 1]] <= curr {
-                stack.removeLast()
+                stack.popLast()
             }
             stack.append(i)
         }
