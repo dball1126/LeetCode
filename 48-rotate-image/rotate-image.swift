32c97173e31f77e18f89d016beaa752b75fdc912
Daniel Ball <g.thirteen333@gmail.com>
2025-06-12 21:32:57 -0400

    Time: 0 ms (100.00%) | Memory: 19.7 MB (60.21%) - LeetSync

--- 48-rotate-image/rotate-image.swift
+++ 48-rotate-image/rotate-image.swift
@@ -1,18 +1,17 @@
 class Solution {
     func rotate(_ matrix: inout [[Int]]) {
-         let n = matrix.count
-         for r in 0..<n {
-            for c in r..<n {
-                var num = matrix[r][c]
+        for r in 0..<matrix.count {
+            for c in r..<matrix.count {
+                var temp = matrix[r][c]
                 matrix[r][c] = matrix[c][r]
-                matrix[c][r] = num 
+                matrix[c][r] = temp
             }
-            var lo = 0, hi = n-1
+            var lo = 0, hi = matrix.count - 1
             while lo < hi {
                 matrix[r].swapAt(lo, hi)
                 lo += 1
                 hi -= 1
             }
-         }
+        }
     }
 }
