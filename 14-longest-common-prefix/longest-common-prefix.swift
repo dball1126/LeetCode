92df5abc17de20d6831d94215dc9e48c7d658eae
Daniel Ball <g.thirteen333@gmail.com>
2025-06-19 23:41:08 -0400

    Time: 2 ms (51.81%) | Memory: 20 MB (37.63%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.swift
+++ 14-longest-common-prefix/longest-common-prefix.swift
@@ -1,33 +1,24 @@
 class Solution {
     func longestCommonPrefix(_ strs: [String]) -> String {
-        var longest = ""
-        if strs.isEmpty { return ""}
-        else if strs.count == 1 { return strs[0] }
-        var len = strs[0]
+        var longestStr: String = ""
+        guard strs.count >= 1 else { return ""}
+        guard strs.count > 1 else { return strs[0] }
+
+        var len: String = strs[0]
 
         for i in len.indices {
-            var char = ""
-            for j in 1..<strs.count{
-                var w1: String = strs[j-1], w2: String = strs[j]
-                var c1: String.Index
-                var c2: String.Index
-                if i < w1.endIndex && i < w2.endIndex {
-                    c1 = w1.index(i, offsetBy: 0)
-                    c2 = w2.index(i, offsetBy: 0)
-                    if c1 == nil || c2 == nil { return longest }
-                    if w1[c1] != w2[c2] { return longest }
-                    if w1[i] != w2[i] { return longest}
+            
+            for j in 1..<strs.count {
+                var curr: String = strs[j]
+                var prev: String = strs[j-1]
+                if i < curr.endIndex && i < prev.endIndex && (String(curr[i]) == String(prev[i])) {
                 } else { 
-                    return longest 
-                }
-
-                if j == strs.count-1 {
-                    char = String(w1[c1])
+                    return longestStr 
                 }
 
             }
-            if !char.isEmpty { longest += char }
+            longestStr += String(len[i])
         }
-        return longest
+        return longestStr
     }
 }
