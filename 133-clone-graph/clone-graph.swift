53b72e5470713d321d868dac453830a03e9cd106
Daniel Ball <g.thirteen333@gmail.com>
2025-06-22 07:54:35 -0400

    Time: 7 ms (78.94%) | Memory: 19.1 MB (11.57%) - LeetSync

--- 133-clone-graph/clone-graph.swift
+++ 133-clone-graph/clone-graph.swift
@@ -13,18 +13,14 @@
 class Solution {
     func cloneGraph(_ node: Node?) -> Node? {
         guard var root = node else { return node }
-        var map: [Int : Node] = [:]
-        var newNode: Node
-        var visited = Set<Int>()
+        var map: [Int : Node] = [:], newNode: Node, visited = Set<Int>()
 
         func dfs(_ nde: Node?) -> Node? {
             guard var n = nde else { return nil }
             if map[n.val] == nil { map[n.val] = Node(n.val)}
             var newNode: Node = map[n.val]!
             visited.insert(n.val)
-
             for item in n.neighbors {
-
                 if map[item!.val] == nil { map[item!.val] = Node(item!.val) }
                 var newItem = map[item!.val]
                 newNode.neighbors.append(newItem)
@@ -35,7 +31,6 @@ class Solution {
             }
             return newNode
         }
-        var result = dfs(root)
-        return result
+        return dfs(root)
     }
 }
