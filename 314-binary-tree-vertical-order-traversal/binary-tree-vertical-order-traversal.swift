79fa69617c862a363d64d2e3dbb4afb18a54eda9
Daniel Ball <g.thirteen333@gmail.com>
2025-06-25 00:15:38 -0400

    Time: 0 ms (100.00%) | Memory: 20.1 MB (7.61%) - LeetSync

---
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.swift
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+class Solution {
+    func verticalOrder(_ root: TreeNode?) -> [[Int]] {
+        guard var r = root else { return []}
+        var minL: Int = 0, maxR: Int = 0
+
+        func dfs(_ node: TreeNode?, _ path: Int) -> Int {
+            guard var n = node else { return 0 }
+            minL = min(minL, path)
+            maxR = max(maxR, path)
+            var item = dfs(n.left, path - 1)
+            var item2 = dfs(n.right, path + 1)
+            return path
+        }
+        dfs(r, 0)
+        var total = abs(minL) + 1 + maxR
+        var result: [[Int]] = Array(repeating: [], count: total)
+
+        var levels: [[(TreeNode, Int)]] = [[(r, 0)]]
+
+        while !levels.isEmpty {
+            var level: [(TreeNode, Int)] = levels.removeFirst()
+            var newLevel: [(TreeNode, Int)] = []
+            for (nde, col) in level {
+                result[col + abs(minL)].append(nde.val)
+                if var left = nde.left {
+                    newLevel.append((left, col - 1))
+                }
+                if var right = nde.right {
+                    newLevel.append((right, col + 1))
+                }
+            }
+            if !newLevel.isEmpty {
+                levels.append(newLevel)
+            }
+        }
+
+        return result
+    }
+}
