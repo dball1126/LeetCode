70623e763cd5ac2a6ec3edd6f4bfdabfe75af7bd
Daniel Ball <g.thirteen333@gmail.com>
2025-06-25 16:54:54 -0400

    Time: 3 ms (5.98%) | Memory: 19.9 MB (20.09%) - LeetSync

--- 62-unique-paths/unique-paths.swift
+++ 62-unique-paths/unique-paths.swift
@@ -1,15 +1,14 @@
+
 class Solution {
     func uniquePaths(_ m: Int, _ n: Int) -> Int {
-        var dp = Array(repeating: Array(repeating: 0, count: n), count: m)
-        for i in 0..<m{
-            for j in 0..<n {
-                if i == 0 || j == 0 {
-                    dp[i][j] = 1
-                } else {
-                    dp[i][j] += dp[i-1][j] + dp[i][j-1]
-                }
+        var dp: [[Int]] = Array(repeating: Array(repeating: 0, count: m), count: n)
+        dp[0][0] = 1
+        for r: Int in 0..<n {
+            for c: Int in 0..<m {
+                if r - 1 >= 0 { dp[r][c] += dp[r-1][c]}
+                if c - 1 >= 0 { dp[r][c] += dp[r][c-1]}
             }
         }
-        return dp[m-1][n-1]
+        return dp[n-1][m-1]
     }
 }
