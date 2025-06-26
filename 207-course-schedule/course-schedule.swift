83f1c61c30641c1e7f1498918cae1de65b3e0393
Daniel Ball <g.thirteen333@gmail.com>
2025-06-26 19:25:12 -0400

    Time: 11 ms (13.68%) | Memory: 20.4 MB (27.92%) - LeetSync

--- 207-course-schedule/course-schedule.swift
+++ 207-course-schedule/course-schedule.swift
@@ -1,27 +1,23 @@
 class Solution {
     func canFinish(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
-        var adjList: [Int:Set<Int>] = [:]
-        for i: Int in 0..<numCourses {
-            adjList[i] = []
-        }
-        for pre in prerequisites {
-            adjList[pre[0]]?.insert(pre[1])
-        }
-        var visited = Set<Int>(), visiting = Set<Int>()
+        var adjList: [Int: Set<Int>] = [:], visited = Set<Int>(), visiting = Set<Int>()
+        for i in 0..<numCourses { adjList[i] = Set<Int>()}
+        for pre in prerequisites { adjList[pre[0]]?.insert(pre[1])}
 
         func hasCycle(_ node: Int) -> Bool {
             guard !visited.contains(node) else { return false }
             guard !visiting.contains(node) else { return true }
             visiting.insert(node)
 
-            for child: Int in adjList[node]! {
-                if hasCycle(child) { return true }
+            for n in adjList[node]! {
+                if hasCycle(n) { return true }
             }
             visiting.remove(node)
             visited.insert(node)
             return false
         }
-        for i: Int in 0..<numCourses {
+
+        for i in 0..<numCourses {
             if hasCycle(i) { return false }
         }
         return true
