657f0bcea7aafa8632134228e5842e47d615d966
Daniel Ball <g.thirteen333@gmail.com>
2025-06-26 22:31:28 -0400

    Time: 0 ms (100.00%) | Memory: 20 MB (44.89%) - LeetSync

--- 1-two-sum/two-sum.swift
+++ 1-two-sum/two-sum.swift
@@ -1,14 +1,13 @@
+
 class Solution {
     func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-
-        var map: [Int:Int] = [:]
-
+        var idxs:[Int:Int] = [:]
         for (i, n) in nums.enumerated() {
-            if var idx = map[target - n] {
-                return [i, idx]
+            if var idx = idxs[target - n] {
+                return [idx, i]
             }
-            map[n] = i
+            idxs[n] = i
         }
-        return []        
+        return []
     }
 }
