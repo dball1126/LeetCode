3d366ab0894409a85bceeb106e40c67baea59b37
Daniel Ball <g.thirteen333@gmail.com>
2025-06-29 13:03:25 -0400

    Time: 1074 ms (6.06%) | Memory: 71 MB (54.54%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.swift
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.swift
@@ -1,25 +1,30 @@
 class Solution {
     func validPath(_ n: Int, _ edges: [[Int]], _ source: Int, _ destination: Int) -> Bool {
-        var adjList: [Int: Set<Int>] = [:], visited = Set<Int>()
-        for edge: [Int] in edges {
-            adjList[edge[0], default: Set<Int>()].insert(edge[1])
-            adjList[edge[1], default: Set<Int>()].insert(edge[0])
-        }
+        var adjList: [Int:Set<Int>] = [:], visited = Set<Int>()
 
-        func dfs(_ node: Int?) -> Bool {
-            guard var nde = node else { return false }
+        for edge in edges {
+            if adjList[edge[0]] == nil { adjList[edge[0]] = Set<Int>()}
+            if adjList[edge[1]] == nil { adjList[edge[1]] = Set<Int>()}
+            adjList[edge[0]]!.insert(edge[1])
+            adjList[edge[1]]!.insert(edge[0])
+        }
+        var bfs: [Int] = [source]
+
+        while !bfs.isEmpty {
+            var node: Int = bfs.removeFirst()
             if node == destination { return true }
-            visited.insert(nde)
-            if var children = adjList[nde] {
-                for child in Array(children) {
+            visited.insert(node)
+
+            if var childSet = adjList[node] {
+                if childSet.contains(destination) { return true }
+                for child: Int in childSet {
                     if !visited.contains(child) {
                         visited.insert(child)
-                        if dfs(child) { return true}
+                        bfs.append(child)
                     }
                 }
             }
-            return false
         }
-        return dfs(source)
+        return false
     }
 }
