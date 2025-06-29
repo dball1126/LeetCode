1c528fb2e232fe572c4bebb5dde72600920c8133
Daniel Ball <g.thirteen333@gmail.com>
2025-06-29 13:46:04 -0400

    Time: 1 ms (75.21%) | Memory: 20.7 MB (12.54%) - LeetSync

--- 207-course-schedule/course-schedule.swift
+++ 207-course-schedule/course-schedule.swift
@@ -1,23 +1,26 @@
 class Solution {
     func canFinish(_ numCourses: Int, _ prerequisites: [[Int]]) -> Bool {
-        var adjList: [Int: Set<Int>] = [:], visited = Set<Int>(), visiting = Set<Int>()
-        for i in 0..<numCourses { adjList[i] = Set<Int>()}
-        for pre in prerequisites { adjList[pre[0]]?.insert(pre[1])}
-
-        func hasCycle(_ node: Int) -> Bool {
-            guard !visited.contains(node) else { return false }
-            guard !visiting.contains(node) else { return true }
-            visiting.insert(node)
-
-            for n in adjList[node]! {
-                if hasCycle(n) { return true }
-            }
-            visiting.remove(node)
-            visited.insert(node)
-            return false
+        var visited: Set<Int> = Set<Int>(), visiting: Set<Int> = Set<Int>()
+        var adjList: [Int:Set<Int>] = [:]
+        for i: Int in 0..<numCourses {
+            if adjList[i] == nil { adjList[i] = Set<Int>()}
+        }
+        for pre: [Int] in prerequisites {
+            adjList[pre[0]]!.insert(pre[1])
         }
 
-        for i in 0..<numCourses {
+        func hasCycle(_ nde: Int) -> Bool {
+            if visited.contains(nde) { return false }
+            if visiting.contains(nde) { return true }
+            visiting.insert(nde)
+            for child: Int in adjList[nde]! {
+                if hasCycle(child) { return true }
+            }
+            visiting.remove(nde)
+            visited.insert(nde)
+            return false
+        }
+        for i: Int in 0..<numCourses {
             if hasCycle(i) { return false }
         }
         return true
