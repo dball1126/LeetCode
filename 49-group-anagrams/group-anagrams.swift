c931dc232e5a830f439e0c7540efb4b73d47600d
Daniel Ball <g.thirteen333@gmail.com>
2025-06-29 00:03:20 -0400

    Time: 11 ms (65.33%) | Memory: 21.9 MB (38.56%) - LeetSync

--- 49-group-anagrams/group-anagrams.swift
+++ 49-group-anagrams/group-anagrams.swift
@@ -1,20 +1,24 @@
 class Solution {
     func groupAnagrams(_ strs: [String]) -> [[String]] {
-        var groups:[String: [String]] = [:]
         var a = Character("a").asciiValue
+        var groups:[String:[String]] = [:]
 
         for s in strs {
             var counts: [Int] = Array(repeating: 0, count: 26)
-            for c in s {
+
+            for (i, c) in s.enumerated() {
                 var idx = Int(c.asciiValue! - a!)
                 counts[idx] += 1
             }
-            var key = ""
-            for (i, n) in counts.enumerated() {
-                if n > 0 { key += "\(i):\(n)"}
+            var key: String = ""
+            for (i, v) in counts.enumerated() {
+                if v > 0 {
+                    key += "\(i):\(v)"
+                }
             }
             groups[key, default: []].append(s)
         }
+
         return Array(groups.values)
     }
 }
