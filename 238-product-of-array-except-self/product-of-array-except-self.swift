a21e59bc4e6c69ad795f4547f7dbab71357d9b37
Daniel Ball <g.thirteen333@gmail.com>
2025-06-29 15:34:10 -0400

    Time: 0 ms (100.00%) | Memory: 25.6 MB (32.65%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.swift
+++ 238-product-of-array-except-self/product-of-array-except-self.swift
@@ -1,21 +1,18 @@
 class Solution {
     func productExceptSelf(_ nums: [Int]) -> [Int] {
-        var n: [Int] = nums
-        var prefixSums: [Int] = Array(repeating: 0, count: nums.count)
-        var prefixSum: Int = 1
-        for (i, v) in n.enumerated() {
-            prefixSum *= v
-            prefixSums[i] = prefixSum
+        var leftPrefixSum: Int = 1, rightPrefixSum: Int = 1
+        var leftSums: [Int] = nums.map{$0}
+        var result: [Int] = nums.map{$0}
+        for (i, v) in nums.enumerated() {
+            leftPrefixSum *= v
+            leftSums[i] = leftPrefixSum
         }
-        var i: Int = n.count - 1
-        prefixSum = 1
-        while i >= 0 {
-            let leftSum: Int = i > 0 ? prefixSums[i - 1] : 1
-            var temp = prefixSum * n[i]
-            n[i] = leftSum * prefixSum
-            prefixSum = temp
-            i -= 1
+
+        for idx: Int in stride(from: nums.count-1, through: 0, by: -1) {
+            var left: Int = idx > 0 ? leftSums[idx - 1] : 1
+            result[idx] = left * rightPrefixSum
+            rightPrefixSum *= nums[idx]
         }
-        return n
+        return result
     }
 }
