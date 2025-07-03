653db4f8a4e1c34fb390935ade982701719e2cd9
Daniel Ball <g.thirteen333@gmail.com>
2025-07-03 18:44:41 -0400

    Time: 0 ms (100.00%) | Memory: 19.5 MB (60.51%) - LeetSync

--- 792-binary-search/binary-search.swift
+++ 792-binary-search/binary-search.swift
@@ -1,7 +1,8 @@
 class Solution {
     func search(_ nums: [Int], _ target: Int) -> Int {
-        var lo: Int = 0, hi: Int = nums.count - 1
-        while lo < hi {
+        var lo: Int = 0, hi: Int = nums.count-1
+
+        while lo <= hi {
             var mid: Int = (hi + lo) / 2
 
             if nums[mid] == target {
@@ -12,6 +13,6 @@ class Solution {
                 lo = mid + 1
             }
         }
-        return nums[lo] == target ? lo : -1
+        return (lo < nums.count && nums[lo] == target) ? lo : -1
     }
 }
