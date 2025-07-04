61354019dc438a1f99098169b6738f13fb6e05ac
Daniel Ball <g.thirteen333@gmail.com>
2025-07-03 20:00:10 -0400

    Time: 18 ms (28.77%) | Memory: 19.7 MB (98.39%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.swift
+++ 125-valid-palindrome/valid-palindrome.swift
@@ -1,10 +1,12 @@
 class Solution {
     func isPalindrome(_ s: String) -> Bool {
-        var lo = s.startIndex, hi = s.index(before: s.endIndex)
+        var lo: String.Index = s.startIndex
+        var hi: String.Index = s.index(before: s.endIndex)
+
         while lo < hi {
             if !(s[lo].isNumber || s[lo].isLetter) {
                 lo = s.index(after: lo)
-            } else if !(s[hi].isNumber || s[hi].isLetter) {
+            } else if !(s[hi].isLetter || s[hi].isNumber) {
                 hi = s.index(before: hi)
             } else {
                 if s[lo].lowercased() != s[hi].lowercased() {
