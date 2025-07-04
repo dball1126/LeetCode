3e709f212c046a9af77e49886c48ee9b5b89a991
Daniel Ball <g.thirteen333@gmail.com>
2025-07-03 20:12:16 -0400

    Time: 3 ms (41.91%) | Memory: 19.4 MB (78.46%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.swift
+++ 2-add-two-numbers/add-two-numbers.swift
@@ -1,4 +1,4 @@
-/**
+/**,
  * Definition for singly-linked list.
  * public class ListNode {
  *     public var val: Int
@@ -10,27 +10,26 @@
  */
 class Solution {
     func addTwoNumbers(_ l1: ListNode?, _ l2: ListNode?) -> ListNode? {
-        var result: ListNode?, n1: ListNode? = l1, n2: ListNode? = l2
-        var curr: ListNode?, carry: Int = 0
+        var result: ListNode?, curr: ListNode?, carry: Int = 0, l: ListNode? = l1, r: ListNode? = l2
 
-        while n1 != nil || n2 != nil {
-            if var newN1: ListNode = n1 {
-                carry += newN1.val
-                n1 = newN1.next
+        while l != nil || r != nil {
+            if var lnx = l {
+                carry += lnx.val
+                l = lnx.next
             }
-            if var newN2: ListNode = n2 {
-                carry += newN2.val
-                n2 = newN2.next
+            if var rnx: ListNode = r {
+                carry += rnx.val
+                r = rnx.next
             }
             var node: ListNode = ListNode(carry % 10)
-            if result == nil { result = node }
-            curr?.next = node
-            curr = node
+            if result == nil { result = node}
+            if curr == nil {
+                curr = node
+            } else { curr!.next = node; curr = node}
+
             carry /= 10
         }
-        if carry > 0 {
-            curr?.next = ListNode(carry)
-        }
-        return result
+        if carry != 0 { curr!.next = ListNode(carry)}
+        return result         
     }
 }
