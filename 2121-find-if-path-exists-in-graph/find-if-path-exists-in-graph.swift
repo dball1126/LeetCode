8c2f05e4554c0fd5187d4aaedcbfab7793c56ac6
Daniel Ball <g.thirteen333@gmail.com>
2025-07-03 23:23:36 -0400

    Time: 1052 ms (5.71%) | Memory: 71.9 MB (52.86%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.swift
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.swift
@@ -1,6 +1,6 @@
 class Solution {
     func validPath(_ n: Int, _ edges: [[Int]], _ source: Int, _ destination: Int) -> Bool {
-        var adjList: [Int:Set<Int>] = [:], visited = Set<Int>()
+        var adjList: [Int: Set<Int>] = [:]
 
         for edge in edges {
             if adjList[edge[0]] == nil { adjList[edge[0]] = Set<Int>()}
@@ -8,19 +8,17 @@ class Solution {
             adjList[edge[0]]!.insert(edge[1])
             adjList[edge[1]]!.insert(edge[0])
         }
-        var bfs: [Int] = [source]
-
-        while !bfs.isEmpty {
-            var node: Int = bfs.removeFirst()
+        var stack: [Int] = [source], visited: Set<Int> = Set<Int>()
+        while !stack.isEmpty {
+            var node: Int = stack.removeFirst()
             if node == destination { return true }
             visited.insert(node)
-
-            if var childSet = adjList[node] {
-                if childSet.contains(destination) { return true }
-                for child: Int in childSet {
-                    if !visited.contains(child) {
-                        visited.insert(child)
-                        bfs.append(child)
+            if var nodes: Set<Int> = adjList[node] {
+                if nodes.contains(destination) { return true }
+                for n in nodes {
+                    if !visited.contains(n) {
+                        stack.append(n)
+                        visited.insert(n)
                     }
                 }
             }
