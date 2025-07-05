8585893dbf31c541caeb9497c48a0058ec806bc5
Daniel Ball <g.thirteen333@gmail.com>
2025-07-05 02:50:31 -0400

    Time: 1 ms (55.43%) | Memory: 19.9 MB (61.62%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.swift
+++ 14-longest-common-prefix/longest-common-prefix.swift
@@ -1,19 +1,16 @@
 class Solution {
     func longestCommonPrefix(_ strs: [String]) -> String {
-        if strs.count == 0 { return "" }
-        if strs.count == 1 { return strs[0]}
-        var longest: String = ""
-        var len: String = strs[0]
-        
-        for i in len.indices {
-
-            for j: Int in 1..<strs.count {
-                var prev = strs[j-1], curr = strs[j]
-                if i < prev.endIndex && i < curr.endIndex && (String(prev[i]) == String(curr[i])) {
+        var longest: String = "", idxs = strs[0].indices
+        if strs.count == 0 { return ""}
+        if strs.count == 1 { return strs[0] }
+        for i in idxs {
+            for j in 1..<strs.count {
+                var prev: String = strs[j-1], curr: String = strs[j]
+                if i < prev.endIndex && i < curr.endIndex && prev[i] == curr[i] {
 
                 } else { return longest }
             }
-            longest += String(len[i])
+            longest += String(strs[0][i])
         }
         return longest
     }
