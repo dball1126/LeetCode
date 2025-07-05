6fa8fdd38c716a2b2e0910a0723af31278f5558b
Daniel Ball <g.thirteen333@gmail.com>
2025-07-04 23:50:15 -0400

    Time: 1 ms (65.35%) | Memory: 20.5 MB (78.64%) - LeetSync

--- 56-merge-intervals/merge-intervals.swift
+++ 56-merge-intervals/merge-intervals.swift
@@ -1,14 +1,14 @@
 class Solution {
     func merge(_ intervals: [[Int]]) -> [[Int]] {
-        var ints: [[Int]] = intervals.sorted{$0[0] < $1[0]}
-        var x = ints[0][0], y = ints[0][1], result: [[Int]] = []
-        for i in 1..<ints.count {
-            var cx: Int = ints[i][0], cy: Int = ints[i][1]
-            if cx > y {
+        var ints: [[Int]] = intervals.sorted{$0[0] < $1[0]}, result: [[Int]] = []
+        var x: Int = ints[0][0], y = ints[0][1]
+
+        for i: Int in 1..<ints.count {
+            if ints[i][0] > y {
                 result.append([x, y])
-                x = cx; y = cy;
+                x = ints[i][0]; y = ints[i][1]
             } else {
-                y = max(y, cy)
+                y = max(y, ints[i][1])
             }
         }
         result.append([x, y])
